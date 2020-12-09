0a50accd2fa1c3d866ff5e575882905d38761565
Rafael Pufal <rafael.pufal23@gmail.com>
2020-12-09 17:15:05 -0300

    adicionando requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const inputText = document.querySelector('#texto-tarefa');
 const buttonAdd = document.querySelector('#criar-tarefa');
-//inputText.addEventListener('change',updateContent);
 buttonAdd.addEventListener('click', addTask);
-function updateContent (inputBox) {
-    const newText = inputBox.target.value;
-}
 function addTask () {
     const taskList = document.querySelector('#lista-tarefas');
     const taskItem = document.createElement('li');
-    
     taskItem.innerText = inputText.value;
     taskList.appendChild(taskItem);
+    taskItem.addEventListener('click', selectItem);
+}
+
+function selectItem(chosenItem) {
+    const itemSelected = chosenItem.target;
+    itemSelected.style.backgroundColor = 'rgb(128,128,128';
+    itemSelected.className +='selected'
 }
