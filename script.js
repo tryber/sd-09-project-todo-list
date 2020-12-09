54381bb85c00f1766f1120c4a4c217258df0bba4
Felipe Cardoso <felipedclc@gmail.com>
2020-12-09 15:12:59 -0300

    req 4

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function createInput() {
-    let input = document.createElement('input');
-    input.id = 'texto-tarefa';
-    input.type = 'text';
-    input.placeholder = 'digite o item';
-    
-    let section1 = document.querySelector('#section1');
-    section1.appendChild(input); 
+  let input = document.createElement('input');
+  input.id = 'texto-tarefa';
+  input.type = 'text';
+  input.placeholder = 'digite o item';
+
+  let section1 = document.querySelector('#section1');
+  section1.appendChild(input);
 }
 createInput();
 
+function createTaskList() {
+  let taskList = document.querySelector('#task-list');
+  let ol = document.createElement('ol');
+  ol.id = 'lista-tarefas';
+  taskList.appendChild(ol);
+}
+createTaskList();
