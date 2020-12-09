f3e158647554332b74eda59c9f9d8726f9ec815d
Marcelo Campos <marcelo.campos.economista@gmail.com>
2020-12-09 14:55:49 -0300

    Adiciona os desafios de 1 a 5

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = window.onload = function () {}
+
+const getDivInput = document.getElementsByClassName('input-container')[0];
+const createInput = document.createElement('input');
+createInput.setAttribute("type", "text");
+createInput.id = 'texto-tarefa';
+getDivInput.appendChild(createInput);
+
+const getDivList = document.getElementsByClassName('list-container')[0];
+const createOList = document.createElement('ol');
+createOList.id = 'lista-tarefas';
+getDivList.appendChild(createOList);
+
+const createAddListButton = document.createElement('button');
+createAddListButton.innerHTML = 'Adicionar';
+createAddListButton.id = 'criar-tarefa';
+getDivInput.appendChild(createAddListButton);
+
+function addNewTask() {
+  const getInputField = document.querySelector('#texto-tarefa');
+  const getAddInputButton = document.querySelector('#criar-tarefa');
+  const getOrdenedList = document.querySelector('#lista-tarefas');
+
+  getAddInputButton.addEventListener('click', function () {
+    const newLi = document.createElement('li');
+    newLi.innerText = getInputField.value;
+
+    getOrdenedList.appendChild(newLi);
+    getInputField.value = '';
+  })
+}
+addNewTask();
