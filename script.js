5032f2caf3cdf2b5bf914cec3bc44f5fef8a35c0
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-09 18:05:03 -0400

    Corrigindo requisito 12

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const btnTask = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const btnDel = document.querySelector('#apaga-tudo');
-const btnDelComplete = document.querySelector('#remover-finalizados')
-const btnSave = document.querySelector('#salvar-tarefas')
+const btnDelComplete = document.querySelector('#remover-finalizados');
+const btnSave = document.querySelector('#salvar-tarefas');
 
 function presetList() {
-  let beforeList = localStorage.getItem('Lista');
-  if (beforeList !== null){
-    beforeList = beforeList.split(',');
-    for (let index = 0; index < beforeList.length; index += 1) {
-      let beforeLi = document.createElement('li');
-      beforeLi.innerText = beforeList[index];
-      beforeLi.className = 'task';
+  //let beforeList = localStorage.getItem('Lista');
+  let storage = localStorage.getItem(0);
+  if (storage !== null) {
+    for (let index = 0; index < localStorage.length; index += 1) {
+      const lista = []
+      let atual = localStorage.getItem(index);
+      atual = atual.split(',');
+      const beforeLi = document.createElement('li');
+      beforeLi.innerText = atual[0];
+      beforeLi.className = atual[1];
       taskList.appendChild(beforeLi);
     }
   }
@@ -62,20 +65,21 @@ function removeCompleted() {
     for (let index = 0; index < listOfComplete.length; index += 1) {
       taskList.removeChild(listOfComplete[index]);
     }
-  })
+  });
 }
 
 function storageList() {
-  let list = document.querySelectorAll('.task');
-  let arrayList = [];
-  for(let index = 0; index < list.length; index += 1) {
+  const list = document.querySelectorAll('.task');
+  for (let index = 0; index < list.length; index += 1) {
+    const arrayList = [];
     arrayList.push(list[index].innerText);
-    localStorage.setItem('Lista', arrayList);
+    arrayList.push(list[index].className);
+    localStorage.setItem(index, arrayList);
   }
 }
 
 function saveList() {
-  btnSave.addEventListener('click', storageList)
+  btnSave.addEventListener('click', storageList);
 }
 
 presetList();
