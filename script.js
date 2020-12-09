d0ad1d4f15ef8afffbf5957c5b19b26657600167
Osvaldo Borges <netoo70@gmail.com>
2020-12-09 17:22:02 -0300

    Requisito 09 foi dificil

--- script.js
+++ script.js
@@ -24,19 +24,52 @@ function changeNotSelected() {
   }
 }
 
+function changeCompleted() {
+  const listTask = document.querySelectorAll('.task-list');
+  for (let index = 0; index < listTask.length; index += 1) {
+    if (listTask[index].className == 'task-list completed') {
+      listTask[index].className = 'task-list completed';
+    } else if (listTask[index].className == 'task-list completed selected') {
+      listTask[index].className = 'task-list completed';
+    } else if (listTask[index].className == 'task-list selected completed') {
+      listTask[index].className = 'task-list completed';
+    } else {
+      listTask[index].className = 'task-list';
+    }
+  }
+}
+
 function changeSelected(origin) {
   if (origin.target.className == 'task-list') {
-    changeNotSelected();
+    changeCompleted()
     origin.target.className += ' selected';
-  } else {
+  } else if (origin.target.className == 'task-list completed') {
+    changeCompleted();
+    origin.target.className += ' selected';
+  } else { 
+    changeCompleted();
+  }
+}
+
+function completedTask(origin) {
+  if (origin.target.className == 'task-list') {
+    origin.target.className += ' completed';
+  } else if (origin.target.className == 'task-list selected') {
+    origin.target.className += ' completed';
+  } else if (origin.target.className == 'task-list selected completed') {
+    origin.target.className = 'task-list selected';
+  } else if (origin.target.className == 'task-list completed selected') {
+    origin.target.className = 'task-list selected';
+  } else if (origin.target.className == 'task-list completed') {
     origin.target.className = 'task-list';
-  } 
+  }
 }
 
 function taskListLoop() {
   const listTask = document.querySelectorAll('.task-list');
   for (let index = 0; index < listTask.length; index += 1) {
     listTask[index].addEventListener('click', changeSelected);
+    listTask[index].addEventListener('dblclick', completedTask)
   }
 }
 
@@ -52,9 +85,3 @@ function createTask() {
 }
 
 buttonTask.addEventListener('click', createTask);
-
-
-function colorList() {
-  
-}
-
