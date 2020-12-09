ac7d00ff9aae4f26b1b6e1a27d00d5f6b789b017
Luiz Henrique <lhfmworker@gmail.com>
2020-12-09 18:08:11 -0300

    entrega requisito 5

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var taskTxt = document.querySelector('#texto-tarefa');
-var createTask = document.querySelector('#criar-tarefa');
+var taskTxt = document.getElementById('texto-tarefa');
+var btnTask = document.getElementById('criar-tarefa');
+var list = document.getElementById('lista-tarefas');
+
+//codigo abaixo gera novo item na lista
+btnTask.addEventListener('click', addList);
+function addList () {
+    var clipboard = taskTxt.value;
+    var itemList = document.createElement('li');
+    itemList.innerText = clipboard;
+    list.appendChild(itemList);
+    taskTxt.value ='';
+}
