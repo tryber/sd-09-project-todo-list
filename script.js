1e6636640a28abd5db8c503b0feefaa4abeb370f
Tiago Yoneda <xyonedax@gmail.com>
2020-12-09 16:04:27 -0300

    arrumando o climate v1.0

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function criaTarefa(){
-    const newTexto = document.querySelector('#texto-tarefa').value;
-    if (!newTexto) {
-        alert('Por favor não deixar em branco')
-    } else {
-        const tarefa = document.createElement('li');
-        tarefa.innerText = newTexto;
-        document.querySelector('ol').appendChild(tarefa);
-        document.querySelector('#texto-tarefa').value = '';
-    }
+function criaTarefa() {
+  const newTexto = document.querySelector('#texto-tarefa').value;
+  if (!newTexto) {
+    alert('Por favor não deixar em branco');
+  } else {
+    const tarefa = document.createElement('li');
+    tarefa.innerText = newTexto;
+    document.querySelector('ol').appendChild(tarefa);
+    document.querySelector('#texto-tarefa').value = '';
+  }
 }
 
-document.querySelector('#criar-tarefa').addEventListener('click',criaTarefa);
+document.querySelector('#criar-tarefa').addEventListener('click', criaTarefa);
