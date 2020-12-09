fbe66280915a17dcbb0c1c9d678a9aeb1e7de975
Felipe Cardoso <felipedclc@gmail.com>
2020-12-09 17:57:10 -0300

    code climate - let/const

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 function createInput() {
-  let input = document.createElement('input');
+  const input = document.createElement('input');
   input.id = 'texto-tarefa';
   input.type = 'text';
   input.placeholder = 'digite o item';
 
-  let section1 = document.querySelector('#section1');
+  const section1 = document.querySelector('#section1');
   section1.appendChild(input);
 }
 createInput();
 
 function createTaskList() {
-  let taskList = document.querySelector('#task-list');
-  let ol = document.createElement('ol');
+  const taskList = document.querySelector('#task-list');
+  const ol = document.createElement('ol');
   ol.id = 'lista-tarefas';
   taskList.appendChild(ol);
 }
 createTaskList();
 
 function addButtonItem() {
-  let btn = document.createElement('button');
+  const btn = document.createElement('button');
   btn.id = 'criar-tarefa';
   btn.type = 'submit';
   btn.innerText = 'Criar tarefa';
   btn.style.marginLeft = '5px';
   btn.style.padding = '3px';
-  let section1 = document.querySelector('#section1');
+  const section1 = document.querySelector('#section1');
   section1.appendChild(btn);
 
   btn.addEventListener('click', function (e) {
     e.preventDefault();
-    let input = document.querySelector('#texto-tarefa');
-    let taskList = document.querySelector('#lista-tarefas');
+    const input = document.querySelector('#texto-tarefa');
+    const taskList = document.querySelector('#lista-tarefas');
 
     if (input.value.length > 0) {
-      let newLi = document.createElement('li');
+      const newLi = document.createElement('li');
       newLi.innerText = input.value;
       taskList.appendChild(newLi);
 
@@ -44,7 +44,7 @@ function addButtonItem() {
 addButtonItem();
 
 function ChangeColorElementsLi() {
-  let taskList = document.querySelector('#lista-tarefas');
+  const taskList = document.querySelector('#lista-tarefas');
 
   taskList.addEventListener('click', function (e) {
     let currentSelected = document.querySelector('.selected');
@@ -57,3 +57,4 @@ function ChangeColorElementsLi() {
   });
 }
 ChangeColorElementsLi();
+
