8e11082a35560a1912aa447910f296135979d657
ggaldino95 <ggaldino95@gmail.com>
2020-12-09 15:11:55 -0300

    [Task 3] Completed - Added a text box to enter a new task

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-window.onload = function () {
+function generateInput() {
+  const container = document.getElementById('main-content');
+  const newInput = document.createElement('input');
+  newInput.type = 'text';
+  newInput.id = 'texto-tarefa';
+  newInput.placeholder = 'Type a new task here.';
 
+  container.appendChild(newInput);
+}
+
+window.onload = function () {
+  generateInput();
 };
