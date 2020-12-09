eb3dce84df04e809c1bdaba56d3ab53d130550b4
lcgoandete <lcgoandete@hotmail.com>
2020-12-09 16:32:49 -0300

    tarefa - 7 🚀

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function createTask() {
   });
 }
 createTask();
+
+function changeTaskColor() {
+  const taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('click', (event) => {
+    for (let index = 0; index < taskList.childElementCount; index += 1) {
+      taskList.children[index].style.backgroundColor = 'white';
+    }
+    const task = event.target;
+    task.style.backgroundColor = 'rgb(128 , 128 , 128)';
+  });
+}
+changeTaskColor();
