bb0a8bca2a77bbaf577c56b513000fa82b5fed82
Luiz Henrique <lhfmworker@gmail.com>
2020-12-09 18:14:12 -0300

    code climate changes1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var taskTxt = document.getElementById('texto-tarefa');
-var btnTask = document.getElementById('criar-tarefa');
-var list = document.getElementById('lista-tarefas');
+let taskTxt = document.getElementById('texto-tarefa');
+let btnTask = document.getElementById('criar-tarefa');
+let list = document.getElementById('lista-tarefas');
 
-//codigo abaixo gera novo item na lista
-btnTask.addEventListener('click', addList);
-function addList () {
-    var clipboard = taskTxt.value;
-    var itemList = document.createElement('li');
-    itemList.innerText = clipboard;
+//  codigo abaixo gera novo item na lista
+function addList() {
+    let clipboard= taskTxt.value;
+    let itemList= document.createElement('li');
+    itemList.innerText= clipboard;
     list.appendChild(itemList);
-    taskTxt.value ='';
+    taskTxt.value='';
 }
+btnTask.addEventListener('click', addList);
