f8814e7024a62545d8eb4262d02a68c19887c121
saulokirchmaier <saulokirchmaier@gmail.com>
2020-12-09 16:47:35 -0300

    7 - trocar a cor de fundo do item clicado.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 window.onload = function() {
-    let todoList = document.getElementById('lista-tarefas');
-    const tasksList = document.getElementById('lista-tarefas');
+  let todoList = document.getElementById('lista-tarefas');
+  const tasksList = document.getElementById('lista-tarefas');
 
-    //Criar lista de tarefas
-    function createNewElementList() {
-        const createTaskButton = document.getElementById('criar-tarefa');
+  //Criar lista de tarefas
+  function createNewElementList() {
+    const createTaskButton = document.getElementById('criar-tarefa');
 
-        createTaskButton.addEventListener('click', function() {
-            const taskTextInput = document.getElementById('texto-tarefa');
-            const newTask = document.createElement('li');
-            newTask.innerText = taskTextInput.value;
-            tasksList.appendChild(newTask);
-            taskTextInput.value = '';
-        });
-    }
+    createTaskButton.addEventListener('click', function() {
+      const taskTextInput = document.getElementById('texto-tarefa');
+      const newTask = document.createElement('li');
+      newTask.innerText = taskTextInput.value;
+      tasksList.appendChild(newTask);
+      taskTextInput.value = '';
+    });
+  }
 
-    createNewElementList();
+  function changeBackgroundColorTaskItem() {
+    tasksList.addEventListener('click', function(event) {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+
+  createNewElementList();
+  changeBackgroundColorTaskItem();
 
 }
