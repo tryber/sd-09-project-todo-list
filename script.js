3a518bd5810650a0a709f0dfd3bf1a6b35aaf25d
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2020-12-09 17:52:06 -0300

    resolvendo o code climate

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-const botao = document.querySelector('#criar-tarefa').addEventListener('click', botaoCriarTarefa);
 const getList = document.getElementById('lista-tarefas');
 const getTitle = document.getElementById('title');
 let indexList = 0;
-let resolverErroSelected = document.createElement('div');
+const resolverErroSelected = document.createElement('div');
 resolverErroSelected.className = 'selected';
 getTitle.appendChild(resolverErroSelected);
-
-function botaoCriarTarefa() {
-  const getInputValue = document.getElementById('texto-tarefa').value;
-  const newList = document.createElement('li');
-  getList.appendChild(newList);
-  newList.innerText = getInputValue;
-  document.getElementById('texto-tarefa').value = '';
-  //listChildrens = getList.children;
-  listReceiveEvent();
-  //listChildrens[indexList].addEventListener('click', coloringBackgroundListItens);
-  //indexList += 1;
-}
-
-function listReceiveEvent() {
-  listChildrens = getList.children;
-  listChildrens[indexList].addEventListener('click', coloringBackgroundListItens);
-  indexList += 1;
-}
 
 function coloringBackgroundListItens(event) {
   let actualColoredItem = event.target;
@@ -31,9 +12,21 @@ function coloringBackgroundListItens(event) {
   selectedItem.classList.remove('selected');
   actualColoredItem.classList.add('selected');
   actualColoredItem.style.backgroundColor = 'rgb(128 , 128 , 128)';
-
 }
 
+function listReceiveEvent() {
+  let listChildrens = getList.children;
+  listChildrens[indexList].addEventListener('click', coloringBackgroundListItens);
+  indexList += 1;
+}
 
+function botaoCriarTarefa() {
+  const getInputValue = document.getElementById('texto-tarefa').value;
+  const newList = document.createElement('li');
+  getList.appendChild(newList);
+  newList.innerText = getInputValue;
+  document.getElementById('texto-tarefa').value = '';
+  listReceiveEvent();
+}
 
-
+const botao = document.querySelector('#criar-tarefa').addEventListener('click', botaoCriarTarefa);
