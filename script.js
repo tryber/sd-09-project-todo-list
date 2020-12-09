e419fcf51114bdb76939c2f847d0bea04899d99d
Rafael Pufal <rafael.pufal23@gmail.com>
2020-12-09 18:31:44 -0300

    modificando requisito 7,8 e 9

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function addTask () {
     taskItem.innerText = inputText.value;
     taskList.appendChild(taskItem);
     taskItem.addEventListener('click', selectItem);
+    taskItem.addEventListener('dblclick', completeItem);
 }
 
 function selectItem(chosenItem) {
@@ -15,11 +16,26 @@ function selectItem(chosenItem) {
         const previousSelected = document.querySelector('.selected');
         previousSelected.classList.remove('selected');
         previousSelected.style.backgroundColor = null;
-        itemSelected.style.backgroundColor = 'rgb(128,128,128)';
-        itemSelected.className += 'selected'
+        // itemSelected.style.backgroundColor = 'rgb(128,128,128)';
+        itemSelected.classList.add('selected');
     }
     else{
-        itemSelected.style.backgroundColor = 'rgb(128,128,128)';
-        itemSelected.className +='selected'
+        // itemSelected.style.backgroundColor = 'rgb(128,128,128)';
+        itemSelected.classList.add('selected');
+    }
+}
+
+function completeItem(completedItem){
+    const completedTask = completedItem.target;
+    if (completedTask.classList.contains('completed')){
+        // const previousCompleted = document.querySelector('.completed');
+        completedTask.classList.remove('completed');
+        // completedTask.style.textDecoration = null;
+        // completedTask.classList.add('completed');
+        // completedTask.style.textDecoration = 'line-through solid rgb(0,0,0)';
+    }
+    else {
+        completedTask.classList.add('completed');
+        // completedTask.style.textDecoration = 'line-through solid rgb(0,0,0)';
     }
 }
