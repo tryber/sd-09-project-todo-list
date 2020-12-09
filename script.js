40b2f00b8087fc6915835dc5ffed6e4c80610b30
Layo Kaminski <layokaminski@gmail.com>
2020-12-09 15:54:19 -0300

    Requisito 5 e possivelmente o requisito 6

--- script.js
+++ script.js
@@ -8,7 +8,50 @@ window.onload = function() {
     }
     titleOperation();
 
-    function addNewTodo() {
-        
+    function createButton() {
+        let button = document.createElement('button')
+        button.id = 'criar-tarefa'
+        button.innerHTML = 'Adicionar Tarefa'
+        document.body.appendChild(button)
     }
+    createButton()
+
+    function createContainer() {
+        let container = document.createElement('div')
+        let button = document.querySelector('#criar-tarefa')
+        let input = document.querySelector('#texto-tarefa')
+        let listOrdened = document.querySelector('#lista-tarefas')
+        container.appendChild(input)
+        container.appendChild(button)
+        container.appendChild(listOrdened)
+        document.body.appendChild(container)
+    }
+    createContainer()
+
+    function addNewTodo() {
+        let button = document.querySelector('#criar-tarefa')
+        let input = document.querySelector('#texto-tarefa')
+        let list = document.querySelector('#lista-tarefas')
+
+        button.addEventListener('click', function() {
+            if (input.value.length > 0) {
+                let newList = document.createElement('li')
+                newList.innerText = input.value
+
+                list.appendChild(newList)
+                input.value = ''
+            }
+        })
+
+        input.addEventListener('keyup', function(event) {
+            if (event.keyCode === 13 && input.value.length > 0) {
+                let newList = document.createElement('li')
+                newList.innerText = input.value
+
+                list.appendChild(newList)
+                input.value = ''
+            }
+        })
+    }
+    addNewTodo()
 }
