a0c4fdd7910b869712cc10c3330a2e1ec339fb9c
saulokirchmaier <saulokirchmaier@gmail.com>
2020-12-09 16:27:09 -0300

    5 - Limpar input.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 window.onload = function() {
     let todoList = document.getElementById('lista-tarefas');
+    const tasksList = document.getElementById('lista-tarefas');
 
     //Criar lista de tarefas
     function createNewElementList() {
         const createTaskButton = document.getElementById('criar-tarefa');
 
         createTaskButton.addEventListener('click', function() {
-            const tasksList = document.getElementById('lista-tarefas');
             const taskTextInput = document.getElementById('texto-tarefa');
             const newTask = document.createElement('li');
             newTask.innerText = taskTextInput.value;
             tasksList.appendChild(newTask);
+            taskTextInput.value = '';
         });
     }
 
