ac2ca1479803a34b0d59c6c8513d66221778a35f
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-09 16:53:59 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const listaOrdenada = document.querySelector('#lista-tarefas');
 const inputBox = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
+const olList = document.querySelector('#lista-tarefas')
 
-function reciveInputText () {
-  button.addEventListener('click', function() {
+function reciveInputText() {
+  button.addEventListener('click', function () {
     let text = inputBox.value;
-    let li = document.createElement('li');
+    const li = document.createElement('li');
     listaOrdenada.appendChild(li);
     li.innerText = text;
     inputBox.value = '';
-  })
+  });
 }
-reciveInputText ();
+reciveInputText();
+
+function changeLiBackgroundColor() {
+olList.addEventListener('click', function (event) {
+event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+})
+}
+changeLiBackgroundColor();
