f4e78f830868f68688d4fe4c2fa06411424a43b9
Johne Alves <johne_af@hotmail.com>
2020-12-09 16:32:08 -0300

    reqiosito 7

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 function newTask() {
-  let list = document.querySelector('#lista-tarefas');
-  let mt = document.querySelector('#texto-tarefa');
-  let newli = document.createElement('li');
+  const list = document.querySelector('#lista-tarefas');
+  const mt = document.querySelector('#texto-tarefa');
+  const newli = document.createElement('li');
+  newli.className = "item-list"
   newli.innerText = mt.value;
-  list.appendChild(newli)
-  mt.value = ''
+  list.appendChild(newli);
+  mt.value = '';
 }
 
 function makeTask() {
-  let button = document.getElementById('criar-tarefa')
-  button.addEventListener('click', newTask)
+  const button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', newTask);
 }
-makeTask()
+makeTask();
+
+function backgroundItemlist() {
+  let fullList = document.querySelector('#lista-tarefas');
+  fullList.addEventListener('click', function(event) {
+    let listOfItem = document.querySelectorAll('.item-list');
+    if (event.target === fullList){
+        return
+    }
+    
+    for (let index = 0; index < listOfItem.length; index += 1) {
+        listOfItem[index].className = 'item-list';
+        listOfItem[index].style.background = 'none'
+    }
+    event.target.style.background = "rgb(128,128,128)";
+    event.target.className += " selected";
+  })
+}
+backgroundItemlist()
