1e10a690457138f3068f04b6af216d46825affc2
BernardoFontes <bfontes.1997@gmail.com>
2020-12-09 19:14:14 -0300

    adicionando primeira funcao novamente

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let botao = document.getElementById("lista-tarefas");
+let lista = document.getElementById("lista-tarefas");
+
+function addItem (){
+    let botao = document.getElementById('criar-tarefa');
+    let lista2 = document.querySelector('#lista-tarefas');
+    function recebeItem(){
+        let item = document.querySelector('#texto-tarefa').value;
+        let li = document.createElement('li');
+        li.innerHTML = item;
+        li.className= 'tarefa';
+        lista2.appendChild(li);
+        clearInputValue();
+    }
+    botao.addEventListener('click', recebeItem);
+}
