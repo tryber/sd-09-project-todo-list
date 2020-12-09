49591df8389a3ec002fc7b07b362754d229a4688
LucioOSilva <lucioosilva00@gmail.com>
2020-12-09 19:47:08 -0300

    adjusts on part five

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 // 3- add <input>
 function insertForm() {
   const elementHolder = document.querySelector('.input-session')
-  let newForm = document.createElement('form');
-  let newLabel = document.createElement('label');
-  let newInput = document.createElement('input');
-  let newButton = document.createElement('button');
+  const newForm = document.createElement('form');
+  const newLabel = document.createElement('label');
+  const newInput = document.createElement('input');
+  const newButton = document.createElement('button');
   newForm.id = 'input-bar';
   newLabel.for = 'data-inserted';
-  newInput.id = 'texto-tarefa'
-  newInput.name = 'tarefa'
+  newInput.id = 'texto-tarefa';
+  newInput.name = 'tarefa';
   newInput.type = 'text';
   newButton.innerText = 'Adicionar tarefa';
   newButton.id = 'criar-tarefa';
@@ -19,37 +19,37 @@ function insertForm() {
   newForm.appendChild(newButton);
   elementHolder.appendChild(newForm);
 }
-insertForm()
+insertForm();
 
 // 4- inserir Lista Ordenada
-function insertTaskOrderedList () {
-    const elementHolder = document.querySelector('.tasks');
-    let newOl = document.createElement('ol');
-    newOl.id = 'lista-tarefas';
-    elementHolder.appendChild(newOl);
+function insertTaskOrderedList() {
+  const elementHolder = document.querySelector('.tasks');
+  const newOl = document.createElement('ol');
+  newOl.id = 'lista-tarefas';
+  elementHolder.appendChild(newOl);
 }
 insertTaskOrderedList();
 
 // 5-
 function clickAddTask() {
-    const form = document.getElementById('input-bar');
-    const taskList = document.querySelector('#lista-tarefas');
-    form.addEventListener('submit', function (event) {
-        let taskItem = document.createElement('li');
-        const taskRead = event.target.tarefa.value;
-        taskItem.innerText = taskRead;
-        taskList.appendChild(taskItem);
-        sessionStorage.setItem(sessionStorage.length, taskRead);
-    });
+  const form = document.getElementById('input-bar');
+  const taskList = document.querySelector('#lista-tarefas');
+  form.addEventListener('submit', function (event) {
+    let taskItem = document.createElement('li');
+    const taskRead = event.target.tarefa.value;
+    taskItem.innerText = taskRead;
+    taskList.appendChild(taskItem);
+    sessionStorage.setItem(sessionStorage.length, taskRead);
+  });
 }
-clickAddTask()
+clickAddTask();
 
 // busca e povoa a OL com itens do session storage (window-load)
 window.addEventListener('load', function () {
-    const taskList = document.querySelector('#lista-tarefas');
-    for (let index = 1; index < sessionStorage.length; index += 1){
-        let taskItem = document.createElement('li');
-        taskItem.innerText = sessionStorage.getItem(index);
-        taskList.appendChild(taskItem);
-    }
+  const taskList = document.querySelector('#lista-tarefas');
+  for (let index = 0; index < sessionStorage.length; index += 1){
+    let taskItem = document.createElement('li');
+    taskItem.innerText = sessionStorage.getItem(index);
+    taskList.appendChild(taskItem);
+  }
 });
