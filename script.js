a418016b27abc13dbf8b2e7dc3ee8c4d43eebfd6
Luiz Henrique <lhfmworker@gmail.com>
2020-12-09 18:41:33 -0300

    add graysize function plus codeclimate fixes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let taskTxt = document.getElementById('texto-tarefa');
 let btnTask = document.getElementById('criar-tarefa');
 let list = document.getElementById('lista-tarefas');
+let items = document.getElementsByTagName('li');
 
 //  codigo abaixo gera novo item na lista
 function addList() {
@@ -8,6 +9,16 @@ function addList() {
   let itemList = document.createElement('li');
   itemList.innerText = clipboard;
   list.appendChild(itemList);
-  taskTxt.value ='';
+  taskTxt.value = '';
+}
+//  codigo para tornar o item da lista cinza quando clicado
+function graySize() {
+  let sltdLi = event.target;
+  for (i in items) {
+    items[i].className = '';
+  }
+  console.log(items);
+  sltdLi.classList = 'selected';
 }
 btnTask.addEventListener('click', addList);
+list.addEventListener('click',graySize);
