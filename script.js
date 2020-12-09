c6d3994eaa0e2da7cd9b129a0bf8ea92503da85b
Johne Alves <johne_af@hotmail.com>
2020-12-09 17:45:47 -0300

    requisito 9

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function newTask() {
   const list = document.querySelector('#lista-tarefas');
   const mt = document.querySelector('#texto-tarefa');
   const newli = document.createElement('li');
-  newli.className = "item-list"
+  newli.className = 'item-list';
   newli.innerText = mt.value;
   list.appendChild(newli);
   mt.value = '';
@@ -15,19 +15,21 @@ function makeTask() {
 makeTask();
 
 function backgroundItemlist() {
-  let fullList = document.querySelector('#lista-tarefas');
-  fullList.addEventListener('click', function(event) {
-    let listOfItem = document.querySelectorAll('.item-list');
-    if (event.target === fullList){
-        return
+  const fullList = document.querySelector('#lista-tarefas');
+  fullList.addEventListener('click', function (event) {
+    const listOfItem = document.querySelectorAll('.item-list');
+    if (event.target === fullList) {
+      return;
     }
-    
     for (let index = 0; index < listOfItem.length; index += 1) {
-        listOfItem[index].className = 'item-list';
-        listOfItem[index].style.background = 'none'
+      listOfItem[index].classList.remove('selected');
+      listOfItem[index].style.background = 'none';
     }
-    event.target.style.background = "rgb(128,128,128)";
-    event.target.className += " selected";
-  })
+    event.target.style.background = 'rgb(128,128,128)';
+    event.target.className += ' selected';
+  });
+  fullList.addEventListener('dblclick', function (event) {
+    event.target.classList.toggle('completed')
+  }) 
 }
-backgroundItemlist()
+backgroundItemlist();
