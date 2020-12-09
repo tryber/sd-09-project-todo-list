8f81f1c6fcbcc3c9b2b0e415a314fcd62369e7b2
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-09 16:31:54 -0300

    adiciona requisito 5

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const listaOrdenada = document.querySelector('#lista-tarefas');
+const inputBox = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
 
+function reciveInputText () {
+  button.addEventListener('click', function() {
+    let text = inputBox.value;
+    let li = document.createElement('li');
+    listaOrdenada.appendChild(li);
+    li.innerText = text;
+    inputBox.value = '';
+  })
+}
+reciveInputText ();
