7bf6f7791909add1926aa7678c8d2ecddd3c1203
Felipe Cardoso <felipedclc@gmail.com>
2020-12-09 15:49:28 -0300

    req 5, add elementos a lsita atraves do button

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ function createTaskList() {
   taskList.appendChild(ol);
 }
 createTaskList();
+
+function addButtonItem() {
+    let btn = document.createElement('button');
+    btn.id = 'criar-tarefa';
+    btn.type = 'submit';
+    btn.innerText = 'Criar tarefa';
+    btn.style.marginLeft = '5px';
+    btn.style.padding = '3px'
+    let section1 = document.querySelector('#section1');
+    section1.appendChild(btn);
+
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        let input = document.querySelector('#texto-tarefa');
+        let taskList = document.querySelector('#lista-tarefas');
+        
+        if (input.value.length > 0) {
+            let newLi = document.createElement('li');
+            newLi.innerText = input.value;
+            taskList.appendChild(newLi);
+            
+            input.value = '';
+        }
+    });
+}
+addButtonItem();
