378de97eb38870d006f201fd85db633b049b7a6e
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2020-12-09 17:46:27 -0300

    requisito 8 completo

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 const botao = document.querySelector('#criar-tarefa').addEventListener('click', botaoCriarTarefa);
+const getList = document.getElementById('lista-tarefas');
+const getTitle = document.getElementById('title');
 let indexList = 0;
+let resolverErroSelected = document.createElement('div');
+resolverErroSelected.className = 'selected';
+getTitle.appendChild(resolverErroSelected);
 
 function botaoCriarTarefa() {
-  const getList = document.getElementById('lista-tarefas');
   const getInputValue = document.getElementById('texto-tarefa').value;
   const newList = document.createElement('li');
   getList.appendChild(newList);
   newList.innerText = getInputValue;
   document.getElementById('texto-tarefa').value = '';
+  //listChildrens = getList.children;
+  listReceiveEvent();
+  //listChildrens[indexList].addEventListener('click', coloringBackgroundListItens);
+  //indexList += 1;
+}
+
+function listReceiveEvent() {
   listChildrens = getList.children;
   listChildrens[indexList].addEventListener('click', coloringBackgroundListItens);
   indexList += 1;
 }
 
 function coloringBackgroundListItens(event) {
-  let actualColoredItem = event.target.style.backgroundColor = 'rgb(128 , 128 , 128)';
+  let actualColoredItem = event.target;
+  const selectedItem = document.querySelector('.selected');
+  selectedItem.style.backgroundColor = 'white';
+  selectedItem.classList.remove('selected');
+  actualColoredItem.classList.add('selected');
+  actualColoredItem.style.backgroundColor = 'rgb(128 , 128 , 128)';
+
 }
 
 
