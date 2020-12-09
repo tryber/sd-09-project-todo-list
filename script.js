40cc8c7867626331cd572b7d4fe0c8ca0fead201
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2020-12-09 16:13:31 -0300

    requisito 6 e 7 completos

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const botao = document.querySelector('#criar-tarefa');
-botao.addEventListener('click', botaoCriarTarefa);
+const botao = document.querySelector('#criar-tarefa').addEventListener('click', botaoCriarTarefa);
+let indexList = 0;
 
 function botaoCriarTarefa() {
   const getList = document.getElementById('lista-tarefas');
-  let getInputValue = document.getElementById('texto-tarefa').value;
+  const getInputValue = document.getElementById('texto-tarefa').value;
   const newList = document.createElement('li');
   getList.appendChild(newList);
   newList.innerText = getInputValue;
-  document.getElementById('texto-tarefa').value = ''
+  document.getElementById('texto-tarefa').value = '';
+  listChildrens = getList.children;
+  listChildrens[indexList].addEventListener('click', coloringBackgroundListItens);
+  indexList += 1;
 }
+
+function coloringBackgroundListItens(event) {
+  let actualColoredItem = event.target.style.backgroundColor = 'rgb(128 , 128 , 128)';
+}
+
+
+
+
