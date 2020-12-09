505dd934237e6e5ceb2863594e3273f71d74918e
LucioOSilva <lucioosilva00@gmail.com>
2020-12-09 18:17:51 -0300

    added task3

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // 1- adiciona <header> e conteudo "Minha Lista de Tarefas"
 // 2- adiciona <p.funcionamnto> com conteuro "Clique duas..."
+// 3- add <input>
+function insertForm() {
+  const elementHolder = document.querySelector('.input-session')
+  let newForm = document.createElement('form');
+  let newLabel = document.createElement('label');
+  let newInput = document.createElement('input');
+  let newButton = document.createElement('button');
+  newForm.id = 'input-bar';
+  newLabel.for = 'data-inserted';
+  newInput.id = 'texto-tarefa'
+  newInput.type = 'text';
+  newButton.innerText = 'Adicionar tarefa';
+  newButton.id = 'criar-tarefa';
+  newForm.appendChild(newLabel);
+  newForm.appendChild(newInput);
+  newForm.appendChild(newButton);
+  elementHolder.appendChild(newForm);
+}
+insertForm()
