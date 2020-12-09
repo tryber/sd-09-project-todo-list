ac0c3705c2fea3a4d13add68b343d88ae04e241c
Caue Lopes <kitoypp@gmail.com>
2020-12-09 16:37:13 -0300

    exercicio 5 e 6

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let getButton = document.querySelector('#criar-tarefa');
 
 function addElement() {
     let acessList = document.querySelector('#texto-tarefa');
-    let elementCreate = document.createElement('li')
-    let acessOl = document.querySelector('#lista-tarefas')
+    let elementCreate = document.createElement('li');
+    let acessOl = document.querySelector('#lista-tarefas');
     elementCreate.innerText = acessList.value
-    acessOl.appendChild(elementCreate)
+    acessOl.appendChild(elementCreate);
+    acessList.value = '';
 }
 
     getButton.addEventListener('click', addElement)
