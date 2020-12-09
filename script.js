cd3984403180f6e0a44ba108dc2d190a84b1ffa8
ederson rodrigo <edersonrodrigo31@gmail.com>
2020-12-09 15:30:27 -0300

    setimo requisito

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ window.onload = function (){
   let button = document.querySelector('#criar-tarefa')
   let input = document.querySelector('#texto-tarefa')
 
-
-
   button.addEventListener('click', addnewassignment)
 
 
@@ -13,8 +11,21 @@ window.onload = function (){
 
     let newelement = document.createElement("li")
     newelement.innerText = content
+    newelement.className = 'assignment'
 
     input.value = ""
     list.appendChild(newelement)
+  }
+}
+
+
+let itemlist = document.querySelectorAll('.assignment')
+
+addEventListener('click', changebackgroundassignment)
+
+
+function changebackgroundassignment(event){
+  if(event.target.classList.contains('assignment')){
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   }
 }
