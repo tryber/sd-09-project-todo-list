378041b2fd36a673ced96f0be0af81361d19c7af
Henrique Brito Elias <henriquebelias@gmail.com>
2020-12-09 17:38:02 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const listaTarefas = document.querySelector('#lista-tarefas');
 const criarTarefa = document.querySelector('#criar-tarefa');
 const textoTarefa = document.querySelector('#texto-tarefa');
+const apagaTudo = document.querySelector('#apaga-tudo');
 
 function addTask() {
-  criarTarefa.addEventListener ('click', function () {
+  criarTarefa.addEventListener('click', function () {
     const itemTarefa = document.createElement('li');
     itemTarefa.innerHTML = textoTarefa.value;
     listaTarefas.appendChild(itemTarefa);
@@ -13,15 +14,25 @@ function addTask() {
 addTask();
 
 function selectItem() {
-  listaTarefas.addEventListener ('click', function(event) {
+  listaTarefas.addEventListener('click', function (event) {
     event.target.classList.toggle('selected');
   });
 }
 selectItem();
 
 function completeItem() {
-  listaTarefas.addEventListener ('dblclick', function(event) {
+  listaTarefas.addEventListener('dblclick', function (event) {
     event.target.classList.toggle('completed');
   });
 }
 completeItem();
+
+function deleteAll() {
+  apagaTudo.addEventListener('click', function () {
+    const item = document.querySelectorAll('li');
+    for (let index = 0; index < item.length; index += 1) {
+      item[index].remove();
+    }
+  });
+}
+deleteAll();
