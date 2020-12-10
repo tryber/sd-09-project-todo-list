b8f2788dfba91b56cd0c5efe03233c8d287e8b2f
LucioOSilva <lucioosilva00@gmail.com>
2020-12-10 02:43:35 -0300

    refactored task9

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
+// 9- click change bgcolor to gray
+function changeBGSelectedItems(elementHtml) {
+  const taskLiItems = document.querySelector('#lista-tarefas').children;
+  elementHtml.addEventListener('click', function (eventT) {
+  for (let index = 0; index < taskLiItems.length; index += 1) {
+    taskLiItems[index].style.backgroundColor = '';
+  }
+  eventT.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+}
+
+// 9- click apply/remove text-decoration
+
+function markupTasks(elementHtml) {
+  elementHtml.addEventListener('dblclick', function (event) {    
+    if (event.target.className === '') {
+      event.target.className = 'completed';
+    } else {
+      event.target.className = '';
+    } 
+  });
+}
+
 // 5
+
 function clickAddTask() {
   const form = document.getElementById('input-bar');
   const taskOlList = document.querySelector('#lista-tarefas');
-  const taskLiItems = taskOlList.children;
   form.addEventListener('submit', function (event) {
     event.preventDefault();
-    let taskItem = document.createElement('li');
+    const taskItem = document.createElement('li');
     const taskRead = event.target.tarefa.value;
     taskItem.innerText = taskRead;
-    taskItem.addEventListener('click', function (eventT) {
-      for (let index = 0; index < taskLiItems.length; index += 1) {
-        taskLiItems[index].style.backgroundColor = '';
-      }
-      eventT.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    })
+    if (taskRead === '') {
+      return;
+    }
     taskOlList.appendChild(taskItem);
     event.target.tarefa.value = '';
+    changeBGSelectedItems(taskItem); //add the eventlistener click change bgcolor to gray
+    markupTasks(taskItem); //add eventlistener click apply/remove text-decoration
   });
 }
 clickAddTask();
