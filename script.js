b4aea4d0d4e82a5f1cf1ff2f0855fbc02dcedb29
Nathália <nathaliaveneziano@icloud.com>
2020-12-10 20:23:21 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// Remove Background dos Itens
+function removeBackgroundItems(arrayTasks) {
+  for (let task of arrayTasks) {
+    if (task.style.backgroundColor !== '') {
+      task.removeAttribute('style');
+    }
+  }
+}
+
 // Botão Adicionar
-document.querySelector('#criar-tarefa').addEventListener('click', function (event) {
-  let tarefa = document.querySelector('#texto-tarefa');
+document.querySelector('#criar-tarefa').addEventListener('click', function () {
+  const tarefa = document.querySelector('#texto-tarefa');
 
   const elementHTML = document.createElement('li');
   elementHTML.innerText = tarefa.value;
@@ -9,4 +18,15 @@ document.querySelector('#criar-tarefa').addEventListener('click', function (even
   tarefa.value = '';
   tarefa.focus();
   // return elementHTML;
+});
+
+// Clique Tarefa
+document.querySelector('#lista-tarefas').addEventListener('click', function (event) {
+  const listaTarefas = event.target;
+  if (listaTarefas.id === '') {
+    const itensTarefa = listaTarefas.parentNode.children;
+    // console.log(listaTarefas);
+    removeBackgroundItems(itensTarefa);
+    listaTarefas.style.backgroundColor = 'rgb(128,128,128)';
+  }
 });
