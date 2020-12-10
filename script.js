754f6c289a14877294c76605b9ce223188a1b680
Enio Nicael <enionicael@gmail.com>
2020-12-10 17:56:16 -0300

    Feature: requisito 5 adicionar botao que adiciona uma nova tarefa na lista.

--- script.js
+++ script.js
@@ -19,23 +19,52 @@ function addParagraph() {
   container.appendChild(createParagraph);
 }
 
+function addContainerTask() {
+  const containerTask = document.createElement('div');
+  containerTask.id = 'container-task';
+  container.appendChild(containerTask);
+}
+
 function addInput() {
+  const containerTask = document.getElementById('container-task');
   const createInput = document.createElement('input');
   createInput.id = 'texto-tarefa';
   createInput.setAttribute('placeholder', 'Task');
-  container.appendChild(createInput);
+  containerTask.appendChild(createInput);
 }
 
 function addList() {
   const createList = document.createElement('ol');
   createList.id = 'lista-tarefas';
-  container.appendChild(createList)
+  container.appendChild(createList);
+}
+
+function handleAddListItem() {
+  const createListItem = document.createElement('li');
+  const inputText = document.getElementById('texto-tarefa');
+  const list = document.getElementById('lista-tarefas');
+  console.log(list);
+  const task = inputText.value;
+  createListItem.innerText = task;
+  list.appendChild(createListItem);
+  inputText.value = '';
+}
+
+function addButton() {
+  const containerTask = document.getElementById('container-task');
+  const createButton = document.createElement('button');
+  createButton.id = 'criar-tarefa';
+  createButton.innerText = 'Adicionar';
+  createButton.addEventListener('click', handleAddListItem);
+  containerTask.appendChild(createButton);
 }
 
 window.onload = function () {
   addHeader();
   addTitle();
   addParagraph();
+  addContainerTask();
   addInput();
   addList();
+  addButton();
 };
