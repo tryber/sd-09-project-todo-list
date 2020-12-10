09655d26fe2d65ff4a2243458dc8e9efb2f973cc
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-10 19:24:29 -0300

    Alterna selected ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnAdd = document.querySelector('#criar-tarefa');
 const olList = document.getElementById('lista-tarefas');
-
+let selected = false;
 
 function createLi() {
   const liList = document.createElement('li');
@@ -10,12 +10,22 @@ function createLi() {
   inputAdd.value = '';
 }
 
-function itemChangeBgColor(event) {
-  const item = event.target;
-  item.style.backgroundColor = 'rgb(128, 128, 128)';
+function selectItem(event) {
+  let item = event.target;
+  item.style.backgroundColor = 'rgb(128 , 128 , 128)';
+  if (selected === true) {
+    let selectedItem = document.querySelector('.selected');
+    // let selectedItem = document.getElementsByClassName('selected');
+    selectedItem.className = '';
+    selectedItem.style.backgroundColor = 'white';
+    console.log(typeof(selectedItem))
+  }
+  selected = true;
+  item.className = 'selected';
+  console.log(item);
 }
 
 window.onload = function() {
   btnAdd.addEventListener('click', createLi);
-  olList.addEventListener('dblclick', itemChangeBgColor);
+  olList.addEventListener('click', selectItem);
 }
