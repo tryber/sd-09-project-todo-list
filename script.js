4a9b048ddeca899256266425f1c3432069a5d324
Renato Feitosa <pythaqua@gmail.com>
2020-12-09 23:35:56 -0300

    -js- [global] add createNewElement/setPropertiesToNewElement funcs

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ const globalElements = {
   newTaskInput: document.querySelector('#texto-tarefa'),
 }
 
+function setPropertiesToNewElement(element, propertiesObject) {
+  const propertiesKeys = Object.keys(propertiesObject);
+  for (let index = 0; index < propertiesKeys.length; index += 1) {
+    const key = propertiesKeys[index];
+    element[key] = propertiesObject[key];
+  }
+}
+
+function createNewElement(tag, propertiesObject) {
+  const newElement = document.createElement(tag);
+  setPropertiesToNewElement(newElement, propertiesObject);
+  return newElement;
+}
+
+// function createNewTask() {
+//   const newTaskText = globalElements.newTaskInput.value;
+//   // newTaskText.de;
+// }
+
 function setCreateTaskEvent() {
-  globalElements.createTaskButton.addEventListener('click', createTask);
+  // globalElements.createTaskButton.addEventListener('click', createNewTask);
 }
 
 function setAllEvents() {
