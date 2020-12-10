5e8c4fc1f133a093650c6299e3c384d576d0e4a3
Pedro Henrique <pedro.876.ph@gmail.com>
2020-12-10 18:51:57 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const btnCreateLi = document.querySelector('#criar-tarefa');
 const captureOl = document.querySelector('#lista-tarefas');
 const newTaskText = document.querySelector('#texto-tarefa');
-
+const slctTask = document.querySelectorAll('task');
 
 function makeLi(content) {
-    const li = document.createElement('li');
-    li.innerText = content;
-    li.classList.add('task');
-    captureOl.appendChild(li);
+  const li = document.createElement('li');
+  li.innerText = content;
+  li.classList.add('task');
+  captureOl.appendChild(li);
 }
 
 function clickBtn(){
-    const newTaskValue = newTaskText.value;
-    if (newTaskValue) makeLi(newTaskValue);
-    newTaskText.value = '';
+  const newTaskValue = newTaskText.value;
+  if (newTaskValue) makeLi(newTaskValue);
+  newTaskText.value = '';
 }
 
 btnCreateLi.addEventListener('click', clickBtn);
 
+function taskSelector(event) {
+  const selectedClass = document.querySelector('.selected');
+  const targetTask = event.target;
+  if(selectedClass !== null) {
+    selectedClass.classList.remove('selected');
+  }
+  targetTask.classList.add('selected');
+}
+
+function getSelector() {
+ captureOl.addEventListener('click', taskSelector);
+}
+
+getSelector();
