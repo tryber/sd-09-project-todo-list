f0b7214e339c9326b111cbfea288fd753aff80b9
Carine Becker <caarine.m@gmail.com>
2020-12-10 19:31:37 -0300

    fixed code climate issues

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-window.onload = function () {
-  addToDo();
-}
-
 const listElement = document.querySelector('#lista-tarefas');
 const inputElement = document.querySelector('#texto-tarefa');
-
-function addToDo() {
-  const buttonCreateTask = document.querySelector('#criar-tarefa')
-  buttonCreateTask.addEventListener('click', function () {
-    const createTask = document.createElement('li');
-    createTask.innerText = inputElement.value;
-    listElement.appendChild(createTask);
-
-    createTask.addEventListener('dblclick', toggleCompletedTask);
-    createTask.addEventListener('click', colorTask);
-    inputElement.value = '';
-  })
-}
+const buttonCreateTask = document.querySelector('#criar-tarefa');
 
 function colorTask(event) {
   let tasks = document.querySelectorAll('#lista-tarefas li');
   tasks.forEach(task => {
     task.style.backgroundColor = '';
-  });
+  })
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 function toggleCompletedTask(event) {
   event.target.classList.toggle('completed');
 }
+
+function addToDo() {
+  const createTask = document.createElement('li');
+  createTask.innerText = inputElement.value;
+  listElement.appendChild(createTask);
+
+  createTask.addEventListener('dblclick', toggleCompletedTask);
+  createTask.addEventListener('click', colorTask);
+  inputElement.value = '';
+}
+
+buttonCreateTask.addEventListener('click', addToDo);
