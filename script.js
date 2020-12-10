585770ff896df82947fdddbe59f4bad8e4253b4c
Natalia Negreiros <nataliaknegres@gmail.com>
2020-12-10 16:35:32 -0300

    [Style] Retirando margin e padding

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const btnAddTask = document.querySelector('#criar-tarefa');
 
+// Função responsável por limpar o campo de input
+function clearTextInput() {
+  document.querySelector('#texto-tarefa').value = '';
+}
+
+// Função responsável por adicionar uma nova tarefa
 function createTask() {
-  let input = document.querySelector('#texto-tarefa').value;
-  let listTask = document.createElement('li');
+  const input = document.querySelector('#texto-tarefa').value;
+  const listTask = document.createElement('li');
   listTask.innerText = input;
   document.querySelector('#lista-tarefas').appendChild(listTask);
   clearTextInput();
 }
 
-btnAddTask.addEventListener('click', createTask)
-
-function clearTextInput() {
-  document.querySelector('#texto-tarefa').value = '';
-}
+btnAddTask.addEventListener('click', createTask);
