934524f3d5fffc62cfc34022b01b9225db98d704
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-10 17:21:10 -0400

    Requisitos 5: concluido

---
+++ script.js
@@ -0,0 +1,20 @@
+const buttonAddTasks = document.querySelector('#criar-tarefa');
+const listTask = document.querySelector('#lista-tarefas');
+
+function createTask() {
+    const inputTask = document.querySelector('#texto-tarefa');
+    if (inputTask.value === '') {
+        return;
+    };
+    let createLi = document.createElement('li');
+    createLi.innerText = inputTask.value;
+    createLi.className = 'task';
+    listTask.appendChild(createLi);
+    inputTask.value = '';
+}
+
+function buttonAddTaskList() {
+    buttonAddTasks.addEventListener('click', createTask);
+}
+
+buttonAddTaskList();
