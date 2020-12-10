90bcee8cae5f03a64959e573f19237ec71e19656
saulokirchmaier <saulokirchmaier@gmail.com>
2020-12-10 17:13:16 -0300

    Adequacao codeclimate x2

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.onload = function () {
   function addTaskButton() {
     const createTaskButton = document.getElementById('criar-tarefa');
 
-    createTaskButton.addEventListener('click', function() {
+    createTaskButton.addEventListener('click', function () {
       const taskTextInput = document.getElementById('texto-tarefa');
       const textTask = taskTextInput.value;
       if (textTask) createNewElementList(textTask, '');
@@ -24,10 +24,10 @@ window.onload = function () {
 
   // Mudar cor de fundo ao selecionar elemento e selecionar o item
   function selectTaskItem() {
-    tasksList.addEventListener('click', function(event) {
+    tasksList.addEventListener('click', function (event) {
       const lastSelected = document.querySelector('.selected');
-      let taskSelect = event.target;
-      if(lastSelected) {
+      const taskSelect = event.target;
+      if (lastSelected) {
         lastSelected.classList.remove('selected');
         taskSelect.classList.add('selected');
       } else taskSelect.classList.add('selected');
@@ -36,7 +36,7 @@ window.onload = function () {
 
   // Risca task quando clicado 2x
   function taskCompleteCheckAndDescheck() {
-    tasksList.addEventListener('dblclick', function(event) {
+    tasksList.addEventListener('dblclick', function (event) {
       const taskCompleted = event.target;
       if (taskCompleted.classList.contains('completed')) taskCompleted.classList.remove('completed');
       else taskCompleted.classList.add('completed');
@@ -47,11 +47,11 @@ window.onload = function () {
   function clearAllTasks() {
     const claerAllButton = document.querySelector('#apaga-tudo');
 
-    claerAllButton.addEventListener('click', function() {
+    claerAllButton.addEventListener('click', function () {
       const todoList = document.querySelector('#lista-tarefas');
       const allTasks = document.querySelectorAll('.task');
 
-      for (let task of allTasks) {
+      for (const task of allTasks) {
         todoList.removeChild(task);
       }
     });
@@ -61,11 +61,11 @@ window.onload = function () {
   function clearCompletedTaskd() {
     const claerCompletedButton = document.querySelector('#remover-finalizados');
 
-    claerCompletedButton.addEventListener('click', function() {
+    claerCompletedButton.addEventListener('click', function () {
       const todoList = document.querySelector('#lista-tarefas');
       const completedTasks = document.querySelectorAll('.completed');
 
-      for (let task of completedTasks) {
+      for (const task of completedTasks) {
         todoList.removeChild(task);
       }
     });
@@ -76,10 +76,10 @@ window.onload = function () {
   function saveItensTaskLocalStorage() {
     const saveTaskButton = document.querySelector('#salvar-tarefas');
 
-    saveTaskButton.addEventListener('click', function() {
+    saveTaskButton.addEventListener('click', function () {
       const allTasks = document.getElementsByClassName('task');
-      let allTasksArray = [];
-      for (let index = 0; index < allTasks.length; index +=1) {
+      const allTasksArray = [];
+      for (let index = 0; index < allTasks.length; index += 1) {
         const task = allTasks[index].innerText;
         const completed = allTasks[index].classList.contains('completed');
         const nameTask = {
@@ -88,7 +88,7 @@ window.onload = function () {
         };
         allTasksArray[index] = nameTask;
       }
-      let allTasksString = JSON.stringify(allTasksArray);
+      const allTasksString = JSON.stringify(allTasksArray);
       localStorage.setItem('taskList', allTasksString);
     });
   }
@@ -97,10 +97,10 @@ window.onload = function () {
   // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
   function restoreItensTaskToList() {
     let savededLocalStorage = localStorage.getItem('taskList');
-    savededLocalStorage =  JSON.parse(savededLocalStorage);
+    savededLocalStorage = JSON.parse(savededLocalStorage);
 
     if (savededLocalStorage) {
-      for (let item of savededLocalStorage) {
+      for (const item of savededLocalStorage) {
         createNewElementList(item.task, item.completed);
       }
     }
@@ -110,7 +110,7 @@ window.onload = function () {
   function removeSelectedTaskItem() {
     const removeSelectedButton = document.querySelector('#remover-selecionado');
 
-    removeSelectedButton.addEventListener('click', function() {
+    removeSelectedButton.addEventListener('click', function () {
       const selectedTask = document.querySelector('.selected');
       tasksList.removeChild(selectedTask);
     });
@@ -123,8 +123,8 @@ window.onload = function () {
     const upButton = document.querySelector('#mover-cima');
     const allTasks = document.getElementsByClassName('task');
 
-    upButton.addEventListener('click', function() {
-      let selectedTask = document.querySelector('.selected');
+    upButton.addEventListener('click', function () {
+      const selectedTask = document.querySelector('.selected');
       let position;
       for (let index = 0; index < allTasks.length; index += 1) {
         if (allTasks[index] === selectedTask) position = index - 1;
@@ -142,16 +142,15 @@ window.onload = function () {
     const downButton = document.querySelector('#mover-baixo');
     const allTasks = document.getElementsByClassName('task');
 
-    downButton.addEventListener('click', function() {
-      let selectedTask = document.querySelector('.selected');
+    downButton.addEventListener('click', function () {
+      const selectedTask = document.querySelector('.selected');
       let position;
       for (let index = 0; index < allTasks.length; index += 1) {
-        if (allTasks[index] == selectedTask) {
+        if (allTasks[index] === selectedTask) {
           position = index + 1;
-        } 
+        }
       }
       if (selectedTask && selectedTask.nextSibling) {
-        console.log('passou')
         tasksList.insertBefore(selectedTask, allTasks[position].nextSibling);
       }
     });
@@ -167,4 +166,4 @@ window.onload = function () {
   removeSelectedTaskItem();
   upTaskPositionInList();
   downTaskPositionInList();
-}
+};
