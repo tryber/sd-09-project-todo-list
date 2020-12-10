a78c7e3c6fcfee85358b77e4c99f554519243b50
Luciano_Lodi <lrlodi@gmail.com>
2020-12-10 17:42:29 -0300

    Requisitos 7 e 8. E tambem adicionei um alert para o caso de input vazio

--- script.js
+++ script.js
@@ -4,16 +4,30 @@ window.onload = function() {
     button.addEventListener('click', addTaskToList)
      
     function addTaskToList() {
-        let taskToAdd = document.getElementById('texto-tarefa').value
+        let taskToAdd = document.getElementById('texto-tarefa').value;
+        if(document.getElementById('texto-tarefa').value == 0) {
+            return alert('Insira uma tarefa');
+        }
         let taskToList = document.createElement('li');
+        taskToList.addEventListener('click', highlight)
+        taskToList.className = 'item-list';
         taskToList.innerText = taskToAdd;
         list.appendChild(taskToList);
-        clearInput();
+        clearInput();        
     }
     
     function clearInput() {
         if(document.getElementById('texto-tarefa').value !== 0) {
             document.getElementById('texto-tarefa').value = '';
         }
+    }
+    function highlight(event) {
+        let listItemToHighlight = event.target;
+        let previousSelectedItem = document.querySelector('.selected');
+        if(listItemToHighlight.classList.contains('selected') !== true) {
+            listItemToHighlight.classList.add('selected');
+            previousSelectedItem.classList.remove('selected');
+        }
+
     }
 }
