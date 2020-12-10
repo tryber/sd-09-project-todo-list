d6fe6edf880e22ae0e798d939d39770deac86c18
Pablo Fernandes <pabloprates@gmail.com>
2020-12-10 14:11:48 -0300

    Requiremet 6, 7, 8 - Ordered task list creator is done, including ordered list

--- script.js
+++ script.js
@@ -5,13 +5,39 @@ function createTask() {
   const newTaskElement = document.createElement('li');
   if (taskTextTyped === '') {
     alert('Please type some text to describe your task!');
+    setFocusToInputText()
     return null;
   }
+  newTaskElement.classList.add('task-item');
   newTaskElement.innerText = taskTextTyped;
   taskList.appendChild(newTaskElement);
   inputTextOfTask.value = '';
+  setFocusToInputText()
   return 0;
 }
 
+function unselectTaskItem() {
+  const taskItems = document.querySelectorAll('.task-item');
+  for (let index = 0; index < taskItems.length; index += 1) {
+    taskItems[index].classList.remove('selected-task');
+  }
+}
+
+function selectTaskItem(evt) {
+  if (evt.target.className) {
+    unselectTaskItem();
+    evt.target.classList.add('selected-task');
+    setFocusToInputText()
+  }
+}
+
+function setFocusToInputText() {
+  inputText = document.getElementById('texto-tarefa');
+  inputText.focus();
+}
+
 const createTaskButton = document.getElementById('criar-tarefa');
+const taskOrderedList = document.getElementById('lista-tarefas');
+taskOrderedList.addEventListener('click', selectTaskItem);
 createTaskButton.addEventListener('click', createTask);
+
