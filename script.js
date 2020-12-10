08cc9a97d5620d6bf6cc9d7fc9356bb6eeb4393f
Genadi Reuben <genadireuben91@gmail.com>
2020-12-09 22:13:33 -0300

    requisitos 3 e 4

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ createParagraph.id = 'funcionamento';
 createParagraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 document.getElementById('task-header').appendChild(createParagraph);
 
+const createInput = document.createElement('input');
+createInput.id = 'texto-tarefa';
+createInput.placeholder = 'Tarefas';
+document.querySelector('#funcionamento').after(createInput);
+
+const taskList = document.createElement('ol');
+taskList.id = 'lista-tarefas';
+document.querySelector('#task-header').append(taskList);
+
+const taskButtonList = document.createElement('button');
+taskButtonList.id = 'criar-tarefa';
+taskButtonList.innerText = 'Adicionar tarefa'
+document.getElementById('texto-tarefa').after(taskButtonList);
