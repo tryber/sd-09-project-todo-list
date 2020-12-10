2ed2d33e14739a60e8847caa7b37ce16baa95939
rach-vp <picanco.raquel@gmail.com>
2020-12-10 10:56:27 -0400

    Algumas correções do CC

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Altera cor de fundo do item da lista selecionado
+function highlight(event) {
+  const clickedItem = event.target;
+  const selectedItemBackgroundColor = 'rgb(128, 128, 128)';
+  if (clickedItem.style.backgroundColor !== selectedItemBackgroundColor) {
+    clickedItem.style.backgroundColor = selectedItemBackgroundColor;
+  } else {
+    clickedItem.style.backgroundColor = 'transparent';
+  }
+}
+
+function highlightListItemEvent() {
+  const listItems = document.querySelectorAll('li');
+  for (let index = 0; index < listItems.length; index += 1) {
+    listItems[index].removeEventListener('click', highlight);
+    listItems[index].addEventListener('click', highlight);
+  }
+}
+
+highlightListItemEvent();
+
 // Adicionar tarefas a lista ordenada #lista-tarefas
 function addTask() {
   const addButton = document.querySelector('#criar-tarefa');
@@ -13,24 +34,3 @@ function addTask() {
 }
 
 addTask();
-
-// Altera cor de fundo do item da lista selecionado
-function highlight(event) {
-  const clickedItem = event.target;
-  const selectedItemBackgroundColor = 'rgb(128, 128, 128)';
-  if (clickedItem.style.backgroundColor !== selectedItemBackgroundColor) {
-    clickedItem.style.backgroundColor = selectedItemBackgroundColor
-  } else {
-    clickedItem.style.backgroundColor = 'transparent';
-  }
-}
-
-function highlightListItemEvent() {
-  const listItems = document.querySelectorAll('li');
-  for (let index = 0; index < listItems.length; index += 1) {
-    listItems[index].removeEventListener('click', highlight);
-    listItems[index].addEventListener('click', highlight);
-  }
-}
-
-highlightListItemEvent();
