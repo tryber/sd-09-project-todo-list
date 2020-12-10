b1562e38b41b9076b41423ba6ad4b210f3b1de28
Christopher <chris.tryber@gmail.com>
2020-12-10 16:03:31 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 window.onload = function () {
-    const criarTarefas = document.querySelector('#criar-tarefa');
-    criarTarefas.addEventListener('click', function () {
-      const textoTarefas = document.querySelector('#texto-tarefa');
-      const listItem = document.createElement('li');
-      let orderList = document.querySelector('#lista-tarefas');
-      const valorTexto = textoTarefas.value;
-      orderList.appendChild(listItem);
-      listItem.className = 'listItem';
-      listItem.innerText = valorTexto;
-      textoTarefas.value = '';
+  const criarTarefas = document.querySelector('#criar-tarefa');
+  function criouTarefa() {
+    const textoTarefas = document.querySelector('#texto-tarefa');
+    const listItem = document.createElement('li');
+    const orderList = document.querySelector('#lista-tarefas');
+    const valorTexto = textoTarefas.value;
+    if (valorTexto === ''){
+      alert('Nenhum texto identificado. Favor digitar novamente!')
+    }else{
+        orderList.appendChild(listItem);
+        listItem.className = 'listItem';
+        listItem.innerText = valorTexto;
+        textoTarefas.value = '';
+        textoTarefas.focus();
+    } 
+  }
+  criarTarefas.addEventListener('click', criouTarefa);
+
+  let paiDaLi = document.querySelector('#lista-tarefas')
+  paiDaLi.addEventListener('click', function (event) {
+      let itemLista = event.target
+      if (itemLista.style.backgroundColor === ''){
+        itemLista.style.backgroundColor = 'rgb(128, 128, 128)'
+      }else{
+        itemLista.style.backgroundColor = ''
+      }
     })
 }
+
