90ae3f669402397cc0e4f8760d258a0ff327f9c7
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-10 16:48:35 -0300

    feito requisito 6 e 7

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ function btnAdd() {
     li.innerText = input.value;
     listTask.appendChild(li);
     input.value = '';
+    backgroundcolorItem();
   });
+}
+
+function backgroundcolorItem() {
+  const listItem = document.querySelectorAll('li');
+  console.log(listItem);
+  for (let index = 0; index < listItem.length; index += 1) {
+    listItem[index].addEventListener('click', function () {
+      listItem[index].style.backgroundColor = 'rgb(128,128,128)';
+    });
+  }
 }
 
 window.onload = function () {
