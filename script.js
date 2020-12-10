64bd50b2e1905043c13ec62513b35f3e495a2910
rach-vp <picanco.raquel@gmail.com>
2020-12-10 10:38:45 -0400

    Implementa limpeza do input após adicionar tarefa

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-//Adicionar tarefas a lista ordenada #lista-tarefas
+// Adicionar tarefas a lista ordenada #lista-tarefas
 function addTask() {
   const addButton = document.querySelector('#criar-tarefa');
-  addButton.addEventListener('click', function() {
+  addButton.addEventListener('click', function () {
     const task = document.querySelector('#texto-tarefa').value;
     const taskList = document.querySelector('#lista-tarefas');
     const listItem = document.createElement('li');
     listItem.innerText = task;
     taskList.appendChild(listItem);
+    document.querySelector('#texto-tarefa').value = '';
   });
 }
 
