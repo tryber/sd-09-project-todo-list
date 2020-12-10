bb84db71d92d7cc7136defa2fa5bedd1979debf5
Marcelo Campos <marcelo.campos.economista@gmail.com>
2020-12-10 13:38:32 -0300

    tentando consertar o desafio 13

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ function movingDown() {
 
   getDownButton.addEventListener('click', function () {
     const getSelectedTask = document.querySelector('.selected');
-    if (getSelectedTask !== null && getSelectedTask.nextElementSibling !== null) {
+    if (getSelectedTask !== undefined && getSelectedTask.nextElementSibling !== undefined) {
       getSelectedTask.parentElement.insertBefore(getSelectedTask.nextElementSibling, getSelectedTask);
     }
   });
