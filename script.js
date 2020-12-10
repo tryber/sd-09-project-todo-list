6c266f58e82d870dc34c5ea19b7a89e12c3a88d6
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-10 19:31:09 -0300

    Requirement 6: created function that updates background color of tasks.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Create a list item (li) child of a ordered list (ol)
-function cresteListItem (task) {
+function cresteListItem(task) {
     const orderedList = document.querySelector("#lista-tarefas");
     const listItem = document.createElement("li");
     listItem.innerText = task;
+    listItem.className = "tarefa";
     orderedList.appendChild(listItem);
     return listItem;
 }
@@ -13,10 +14,15 @@ function clearInput(inputValue) {
 }
 
 // Create Task
-function addTask () {
+function addTask() {
     const taskInput = document.querySelector("#texto-tarefa");
     cresteListItem(taskInput.value);
     clearInput(taskInput);
+}
+
+function changeTaskColor(event) {
+    const selectedItem = event.target;
+    selectedItem.style.backgroundColor = "rgb(128, 128, 128)";
 }
 
 // Prevent submit of a input in form
@@ -27,7 +33,9 @@ function preventSubmit(event) {
 window.onload = function () {
     const buttonTask = document.querySelector("#criar-tarefa");
     const taskForm = document.querySelector("#formulario-tarefa");
+    const taskList = document.querySelector("#lista-tarefas");
     
-    buttonTask.addEventListener('click', addTask);
-    taskForm.addEventListener('submit', preventSubmit);
+    buttonTask.addEventListener("click", addTask);
+    taskForm.addEventListener("submit", preventSubmit);
+    taskList.addEventListener("click", changeTaskColor);
 };
