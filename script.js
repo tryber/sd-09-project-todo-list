2cedc4ec075d8a924cb4c6382a5a03f984412ea4
Raphael Rêgo <raphael.mlrego@gmail.com>
2020-12-10 19:34:21 -0300

    adicionando botao apagar tudo

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function selected (textoTarefa) {
-    if (document.querySelectorAll('.selected').length > 0) {
+  if (document.querySelectorAll('.selected').length > 0) {
+      if (textoTarefa.classList.contains('selected')) {
+        textoTarefa.classList.remove('selected');
+      } else {        
         document.querySelector('.selected').classList.remove('selected');  
         textoTarefa.classList.add('selected');
-      } else {        
-        textoTarefa.classList.add('selected');
       }
+      document.querySelector('.selected').classList.remove('selected');  
+      textoTarefa.classList.add('selected');
+    } else {        
+      textoTarefa.classList.add('selected');
+    }
+}
+function completed (textoTarefa) {
+    if (textoTarefa.classList.contains('completed')) {
+      textoTarefa.classList.remove('completed');
+    } else {
+      textoTarefa.classList.add('completed');
+    }
 }
 function criarTarefa () {
   if (document.querySelector('#texto-tarefa').value === ''){
@@ -14,7 +27,10 @@ function criarTarefa () {
     textoTarefa.innerText = document.querySelector('#texto-tarefa').value;
     document.querySelector('#lista-tarefas').appendChild(textoTarefa);
     textoTarefa.addEventListener('click', function() {
-      selected(textoTarefa);
+      selected(textoTarefa);      
+    });
+    textoTarefa.addEventListener('dblclick', function() {
+        completed(textoTarefa);
     });
   }
 }
@@ -25,3 +41,11 @@ buttonCriarTarefa.addEventListener('click', function() {
   criarTarefa();
   document.querySelector('#texto-tarefa').value = '';
 });
+
+let buttonApagarTudo = document.querySelector('#apaga-tudo');
+buttonApagarTudo.addEventListener('click', function() {
+    let li = document.querySelectorAll('li');
+    for (let index = 0; index < li.length; index += 1) {
+      document.querySelector('#lista-tarefas').removeChild(li[index]);
+    }
+})
