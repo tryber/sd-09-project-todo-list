9bd198511d1625f9ee711cfd6bd39f85a2a9f56c
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-10 16:38:33 -0300

    Ajustes css

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let btn_Add = document.querySelector('#criar-tarefa');
+const btn_Add = document.querySelector('#criar-tarefa');
 
 function create_li() {
-  let ol_list = document.getElementById('lista-tarefas');
-  let li_list = document.createElement('li');
-  let input_Add = document.querySelector('#texto-tarefa');
-  li_list.innerText = input_Add.value;
-  ol_list.appendChild(li_list);
+
+  const olList = document.getElementById('lista-tarefas');
+  const liList = document.createElement('li');
+  const input_Add = document.querySelector('#texto-tarefa');
+  liList.innerText = input_Add.value;
+  olList.appendChild(liList);
   input_Add.value = '';
 }
-
 
 window.onload = function() {
 
   btn_Add.addEventListener('click', create_li);
-
-
-
-
-
 
 }
