d48e69decd0e81c6ab3851ae4037a2767cb3616e
LucioOSilva <lucioosilva00@gmail.com>
2020-12-10 01:27:23 -0300

    feature selector item task7 and 8

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// 5
 function clickAddTask() {
   const form = document.getElementById('input-bar');
-  const taskList = document.querySelector('#lista-tarefas');
+  const taskOlList = document.querySelector('#lista-tarefas');
+  const taskLiItems = taskOlList.children;
   form.addEventListener('submit', function (event) {
     event.preventDefault();
     let taskItem = document.createElement('li');
     const taskRead = event.target.tarefa.value;
     taskItem.innerText = taskRead;
-    taskList.appendChild(taskItem);
+    taskItem.addEventListener('click', function (eventT) {
+      for (let index = 0; index < taskLiItems.length; index += 1) {
+        taskLiItems[index].style.backgroundColor = '';
+      }
+      eventT.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    })
+    taskOlList.appendChild(taskItem);
     event.target.tarefa.value = '';
   });
 }
 clickAddTask();
-
-
