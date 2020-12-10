f9525a29a869f16c32713cbb3358063c9ec551c6
Luiz Henrique <lhfmworker@gmail.com>
2020-12-10 08:51:46 -0300

    fixed some code climate rules

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let taskTxt = document.getElementById('texto-tarefa');
-let btnTask = document.getElementById('criar-tarefa');
-let list = document.getElementById('lista-tarefas');
-let items = document.getElementsByTagName('li');
-let btnErase = document.getElementById('apaga-tudo');
+const taskTxt = document.getElementById('texto-tarefa');
+const btnTask = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const items = document.getElementsByTagName('li');
+const btnErase = document.getElementById('apaga-tudo');
 
 //  codigo abaixo gera novo item na lista
 function addList() {
-  let clipboard = taskTxt.value;
-  let itemList = document.createElement('li');
+  const clipboard = taskTxt.value;
+  const itemList = document.createElement('li');
   itemList.innerText = clipboard;
   list.appendChild(itemList);
   taskTxt.value = '';
@@ -15,30 +15,30 @@ function addList() {
 
 //  codigo para tornar o item da lista cinza quando clicado
 function graySize() {
-  let sltdLi = event.target;
-  for (let i=0; i<items.length; i+= 1) {
+  const sltdLi = event.target;
+  for (let i = 0; i < items.length; i += 1) {
     items[i].classList.remove('selected');
   }
   sltdLi.classList.add('selected');
 }
 btnTask.addEventListener('click', addList);
-list.addEventListener('click',graySize);
+list.addEventListener('click', graySize);
 
 //  codigo para apagar todos os itens da lista
 function eraseAll() {
-  for (let i=items.length-1; i>=0; i -=1){
+  for (let i = items.length - 1; i >= 0; i -= 1) {
     list.removeChild(items[i]);
   }
 }
-btnErase.addEventListener('click',eraseAll);
+btnErase.addEventListener('click', eraseAll);
 
 // codigo a seguir confere linha riscada em cada tarefa
 function lineT() {
-  let doneTask = event.target;
-  if (doneTask.className.includes('completed')){
+  const doneTask = event.target;
+  if (doneTask.className.includes('completed')) {
     doneTask.classList.remove('completed');
   } else {
     doneTask.classList.add('completed');
   }
 }
-list.addEventListener('dblclick',lineT);
+list.addEventListener('dblclick', lineT);
