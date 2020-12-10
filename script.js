f157273c605196da3a633f69a900f4f6b851edc1
MarcosAlbuquerque <mpdsa20@gmail.com>
2020-12-10 17:41:32 -0300

    evaluator nao verificou, enviando para avaliaçao e reduzindo erros do codeClimate

--- script.js
+++ script.js
@@ -7,31 +7,32 @@ const moverBaixo = document.getElementById('mover-baixo');
 const apagaTudo = document.getElementById('apaga-tudo');
 const removerFinalizados = document.getElementById('remover-finalizados');
 const salvarTarefas = document.getElementById('salvar-tarefas');
+const itemsLista = listaTarefas.children;
+const nomeItem = document.createElement('li');
+const qntItems = 0;
 let tamanhoLista = listaTarefas.children.length;
-let itemsLista = listaTarefas.children;
 
 criarTarefa.addEventListener('click', () => {
-    let nome = document.createElement('li');
-    if (textoTarefa.value !== '') {
-      nome.innerHTML = textoTarefa.value;
-      listaTarefas.appendChild(nome);
-      textoTarefa.value = '';
-      tamanhoLista += 1;
-    }
+  if (textoTarefa.value !== '') {
+    nomeItem.innerHTML = textoTarefa.value;
+    listaTarefas.appendChild(nomeItem);
+    textoTarefa.value = '';
+    tamanhoLista += 1;
+  }
 });
 
 listaTarefas.addEventListener('click', (event) => {
-    for (let i = 0; i < tamanhoLista; i += 1) {
-      itemsLista[i].style.backgroundColor = '';
-      if (itemsLista[i]=== event.target) {
-        itemsLista[i].style.backgroundColor = 'rgb(128, 128, 128)'
-      }
+  for (let i = 0; i < tamanhoLista; i += 1) {
+    itemsLista[i].style.backgroundColor = '';
+    if (itemsLista[i] === event.target) {
+      itemsLista[i].style.backgroundColor = 'rgb(128, 128, 128)';
     }
+  }
 });
 
 listaTarefas.addEventListener('dblclick', (event) => {
   for (let i = 0; i <= tamanhoLista; i += 1) {
-    if (itemsLista[i]=== event.target) {
+    if (itemsLista[i] === event.target) {
       if (itemsLista[i].classList[0] === undefined) {
         itemsLista[i].classList.add('completed');
       } else {
@@ -42,34 +43,28 @@ listaTarefas.addEventListener('dblclick', (event) => {
 });
 
 apagaTudo.addEventListener('click', () => {
-    let qntItems = 0;
-    while (qntItems < listaTarefas.children.length) {
-      listaTarefas.removeChild(listaTarefas.childNodes[0])
-    };
+  while (qntItems < listaTarefas.children.length) {
+    listaTarefas.removeChild(listaTarefas.childNodes[0]);
+  }
 });
 
 removerSelecionado.addEventListener('click', (event) => {
-  console.log(event.target)
+  // console.log(event.target)
   // console.log(lista.children)
 });
 
 moverCima.addEventListener('click', (event) => {
-  console.log(event.target)
+  // console.log(event.target)
   // console.log(lista.children)
 });
 
 moverBaixo.addEventListener('click', (event) => {
-  console.log(event.target)
+  // console.log(event.target)
   // console.log(lista.children)
 });
 
-
-
 removerFinalizados.addEventListener('click', (event) => {
-  
 });
 
 salvarTarefas.addEventListener('click', (event) => {
-  console.log(event.target)
-  // console.log(lista.children)
 });
