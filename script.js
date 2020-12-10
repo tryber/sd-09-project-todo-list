7e481c3af8e7066f7b7117dc5db6c8be7a90491d
alailsonko <aalailson3@gmail.com>
2020-12-09 22:36:54 -0300

    feat:challenge 5

---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", main)
+
+function main () {
+  const tasks = Tasks()
+  tasks.createTask()
+  loadMsg()
+}
+
+
+
+function Factory(){
+  const TagButtonAddTask = document.getElementById("criar-tarefa")
+  const TagOlListTasks = document.getElementById("lista-tarefas")
+  const TagInputerTask = document.getElementById("texto-tarefa")
+  return {
+    MyTagButtonAddTask: TagButtonAddTask,
+    MyTagOlListTasks: TagOlListTasks,
+    MyTagInputerTask: TagInputerTask
+  }
+}
+
+const nodeListTask = null
+
+function Tasks() {
+  const factory = Factory()
+
+  function createTask () {
+    const myTagButtonAddTask = factory.MyTagButtonAddTask
+    myTagButtonAddTask.addEventListener("click", function (e) {
+      e.preventDefault()
+
+      const myTagInputerTask = factory.MyTagInputerTask
+      const valueTask = myTagInputerTask.value
+      const myTagOlListTasks = factory.MyTagOlListTasks
+
+    myTagOlListTasks.innerHTML += `<li>${valueTask}<li>`
+
+    const indexTheLast = myTagOlListTasks.childNodes.length - 1
+
+    const removeTheLast = myTagOlListTasks.childNodes[indexTheLast]
+    myTagOlListTasks.removeChild(removeTheLast)
+
+    // clean the input after click
+    myTagInputerTask.value = null
+    })
+
+    console.log("createTask working")
+  }
+  return {
+    createTask: createTask
+  }
+
+}
+
+
+function loadMsg () {
+  console.log("scripts working 🚀 ")
+}
