a46f40a649b3ee664c8e8b9c8f8b7722bb6ecd67
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-12-10 18:23:42 -0300

    Requisito 8 Concluido

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ document.getElementById('criar-tarefa').addEventListener('click', function (){
 function paintItem() {
     let taskList = document.getElementById('lista-tarefas');
     taskList.addEventListener('click', function(event) {
+        let getList = document.getElementsByTagName('li');
+        for (let i = 0; i < getList.length; i++) {
+            getList[i].style.backgroundColor = ''
+        }
         if (event.target.style.backgroundColor !== 'rgb(128, 128, 128)') {
             event.target.style.backgroundColor = 'rgb(128, 128, 128)';
         }
