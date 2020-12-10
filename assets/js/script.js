bb39342d406d8a0b84311ddfce38513b54f5bcf5
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-12-10 02:09:31 -0300

    Commit do que foi feito que o sono bateu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
 // Adicionar nova tarefa
 function createNewTask() {
   const tasksList = document.querySelector('#lista-tarefas');
-    const newItem = document.createElement('li');
-    let newTask = document.querySelector('#texto-tarefa');
-    newItem.setAttribute('class', 'task');
-    tasksList.appendChild(newItem).innerText = newTask.value;
-    newTask.value = '';
+  const newItem = document.createElement('li');
+  let newTask = document.querySelector('#texto-tarefa');
+  newItem.setAttribute('class', 'task');
+  tasksList.appendChild(newItem).innerText = newTask.value;
+  newTask.value = '';
+}
+
+// Selecionar uma tarefa da lista
+function selectTask() {
+  const tasksList = document.querySelector('#lista-tarefas');
+  for (let index = 0; index < tasksList.childElementCount; index += 1) {
+    tasksList.children[index].onclick
+    /* tasksList.children[index].addEventListener('click', function() {
+      if (tasksList.children[index]) {
+        console.log(tasksList.children[index].innerText);
+      }
+    }) */
   }
-  
-  window.onload = function() {
-    const buttonNewTask = document.querySelector('#criar-tarefa');
-    buttonNewTask.addEventListener('click', createNewTask);
-  }
+}
+
+window.onload = function() {
+  const buttonNewTask = document.querySelector('#criar-tarefa');
+  buttonNewTask.addEventListener('click', createNewTask);
+  const tasksList = document.querySelector('#lista-tarefas');
+  //tasksList.addEventListener('click', selectTask);
+}
