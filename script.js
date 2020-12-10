166af602c69105e460b2b90a8aa714cfbf17b3a1
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-12-10 18:14:58 -0300

    Requisito 7 Concluido

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-document.getElementById('criar-tarefa').addEventListener('click', function(){
-    let task1 = document.querySelector('input').value;
-    let li = document.createElement('li');
-    document.querySelector('input').value = '';
-    li.innerText = task1;
-    document.getElementById('lista-tarefas').appendChild(li)
+document.getElementById('criar-tarefa').addEventListener('click', function (){
+  const task1 = document.querySelector('input').value;
+  const li = document.createElement('li');
+  document.querySelector('input').value = '';
+  li.innerText = task1;
+  document.getElementById('lista-tarefas').appendChild(li);
 })
+
+function paintItem() {
+    let taskList = document.getElementById('lista-tarefas');
+    taskList.addEventListener('click', function(event) {
+        if (event.target.style.backgroundColor !== 'rgb(128, 128, 128)') {
+            event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        }
+    });
+}
+
+paintItem();
