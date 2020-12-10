acfc7cda56178cf1975832558cef403d6577eae6
rach-vp <picanco.raquel@gmail.com>
2020-12-10 10:53:58 -0400

    Implementa highlight da task selecionada

--- script.js
+++ script.js
@@ -7,8 +7,30 @@ function addTask() {
     const listItem = document.createElement('li');
     listItem.innerText = task;
     taskList.appendChild(listItem);
+    highlightListItemEvent();
     document.querySelector('#texto-tarefa').value = '';
   });
 }
 
 addTask();
+
+// Altera cor de fundo do item da lista selecionado
+function highlight(event) {
+  const clickedItem = event.target;
+  const selectedItemBackgroundColor = 'rgb(128, 128, 128)';
+  if (clickedItem.style.backgroundColor !== selectedItemBackgroundColor) {
+    clickedItem.style.backgroundColor = selectedItemBackgroundColor
+  } else {
+    clickedItem.style.backgroundColor = 'transparent';
+  }
+}
+
+function highlightListItemEvent() {
+  const listItems = document.querySelectorAll('li');
+  for (let index = 0; index < listItems.length; index += 1) {
+    listItems[index].removeEventListener('click', highlight);
+    listItems[index].addEventListener('click', highlight);
+  }
+}
+
+highlightListItemEvent();
