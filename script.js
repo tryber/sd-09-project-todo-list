bc2f2212a772c0ddcf36f53d642cb07154cd4129
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-10 16:54:44 -0300

    Requisito 7 criado

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const btn_Add = document.querySelector('#criar-tarefa');
+const olList = document.getElementById('lista-tarefas');
+
 
 function create_li() {
 
-  const olList = document.getElementById('lista-tarefas');
   const liList = document.createElement('li');
   const input_Add = document.querySelector('#texto-tarefa');
   liList.innerText = input_Add.value;
   olList.appendChild(liList);
   input_Add.value = '';
+
+}
+
+function itemChangeBgColor(event) {
+
+  let item = event.target;
+  item.style.backgroundColor = 'rgb(128, 128, 128)';
+
 }
 
 window.onload = function() {
 
   btn_Add.addEventListener('click', create_li);
+  olList.addEventListener('dblclick', itemChangeBgColor);
 
 }
