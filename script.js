da9d0044ce37d39240491505e33f11a78c4738ea
Christopher <chris.tryber@gmail.com>
2020-12-10 14:21:58 -0300

    segundo teste 4 e 5

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.onload = function(){
-    let criarTarefas = document.querySelector('#criar-tarefa');
-    criarTarefas.addEventListener('click', function(){
-        let textoTarefas = document.querySelector('#texto-tarefa');
-        let listItem = document.createElement('li');
-        let orderList = document.querySelector('#lista-tarefas');
-        let valorTexto = textoTarefas.value;
-        orderList.appendChild(listItem)
-        listItem.className = 'listItem'
-        listItem.innerText = valorTexto;
-        textoTarefas.value = '';
+window.onload = function () {
+    const criarTarefas = document.querySelector('#criar-tarefa');
+    criarTarefas.addEventListener('click', function () {
+      const textoTarefas = document.querySelector('#texto-tarefa');
+      const listItem = document.createElement('li');
+      let orderList = document.querySelector('#lista-tarefas');
+      const valorTexto = textoTarefas.value;
+      orderList.appendChild(listItem);
+      listItem.className = 'listItem';
+      listItem.innerText = valorTexto;
+      textoTarefas.value = '';
     })
 }
