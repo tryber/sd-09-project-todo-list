8190cea30c68c8f1da2313ff6b2f98897e638581
Carine Becker <caarine.m@gmail.com>
2020-12-10 18:51:45 -0300

    add todo function

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-window.onload = function() {
-    addToDo();
-    
+window.onload = function () {
+  addToDo();
 }
 
 const listElement = document.querySelector('#lista-tarefas');
 const inputElement = document.querySelector('#texto-tarefa');
 
 function addToDo() {
-    const buttonCreateTask = document.querySelector('#criar-tarefa')
-    buttonCreateTask.addEventListener('click', function() {
-        const createTask = document.createElement('li');
-        createTask.innerText = inputElement.value;
-        listElement.appendChild(createTask);
+  const buttonCreateTask = document.querySelector('#criar-tarefa')
+  buttonCreateTask.addEventListener('click', function () {
+    const createTask = document.createElement('li');
+    createTask.innerText = inputElement.value;
+    listElement.appendChild(createTask);
 
-        createTask.addEventListener('click', colorTask);
-
-        inputElement.value = '';
-    })
+    createTask.addEventListener('dblclick', completedTask);
+    createTask.addEventListener('click', colorTask);
+    inputElement.value = '';
+  })
 }
 
 function colorTask(event) {
-    const tasks = document.querySelectorAll('#lista-tarefas li')
-    tasks.forEach(task => {
-        task.style.backgroundColor = ''
-    });
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  let tasks = document.querySelectorAll('#lista-tarefas li');
+  tasks.forEach(task => {
+    task.style.backgroundColor = '';
+  });
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function toggleCompletedTask(event) {
+  event.target.classList.toggle('completed');
 }
