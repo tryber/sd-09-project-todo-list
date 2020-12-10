f1eb45f2ecdf9e2e48fe70908ea0a6469fbd2e3a
danielfasanaro <danielfasanaro@gmail.com>
2020-12-10 06:45:51 -0300

    7th/8th Requiriments - Added selected changeSelectedTask function

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-function addTask () {
+function addTask() {
   const taskName = document.querySelector('#texto-tarefa');
   const taskList = document.querySelector('#lista-tarefas');
   const listItem = document.createElement('li');
   listItem.innerText = taskName.value;
+  listItem.addEventListener('click', changeTaskBgcolor);
   taskName.value = '';
   taskList.appendChild(listItem);
+}
+
+function changeSelectedTask(event) {
+  const selectedTask = document.querySelector('.selected')
+  if (selectedTask) {
+    selectedTask.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
 }
 
 function addCreateTaskListener() {
