f357c9415a87195b7a013d08eebe05a27da22eaf
danielfasanaro <danielfasanaro@gmail.com>
2020-12-10 07:05:09 -0300

    9th Requiriments - Added changedCompletedTask function

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function addTask() {
-  const taskName = document.querySelector('#texto-tarefa');
-  const taskList = document.querySelector('#lista-tarefas');
-  const listItem = document.createElement('li');
-  listItem.innerText = taskName.value;
-  listItem.addEventListener('click', changeSelectedTask);
-  taskName.value = '';
-  taskList.appendChild(listItem);
+function changeCompletedTask(event) {
+  if (event.target.className === 'completed selected') {
+    event.target.className = 'selected';
+  } else {
+    event.target.classList.add('completed');
+  }
 }
 
 function changeSelectedTask(event) {
@@ -14,6 +12,17 @@ function changeSelectedTask(event) {
     selectedTask.classList.remove('selected');
   }
   event.target.classList.add('selected');
+}
+
+function addTask() {
+  const taskName = document.querySelector('#texto-tarefa');
+  const taskList = document.querySelector('#lista-tarefas');
+  const listItem = document.createElement('li');
+  listItem.innerText = taskName.value;
+  listItem.addEventListener('click', changeSelectedTask);
+  listItem.addEventListener('dblclick', changeCompletedTask);
+  taskName.value = '';
+  taskList.appendChild(listItem);
 }
 
 function addCreateTaskListener() {
