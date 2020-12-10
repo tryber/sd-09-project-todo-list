4ba3a7eb1b692bb21417475d88455cf3236641b3
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-10 17:28:55 -0400

    Requisito 5: refatorando JavaScript

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const buttonAddTasks = document.querySelector('#criar-tarefa');
 const listTask = document.querySelector('#lista-tarefas');
 
 function createTask() {
-    const inputTask = document.querySelector('#texto-tarefa');
-    if (inputTask.value === '') {
-        return;
-    };
-    let createLi = document.createElement('li');
-    createLi.innerText = inputTask.value;
-    createLi.className = 'task';
-    listTask.appendChild(createLi);
-    inputTask.value = '';
+  const inputTask = document.querySelector('#texto-tarefa');
+  if (inputTask.value === '') {
+    return;
+  };
+  const createLi = document.createElement('li');
+  createLi.innerText = inputTask.value;
+  createLi.className = 'task';
+  listTask.appendChild(createLi);
+  inputTask.value = '';
 }
 
 function buttonAddTaskList() {
-    buttonAddTasks.addEventListener('click', createTask);
+  buttonAddTasks.addEventListener('click', createTask);
 }
 
 buttonAddTaskList();
