02a8aec977a23db1b707625dd4760c40884a7126
ViniciusChagas <viniciushrcs@gmail.com>
2020-12-10 22:25:05 -0300

    colorindo o background do item clicado

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let buttonNewTask = document.getElementById('criar-tarefa');
+let getTaskList = document.querySelector('#lista-tarefas');
 
 buttonNewTask.addEventListener('click', function(){
-    let getTaskList = document.getElementById('lista-tarefas');
     let inputText = document.getElementById('texto-tarefa').value;
     let newLi = document.createElement('li');
     newLi.innerText = inputText;
+    newLi.className = 'item-list'
+    newLi.style.backgroundColor = 'white'
     getTaskList.appendChild(newLi)
     cleartext();
 
@@ -13,3 +15,17 @@ buttonNewTask.addEventListener('click', function(){
     }
 
 })
+
+getTaskList.addEventListener('click', function(itemOrigin){
+    let itemClicked = itemOrigin.target
+    let newColor = 'rgb(128,128,128)'
+    let itensList = document.querySelectorAll('.item-list')
+
+    if (itemClicked.style.backgroundColor !== newColor){
+        for (let index = 0; index < itensList.length; index +=1){
+            itensList[index].style.backgroundColor = 'white'
+        }
+        itemClicked.style.backgroundColor = newColor
+    } 
+})
+
