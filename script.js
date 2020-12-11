d02f0af09ed427d98a432933b15c6dbf92ae806e
Genadi Reuben <genadireuben91@gmail.com>
2020-12-10 22:22:47 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -9,13 +9,35 @@ function addTask() {
   const createLi = document.createElement('li');
   if (inputValue) {
     createLi.innerText = inputValue;
-    createLi.classList.add('taskItem');
+    createLi.classList.add('task-item')
     innerTaskList.appendChild(createLi);
     inputPlace.value = '';
   };
 
-
-
 };
 
 buttonAddTask.addEventListener('click', addTask);
+
+
+
+function selectTaskItem(event) {
+  const selectedTask = document.querySelectorAll('.task-item.selected');
+  const itensTask = document.querySelectorAll('.task-item');
+  console.log(selectedTask)
+  for (let index = 0; index < selectedTask.length; index++) {
+    selectedTask[index].classList.remove('selected')
+  }
+  if (event.target.className !== 'task-item selected') {
+    event.target.classList.add('selected')
+  } else if (event.target.className === 'task-item selected') {
+    event.target.classList.remove('selected')
+  }
+
+}
+
+const selectedItem = document.querySelectorAll('#lista-tarefas');
+for (let index = 0; index < selectedItem.length; index++) {
+  selectedItem[index].addEventListener('click', selectTaskItem);
+}
+
+
