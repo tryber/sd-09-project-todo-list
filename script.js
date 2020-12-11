a5741ec6335a6b9a6c5aa7706b0fcdc51c7b54e1
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-11 02:41:23 -0300

    Requisitos 5

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variable
 const buttonTask = document.querySelector('.criar-tarefa');
 buttonTask.textContent = 'Salvar Tarefa';
 buttonTask.style.height = ('20px');
@@ -5,14 +6,17 @@ buttonTask.style.height = ('20px');
 const list = document.createElement('ul');
 document.body.appendChild(list);
 
+// Create a list via the button
 function taskList() {
   buttonTask.addEventListener('click', () => {
     const taskText = document.querySelector('.texto-tarefa');
     const ol = document.createElement('ol');
 
+    ol.id = 'lista-tarelas';
     ol.className = 'lista-tarelas';
     ol.textContent = taskText.value;
     list.appendChild(ol);
+    taskText.value = '';
   });
 }
 taskList();
