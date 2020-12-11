e2b71dc0d313cf54ecf469bef2c3ad8ea1e883e5
Matheus Monteiro <monteiromsh@gmail.com>
2020-12-11 00:47:18 -0300

    Commit de exercicios 5

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 window.onload = functionsLoaded();
 
-function functionsLoaded(){
-    newItemList()
+function functionsLoaded() {
+  newItemList();
 }
 
-function clearInput(){
+function clearInput() {
+  let inputLocal = document.querySelector("#texto-tarefa");
+  inputLocal.value = "";
+}
+
+function newItemList() {
+  let buttonLocal = document.querySelector("#criar-tarefa");
+
+  buttonLocal.addEventListener("click", function () {
     let inputLocal = document.querySelector("#texto-tarefa");
-    inputLocal.value = "";
+    let taskName = inputLocal.value;
+    let listLocal = document.querySelector("#lista-tarefas");
+
+    let newItem = document.createElement("li");
+    newItem.className = "list-item";
+    newItem.innerText = taskName;
+    listLocal.appendChild(newItem);
+
+    clearInput();
+    clickColor();
+  });
 }
 
-function newItemList(){
-    let buttonLocal = document.querySelector("#criar-tarefa")
-
-    buttonLocal.addEventListener("click", function(){
-        let inputLocal = document.querySelector("#texto-tarefa")
-        let taskName = inputLocal.value
-        let listLocal = document.querySelector("#lista-tarefas")
-
-        let newItem = document.createElement("li")
-        newItem.className = "list-item"
-        newItem.innerText = taskName
-        listLocal.appendChild(newItem)
-
-        clearInput()
-    })
+function clickColor() {
+  window.addEventListener("click", function (event) {
+    let element = event.target;
+    if (element.className === "list-item") {
+      element.style.color = "rgb(128, 128, 128)";
+    }
+  });
 }
