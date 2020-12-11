074883ef6b7adcc396f5dcf82427a58789015e93
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-10 21:28:19 -0300

    Requirement 8: Update the functions that change background color of tasks.

--- script.js
+++ script.js
@@ -13,16 +13,43 @@ function clearInput(inputValue) {
     inputValue.value = null;
 }
 
-// Create Task
+// Create task(li) in oredered list(ol)
 function addTask() {
     const taskInput = document.querySelector("#texto-tarefa");
     cresteListItem(taskInput.value);
     clearInput(taskInput);
 }
 
-function changeTaskColor(event) {
+// Change background-color of elements with class selected
+function updateBackgroundColor() {
+    const listItems = document.querySelectorAll("li");
+    for (let index = 0; index < listItems.length; index += 1 ) {
+        if (listItems[index].className === "selected") {
+            listItems[index].style.backgroundColor = "rgb(128, 128, 128)";
+        } else {
+            listItems[index].style.backgroundColor = null;
+        }
+    }
+}
+
+// Update the class of selected itens in task list
+function updateItemClass(item) {
+    const listItems = document.querySelectorAll("li");
+    for (let index = 0; index < listItems.length; index += 1 ) {
+        if (listItems[index].className === "selected") {
+            listItems[index].className = "tarefa";
+        }
+    }
+    item.className = "selected";
+}
+
+// Make the call of the functions that update class and update background-color of selected itens
+function updateTaskColor(event) {
     const selectedItem = event.target;
-    selectedItem.style.backgroundColor = "rgb(128, 128, 128)";
+    if( selectedItem.className === "tarefa"){
+        updateItemClass(selectedItem);
+    }
+    updateBackgroundColor();
 }
 
 // Prevent submit of a input in form
@@ -37,5 +64,5 @@ window.onload = function () {
     
     buttonTask.addEventListener("click", addTask);
     taskForm.addEventListener("submit", preventSubmit);
-    taskList.addEventListener("click", changeTaskColor);
+    taskList.addEventListener("click", updateTaskColor);
 };
