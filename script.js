8343afe673470e4522f426899e54809295b12b98
ViniciusChagas <viniciushrcs@gmail.com>
2020-12-10 21:25:27 -0300

    including new tasks working

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let buttonNewTask = document.getElementById('criar-tarefa');
 
+buttonNewTask.addEventListener('click', function(){
+    let getTaskList = document.getElementById('lista-tarefas');
+    let inputText = document.getElementById('texto-tarefa').value;
+    let newLi = document.createElement('li');
+    newLi.innerText = inputText;
+    getTaskList.appendChild(newLi)
+    cleartext();
+
+    function cleartext(){
+        document.getElementById('texto-tarefa').value = ''
+    }
+
+})
