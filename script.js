458815ac4cade7844021ceff60f0151bdefc7000
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-11 02:28:03 -0300

    Requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function taskList() {
+const buttonTask = document.querySelector('.criar-tarefa');
+buttonTask.textContent = 'Salvar Tarefa';
+buttonTask.style.height = ('20px');
 
+const list = document.createElement('ul');
+document.body.appendChild(list);
+
+function taskList() {
+  buttonTask.addEventListener('click', () => {
+    const taskText = document.querySelector('.texto-tarefa');
+    const ol = document.createElement('ol');
+
+    ol.className = 'lista-tarelas';
+    ol.textContent = taskText.value;
+    list.appendChild(ol);
+  });
 }
+taskList();
