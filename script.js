fcd9cce1865d5288e28d67dcd98231de2d76859c
Daniela Muller <danielamuller20@gmail.com>
2020-12-12 14:31:52 -0300

    Alteração do backgroundColor da lista

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+const text = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
+const orderedList = document.querySelector('#lista-tarefas');
+const list = document.createElement('li');
+list.className = 'todo-list';
+
 function addTask() {
-  const text = document.querySelector('#texto-tarefa');
-  const button = document.querySelector('#criar-tarefa');
-  const orderedList = document.querySelector('#lista-tarefas');
-  
   button.addEventListener('click', function () {
     if(text.value === '') {
       alert('[ERRO!] - Digite algum valor!');
     } else {
       const list = document.createElement('li');
-      list.className = 'todo-list'
+      list.className = 'todo-list';
       orderedList.appendChild(list);
       list.innerText = text.value;
       text.value = '';
@@ -18,9 +20,12 @@ function addTask() {
 addTask();
 
 function chamgeBackgroudColorOfList() {
-  const list = document.querySelector('#lista-tarefas')
-  list.addEventListener('click', function (event) {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+  orderedList.addEventListener('click', function (event) {
+    if (event.target.className === 'lista-de-tarefas') {
+      event.target.style.backgroundColor = 'rgb(228, 250, 228)';
+    }else if(list.classList.contains('todo-list')) {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
   });
 }
 chamgeBackgroudColorOfList();
