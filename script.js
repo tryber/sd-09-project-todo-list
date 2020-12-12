f50199516206a23201785a0923c64f6fdbc6a890
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-12 12:00:18 -0300

    Requisitos 5 test

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-// Variable
+// Variables
 const buttonTask = document.querySelector('.criar-tarefa');
 buttonTask.textContent = 'Salvar Tarefa';
 buttonTask.style.height = ('20px');
 
 const list = document.createElement('ul');
 document.body.appendChild(list);
+list.background
 
 // Create a list via the button
 function taskList() {
-  buttonTask.addEventListener('click', () => {
-    const taskText = document.querySelector('.texto-tarefa');
+    buttonTask.addEventListener('click', () => {
+      const taskText = document.querySelector('.texto-tarefa');
+      if (!taskText.value) {
+        alert ('Lista vazia, digite sua tarefa!') } else {
     const ol = document.createElement('ol');
 
+    list.appendChild(ol);
     ol.id = 'lista-tarelas';
     ol.className = 'lista-tarelas';
     ol.textContent = taskText.value;
-    list.appendChild(ol);
     taskText.value = '';
+  }
   });
 }
 taskList();
