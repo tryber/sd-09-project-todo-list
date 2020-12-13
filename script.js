0d942ecaee16173bf65c1e6e2fd3354f10c9f752
MZTBTG <mztbtg@hotmail.com>
2020-12-13 03:03:15 -0300

    Até o item 8 concluido. 'Sem erros do CodeClimate'

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const contentInput = document.getElementById('texto-tarefa');
 const orderList = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
+const line = document.getElementsByClassName('line');
 
 function addItemInList() {
-  let line = document.createElement('li');
-  line.className = 'line';
-  line.innerText = contentInput.value;
-  orderList.appendChild(line);
-  contentInput.value = '';
+  while (contentInput.value !== '') {
+    const line = document.createElement('li');
+    line.className = 'line';
+    line.innerText = contentInput.value;
+    orderList.appendChild(line);
+    contentInput.value = '';
+  };
 }
 
-function buttonAddItem() {
-  button.addEventListener('click', addItemInList);
+function selectText(event) {
+  for (let index = 0; index < line.length; index += 1) {
+    line[index].removeAttribute('style');
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 window.onload = function init() {
-  buttonAddItem()
-}
+  button.addEventListener('click', addItemInList);
+  orderList.addEventListener('dblclick', selectText);
+};
