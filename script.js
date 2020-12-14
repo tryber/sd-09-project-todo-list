f28177fcd8bbe475c26e95f088a9191b68cfc5e6
Pedro Henrique <pedroenrique.400@gmail.com>
2020-12-14 13:12:02 -0300

    Faz o requisito 5

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ let orderedList = document.createElement('ol');
 orderedList.id = 'lista-tarefas';
 let body = document.querySelector('body');
 body.appendChild(orderedList);
+let button = document.createElement('button');
+button.id = 'criar-tarefa';
+button.innerHTML = 'Criar tarefa';
+body.appendChild(button);
+textBar = document.querySelector('input');
+
+window.onload = function() {
+    button.addEventListener('click', function() {
+        let itens = document.createElement('li');
+        itens.innerHTML = textBar.value;
+        orderedList.appendChild(itens);
+        textBar.value = '';
+    });
+}
