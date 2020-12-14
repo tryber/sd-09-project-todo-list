6a2f13e86e6e61013c62f095fbc3a06fc13dce6d
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-14 20:50:19 -0300

    corrigido codeclimate

--- script.js
+++ script.js
@@ -59,8 +59,8 @@ function upTaskSelected() {
   const elementsLi = document.querySelectorAll('li');
   for (let index = 0; index < elementsLi.length; index += 1) {
     if ((elementsLi[index].style.backgroundColor === 'rgb(128, 128, 128)') && (elementsLi[index] !== taskList.firstChild)) {
-      let liAux = { 
-        text: elementsLi[index].previousElementSibling.innerText, 
+      let liAux = {
+        text: elementsLi[index].previousElementSibling.innerText,
         class: elementsLi[index].previousElementSibling.className,
       }
       elementsLi[index].previousElementSibling.innerText = elementsLi[index].innerText;
@@ -80,7 +80,7 @@ function downTaskSelected() {
   for (let index = 0; index < elementsLi.length; index += 1) {
     if ((elementsLi[index].style.backgroundColor === 'rgb(128, 128, 128)') && (elementsLi[index] !== taskList.lastChild)) {
       let liAux = {
-        text: elementsLi[index].nextElementSibling.innerText, 
+        text: elementsLi[index].nextElementSibling.innerText,
         class: elementsLi[index].nextElementSibling.className,
       }
       elementsLi[index].nextElementSibling.innerText = elementsLi[index].innerText;
