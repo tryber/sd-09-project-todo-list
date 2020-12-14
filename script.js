b878e6e2ab720f3b224a51e0a1096a836724a35d
Matheus Mendes <matheus.unilog@gmail.com>
2020-12-13 21:20:41 -0300

    Code climate adjusts

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function checkText(task) {
   if (task === '') {
     alert('FUDEEEEEEEEEEEEU');
-    } else {
-  const taskList = document.querySelector('#lista-tarefas');
-  const listItem = document.createElement('li');
-  listItem.innerText = task;
-  listItem.className = 'task';
+  } else {
+    const taskList = document.querySelector('#lista-tarefas');
+    const listItem = document.createElement('li');
+    listItem.innerText = task;
+    listItem.className = 'task';
 
-  taskList.appendChild(listItem);
-  document.querySelector('#texto-tarefa').value = '';
- }
+    taskList.appendChild(listItem);
+    document.querySelector('#texto-tarefa').value = '';
+  }
 }
 
 function addTask() {
@@ -17,9 +17,9 @@ function addTask() {
   const inputBox = document.querySelector('#texto-tarefa');
     addButton.addEventListener('click', function () {
         checkText(inputBox.value);
-  });
+    });
     inputBox.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
+      if (event.keyCode === 13) {
       checkText(inputBox.value);
     }
   });
@@ -39,5 +39,4 @@ function clearAll() {
     }
   });
 }
-  
 clearAll();
