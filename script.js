955d6db10d6cddde8570de0d7039f4ad6b88d67e
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-14 14:44:35 -0300

    Requisitos correçoes

--- script.js
+++ script.js
@@ -3,27 +3,37 @@ const buttonTask = document.querySelector('.criar-tarefa');
 buttonTask.textContent = 'Salvar Tarefa';
 buttonTask.style.height = '20px';
 
-const list = document.createElement('ul');
+const list = document.querySelector('ul');
 document.body.appendChild(list);
 
 // Create a list via the button
 function taskList() {
   buttonTask.addEventListener('click', () => {
-    const taskText = document.querySelector('.texto-tarefa');
-    if (!taskText.value) {
+    const inputText = document.querySelector('.texto-tarefa');
+    if (!inputText.value) {
       alert('Lista vazia, digite sua tarefa!');
     } else {
-      const ol = document.createElement('ol');
-      ol.id = 'lista-tarefas';
-
+      const ol = document.createElement('ol')
       list.appendChild(ol);
-      ol.className = 'lista-tarelas';
-      ol.textContent = taskText.value;
-      taskText.value = '';
+      ol.textContent = inputText.value;
+      ol.id = 'lista-tarefa'
+      ol.className = 'lista-tarefas'
+      inputText.value = '';
+    }
+  });
+}
+
+function paintTask() {
+  list.addEventListener('click', (event) => {
+    if (event.target) {
+      event.target.style.backgroundColor = 'gray';
+    } else {
+      event.target.style.backgroundColor = null;
     }
   });
 }
 
 window.onload = function () {
   taskList();
+  paintTask();
 };
