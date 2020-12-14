db574615e44be8b7f71837dd6c85ce749c94a725
Diego Vini <analistamm@gmail.com>
2020-12-14 08:38:20 -0300

    Ajustando código

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 let taskList = [];
-let listTask = document.querySelector('#lista-tarefas')
+const listTask = document.querySelector('#lista-tarefas');
 
-function addItemButton(){
-  let clickButton = document.querySelector('#criar-tarefa')
-  clickButton.addEventListener('click', function(event){
-    let textInBox = document.querySelector('#texto-tarefa').value
-    taskList.push(textInBox) 
-    listTask.innerHTML = ''
+function addItemButton() {
+  const clickButton = document.querySelector('#criar-tarefa');
+  clickButton.addEventListener('click', function() {
+    let textInBox = document.querySelector('#texto-tarefa').value;
+    taskList.push(textInBox);
+    listTask.innerHTML = '';
     taskListItem();
     textInBox = document.querySelector('#texto-tarefa').value = '';  
-})
+  });
 }
 
-function taskListItem(){
-   for (let listItem = 0; listItem < taskList.length; listItem += 1){
-    let taskItem = taskList[listItem]
-    let item = document.createElement('li');   
-    item.innerText = taskItem;
-    listTask.appendChild(item)
+function taskListItem() {
+  for (let listItem = 0; listItem < taskList.length; listItem += 1){
+   const taskItem = taskList[listItem];
+   let item = document.createElement('li');
+   item.innerText = taskItem;
+   listTask.appendChild(item);
    }
 }
 
-
-window.onload = function(){
+window.onload = function() {
     addItemButton();
-    taskListItem()
-    
+    taskListItem();
 };
