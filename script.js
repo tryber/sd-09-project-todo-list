b7044e9f94bed839659b27c80ff223e53bb076d2
marcusWittho <witthoeft.mcs@hotmail.com>
2020-12-13 21:53:17 -0300

    To Do List, resolução dos requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textoTarefa = document.querySelector('#texto-tarefa');
 const btnCreateTask = document.querySelector('#criar-tarefa');
 const olList = document.querySelector('#lista-tarefas');
+const itemsList = document.getElementsByClassName('olItem');
 
 // Recupera o valor do input e salva em um array
 const list = [];
@@ -13,6 +14,7 @@ function createList() {
 function setTask() {
   createList();
   const item = document.createElement('li');
+  item.className = 'olItem lineColor';
   item.innerText = list[list.length - 1];
   olList.appendChild(item);
 }
@@ -22,3 +24,24 @@ btnCreateTask.addEventListener('click', () => {
   textoTarefa.value = '';
   textoTarefa.focus();
 });
+
+function removeClassLineColor() {
+  for (let item = 0; item < itemsList.length; item += 1) {
+    if (itemsList[item].className === 'olItem lineColor') {
+      itemsList[item].classList.remove('lineColor');
+    }
+  }
+}
+
+olList.addEventListener('click', function (event) {
+  removeClassLineColor();
+  event.target.classList.toggle('lineColor');
+})
+
+textoTarefa.addEventListener('keypress', function (tecla) {
+  if (tecla.keyCode === 13) {
+    setTask();
+    textoTarefa.value = '';
+    textoTarefa.focus();
+  }
+})
