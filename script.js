41a37e0857f69dbafea0e189a8b95ad4f451895a
alailsonko <aalailson3@gmail.com>
2020-12-15 03:05:19 -0300

    feat: challenge 7 and 8

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener("load", main)
 function main () {
   const tasks = Tasks()
   tasks.createTask()
+  tasks.selectTask()
   loadMsg()
 }
 
@@ -12,10 +13,13 @@ function Factory(){
   const TagButtonAddTask = document.getElementById("criar-tarefa")
   const TagOlListTasks = document.getElementById("lista-tarefas")
   const TagInputerTask = document.getElementById("texto-tarefa")
+  const TagLiItemTasks = document.getElementsByClassName("item-list")
+
   return {
     MyTagButtonAddTask: TagButtonAddTask,
     MyTagOlListTasks: TagOlListTasks,
-    MyTagInputerTask: TagInputerTask
+    MyTagInputerTask: TagInputerTask,
+    MyTagLiItemTasks: TagLiItemTasks
   }
 }
 
@@ -24,27 +28,45 @@ const nodeListTask = null
 function Tasks() {
   const factory = Factory()
 
-  function createTask () {
+  let myTagLiItemTasks = factory.MyTagLiItemTasks
+
+   function createTask () {
     const myTagButtonAddTask = factory.MyTagButtonAddTask
     myTagButtonAddTask.addEventListener("click", function (e) {
       e.preventDefault()
-
       const myTagInputerTask = factory.MyTagInputerTask
       const valueTask = myTagInputerTask.value
       const myTagOlListTasks = factory.MyTagOlListTasks
 
-      myTagOlListTasks.innerHTML += `<li>${valueTask}</li>`
-
+      myTagOlListTasks.innerHTML += `<li class="item-list">${valueTask}</li>`
+      myTagLiItemTasks = factory.MyTagLiItemTasks
+      console.log(factory.MyTagLiItemTasks)
+      selectTask()
     // clean the input after click
     myTagInputerTask.value = null
     })
+  }
+ function selectTask () {
+
+    for(let i = 0; i < myTagLiItemTasks.length; i++) {
+      myTagLiItemTasks[i].addEventListener('click', function(){
+        if(myTagLiItemTasks[i].classList[1]){
+          myTagLiItemTasks[i].classList.remove("selectedTask")
+        } else {
+        for(let j = 0; j < myTagLiItemTasks.length; j++) {
+          myTagLiItemTasks[j].classList.remove("selectedTask")
+        }
+          myTagLiItemTasks[i].classList.add("selectedTask")
+        }
+      })
+    }
+    }
 
     console.log("createTask working")
-  }
   return {
+    selectTask: selectTask,
     createTask: createTask
   }
-
 }
 
 
