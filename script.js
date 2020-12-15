0e0a26788304638ad46347bd1c7314bce8d32a4c
J.Henrique <jhenriqueribeirodesa@gmail.com>
2020-12-15 10:18:37 -0300

    fix code climate semicolon

--- script.js
+++ script.js
@@ -3,29 +3,29 @@
  * @param {HTMLOListElement} taskListElement
  */
 function handleAddTask(inputElement, taskListElement) {
-  return function (event) {
-    const inputText = inputElement.value.trim()
-    const listItem = document.createElement('li')
-    listItem.innerText = inputText
+  return function () {
+    const inputText = inputElement.value.trim();
+    const listItem = document.createElement('li');
+    listItem.innerText = inputText;
 
-    if (!inputText) return
+    if (!inputText) return;
 
-    taskListElement.appendChild(listItem)
-    inputElement.value = ''
-    inputElement.focus()
+    taskListElement.appendChild(listItem);
+    inputElement.value = '';
+    inputElement.focus();
   }
 }
 
 window.onload = () => {
-  const buttonAddTask = document.getElementById('criar-tarefa')
-  const inputTask = document.getElementById('texto-tarefa')
-  const taskList = document.getElementById('lista-tarefas')
-  inputTask.focus()
+  const buttonAddTask = document.getElementById('criar-tarefa');
+  const inputTask = document.getElementById('texto-tarefa');
+  const taskList = document.getElementById('lista-tarefas');
+  inputTask.focus();
 
-  buttonAddTask.addEventListener('click', handleAddTask(inputTask, taskList))
+  buttonAddTask.addEventListener('click', handleAddTask(inputTask, taskList));
   inputTask.addEventListener('keypress', (event) => {
     if(event.key === 'Enter') {
-      handleAddTask(inputTask, taskList)(event)
+      handleAddTask(inputTask, taskList)();
     }
-  })
+  });
 };
