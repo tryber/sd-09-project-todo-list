0a1363a9cf7737feaabdb37e57c47e27ea87938a
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-15 18:45:05 -0300

    Add selected class

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function () {
   createOrganizedList();
+  const taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('click', updateTaskColor);
 }
 
 function createOrganizedList() {
@@ -15,3 +17,72 @@ function createOrganizedList() {
     input.value = '';
   });
 }
+
+function removeClassSelected() {
+  const tasksSelected = document.querySelectorAll('.selected');
+  for (let index = 0; index < tasksSelected.length; index += 1) {
+    tasksSelected[index].classList.remove('selected');
+  }
+}
+
+function updateBackgroundColor() {
+  const tasks = document.querySelectorAll('li');
+  for (let index = 0; index < tasks.length; index += 1) {
+    if (tasks[index].classList.contains('selected')) {
+      tasks[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
+      tasks[index].style.backgroundColor = null;
+    }
+  }
+}
+
+function updateTaskColor(event) {
+  const selectedItem = event.target;
+  if (selectedItem.classList.contains('tarefa')) {
+    removeClassSelected();
+    selectedItem.className += ' selected';
+  }
+  updateBackgroundColor();
+}
+
+/*
+function changeItemBackgroundColor() {
+  const itens = document.getElementsByClassName('tarefa');
+  for (let index = 0; index < itens.lenght; index += 1) {
+    itens[index].addEventListener('click', function (event) {
+      for (let index2 = 0; index2 < itens.lenght; index2 += 1) {
+        itens[index2].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    });
+  }
+}
+*/
+
+/*
+function changeItemBackgroundColor() {
+  const itens = document.querySelectorAll('li');
+  const organizedList = document.getElementById('lista-tarefas');
+  organizedList.addEventListener('click', function (event) {
+    for (let index = 0; index < itens.lenght; index += 1) {
+      organizedList[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+}
+*/
+
+/*
+function changeItemBackgroundColor() {
+  const itens = document.querySelectorAll('li');
+  const organizedList = document.getElementById('lista-tarefas');
+  organizedList.addEventListener('click', function (event) {
+    for (let index = 0; index < organizedList.length; index += 1) { 
+      if (organizedList[index].className === 'selected') {
+        organizedList[index].classList.remove('selected');
+      }
+    }
+    event.target.classList.add('selected');
+  });
+}
+*/
