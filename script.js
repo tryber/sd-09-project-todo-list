94f58c4f0528e0fc204e6cdd23e9cd080b0c163d
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-15 16:09:22 -0300

    Code Climate: adjusted identation in javascripit.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Create a list item (li) child of a ordered list (ol)
 function cresteListItem(task) {
-	const orderedList = document.querySelector('#lista-tarefas');
-	const listItem = document.createElement('li');
-	listItem.innerText = task;
-	listItem.className = 'tarefa';
-	orderedList.appendChild(listItem);
+  const orderedList = document.querySelector('#lista-tarefas');
+  const listItem = document.createElement('li');
+  listItem.innerText = task;
+  listItem.className = 'tarefa';
+  orderedList.appendChild(listItem);
   return listItem;
 }
 
@@ -15,21 +15,21 @@ function clearInput(inputValue) {
 
 // Create task(li) in oredered list(ol)
 function addTask() {
-	const taskInput = document.querySelector('#texto-tarefa');
-	cresteListItem(taskInput.value);
-	clearInput(taskInput);
+  const taskInput = document.querySelector('#texto-tarefa');
+  cresteListItem(taskInput.value);
+  clearInput(taskInput);
 }
 
 // Change background-color of elements with class selected
 function updateBackgroundColor() {
-	const tasks = document.querySelectorAll('li');
-	for (let index = 0; index < tasks.length; index += 1 ) {
-		if (tasks[index].classList.contains('selected')) {
-			tasks[index].style.backgroundColor = 'rgb(128, 128, 128)';
-		} else {
-			tasks[index].style.backgroundColor = null;
-		}
-	}
+  const tasks = document.querySelectorAll('li');
+  for (let index = 0; index < tasks.length; index += 1 ) {
+    if (tasks[index].classList.contains('selected')) {
+      tasks[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
+      tasks[index].style.backgroundColor = null;
+    }
+  }
 }
 
 // Update text-decoration line-through of item
@@ -45,8 +45,8 @@ function updateLineThrough(item) {
 
 // Remove the class 'selected' form li elements
 function removeClassSelected() {
-	const tasksSelected = document.querySelectorAll('.selected');
-	for (let index = 0; index < tasksSelected.length; index += 1) {
+  const tasksSelected = document.querySelectorAll('.selected');
+  for (let index = 0; index < tasksSelected.length; index += 1) {
     tasksSelected[index].classList.remove('selected');
   }
 }
@@ -54,24 +54,24 @@ function removeClassSelected() {
 // Remove the class 'completed' form li elements
 function removeClassCompleted() {
   const tasksCompleted = document.querySelectorAll('.completed');
-	for (let index = 0; index < tasksCompleted.length; index += 1) {
+  for (let index = 0; index < tasksCompleted.length; index += 1) {
     tasksCompleted[index].classList.remove('completed');
   }
 }
 
 // Add class selected and call the function that update background-color of selected itens
 function updateTaskColor(event) {
-	const selectedItem = event.target;
-	if( selectedItem.classList.contains('tarefa')) {
+  const selectedItem = event.target;
+  if( selectedItem.classList.contains('tarefa')) {
     removeClassSelected();
     selectedItem.className += ' selected';
-	}
-	updateBackgroundColor();
+  }
+  updateBackgroundColor();
 }
 
 // Add class completed and call the function that update text-decoration line-through of item
 function updateTaskCompleted(event) {
-	const completedItem = event.target;
+  const completedItem = event.target;
   if(completedItem.classList.contains('tarefa')) {
     completedItem.className += ' completed';
   }
@@ -163,13 +163,13 @@ function moveDown() {
 
 // Prevent submit of a input in form
 function preventSubmit(event) {
-	event.preventDefault();
+  event.preventDefault();
 }
 
 // Load function and event listeners at the window load
 window.onload = function () {
-	const buttonTask = document.querySelector('#criar-tarefa');
-	const taskForm = document.querySelector('#formulario-tarefa');
+  const buttonTask = document.querySelector('#criar-tarefa');
+  const taskForm = document.querySelector('#formulario-tarefa');
   const taskList = document.querySelector('#lista-tarefas');
   const buttonDelete = document.querySelector('#apaga-tudo');
   const buttonConclude = document.querySelector('#remover-finalizados');
@@ -177,9 +177,9 @@ window.onload = function () {
   const buttonMoveUp = document.querySelector('#mover-cima');
   const buttonMoveDown = document.querySelector('#mover-baixo');
   const buttonRemoveSelected = document.querySelector('#remover-selecionado');
-	buttonTask.addEventListener('click', addTask);
-	taskForm.addEventListener('submit', preventSubmit);
-	taskList.addEventListener('click', updateTaskColor);
+  buttonTask.addEventListener('click', addTask);
+  taskForm.addEventListener('submit', preventSubmit);
+  taskList.addEventListener('click', updateTaskColor);
   taskList.addEventListener('dblclick', updateTaskCompleted);
   buttonDelete.addEventListener('click', deleteAllTasks);
   buttonConclude.addEventListener('click', removeTask);
