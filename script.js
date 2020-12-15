98c4ff73eb5fe09241aa52e74075be9d0e5136a8
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-15 11:28:16 -0300

    created butto for step 5, still without any function

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ function structure() {
   let append = document.getElementById('infoSection');
   let createP = document.createElement('p');
   createP.id = 'funcionamento';
-  createP.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
+  createP.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
   append.appendChild(createP);
   let createOL = document.createElement('ol');
   createOL.id = 'lista-tarefas';
   append.appendChild(createOL);
+  let createButton = document.createElement('button');
+  createButton.id = 'criar-tarefa';  
+  append.appendChild(createButton);
   let createInput = document.createElement('input');
   createInput.id = 'texto-tarefa'
   createInput.setAttribute('type', 'text');
