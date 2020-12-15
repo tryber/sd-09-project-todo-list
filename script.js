3df8b7f234be4c33a167fe683975fa2b99b38ca2
MZTBTG <mztbtg@hotmail.com>
2020-12-15 02:53:47 -0300

    Corrigindo erro quando item chega ao topo

--- script.js
+++ script.js
@@ -21,12 +21,14 @@ function addItemInList() {
 }
 
 function reselecting(event) {
-  if (event.target.className === 'line completed') {
-    event.target.className = 'line selected completed';
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  } else {
-    event.target.className = 'line selected';
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  if (event.target !== undefined) {
+    if (event.target.className === 'line completed') {
+      event.target.className = 'line selected completed';
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
+      event.target.className = 'line selected';
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
   }
 }
 
@@ -71,24 +73,27 @@ function clearAll() {
 
 function moveUp() {
   for (let index = 0; index < line.length; index += 1) {
-    if (line[index].className === 'line selected' || line[index].className === 'line selected completed') {
-      const storeLine = line[index - 1].innerHTML;
-      const storeClassUp = line[index - 1].className;
-      const storeClassDown = line[index].className;
-      line[index - 1].innerHTML = line[index].innerHTML;
-      line[index].innerHTML = storeLine;
-      line[index].className = storeClassUp;
-      line[index].style.backgroundColor = '';
-      line[index - 1].className = storeClassDown;
-      line[index - 1].style.backgroundColor = 'rgb(128, 128, 128)';
-      break;
+    if (line[index - 1]) {
+      if (line[index].className === 'line selected' || line[index].className === 'line selected completed') {
+        const storeLine = line[index - 1].innerHTML;
+        const storeClassUp = line[index - 1].className;
+        const storeClassDown = line[index].className;
+        line[index - 1].innerHTML = line[index].innerHTML;
+        line[index].innerHTML = storeLine;
+        line[index].className = storeClassUp;
+        line[index].style.backgroundColor = '';
+        line[index - 1].className = storeClassDown;
+        line[index - 1].style.backgroundColor = 'rgb(128, 128, 128)';
+        break;
+      }
     }
   }
 }
 
 function moveDown() {
   for (let index = 0; index < line.length; index += 1) {
-    if (line[index].className === 'line selected' || line[index].className === 'line selected completed') {
+    if (line[index + 1]) {
+      if (line[index].className === 'line selected' || line[index].className === 'line selected completed') {
       const storeLine = line[index].innerHTML;
       const storeClassUp = line[index + 1].className;
       const storeClassDown = line[index].className;
@@ -99,6 +104,7 @@ function moveDown() {
       line[index + 1].innerHTML = storeLine;
       line[index + 1].className = storeClassDown;
       break;
+      }
     }
   }
 }
