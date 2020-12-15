786ffe7119a78d7cf8c41e2d939f03bacb56ed2d
cleber-teixeira <cleberlt@gmail.com>
2020-12-15 10:05:11 -0300

    09- Adiciona duplo clique para ítens realizados. :white_check_mark:

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function funcaoAdicionar() {
-  const botao = document.querySelector('#criar-tarefa');
-  botao.addEventListener('click', function () {
-    criarTarefa();
-  })
-}
-
 function criarTarefa() {
   const entradaTextoTarefa = document.getElementById('texto-tarefa');
   const listaTarefas = document.getElementById('lista-tarefas');
@@ -15,25 +8,60 @@ function criarTarefa() {
   entradaTextoTarefa.value = '';
 }
 
-function seleciona() {
-  const selecionar = document.querySelector('#lista-tarefas');
-  selecionar.addEventListener('click', function (event) {
-    let selecao = event.target;
-    limpaSelecao();
-    selecao.className = 'selecionado';
+function funcaoAdicionar() {
+  const botao = document.querySelector('#criar-tarefa');
+  botao.addEventListener('click', function () {
+    criarTarefa();
   });
 }
 
-function limpaSelecao () {
-  let selecao = document.querySelectorAll('li');
-  for (let indice = 0; indice < selecao.length; indice += 1) {
-    if (selecao[indice].className = 'selecionado') {
-      selecao[indice].className = 'tarefa';
-    }
+function limpaSelecao() {
+  const selecao = document.querySelectorAll('li');
+  for (let index = 0; index < selecao.length; index += 1) {
+    selecao[index].classList.remove('selecionado');
   }
+}
+
+function seleciona() {
+  const selecionar = document.querySelector('#lista-tarefas');
+  selecionar.addEventListener('click', function (event) {
+    const selecao = event.target;
+    let cont = 0;
+    for (let index = 0; index < selecao.classList.length; index += 1) {
+      if (selecao.classList[index] === 'selecionado') {
+        cont += 1;
+      }
+    }
+    if (cont === 0) {
+      limpaSelecao();
+      selecao.classList.add('selecionado');
+    } else {
+      limpaSelecao();
+    }
+  });
+}
+
+function tarefaConcluida() {
+  const listaTarefas = document.querySelector('#lista-tarefas');
+  listaTarefas.addEventListener('dblclick', function (event) {
+    const completado = event.target;
+    console.log(completado.classList.length);
+    let cont = 0;
+    for (let index = 0; index < completado.classList.length; index += 1) {
+      if ( completado.classList[index] === 'completed') {
+        cont += 1;
+      }
+    }
+    if (cont === 0) {
+      completado.classList.add('completed');
+    } else {
+      completado.classList.remove('completed');
+    }
+  });
 }
 
 window.onload = function () {
   funcaoAdicionar();
   seleciona();
-}
+  tarefaConcluida();
+};
