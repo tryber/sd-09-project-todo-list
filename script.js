ce17c18204ce46cefe68a13bab4b606d9590b712
Layo Kaminski <layokaminski@gmail.com>
2020-12-15 14:09:55 -0300

    Ajustes Code Climate

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
 window.onload = function () {
   function titleOperation() {
-    let operation = document.createElement('h5');
-    let title = document.querySelector('.title');
+    const operation = document.createElement('h5');
+    const title = document.querySelector('.title');
     title.appendChild(operation);
     operation.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
     operation.id = 'funcionamento';
   }
   titleOperation();
 
-	function createButton() {
-	  let button = document.createElement('button');
+  function createButton() {
+	  const button = document.createElement('button');
 	  button.id = 'criar-tarefa';
 	  button.innerHTML = 'Adicionar Tarefa';
-	  document.body.appendChild(button);
+		document.body.appendChild(button);
   }
   createButton();
 
-	function createContainer() {
-		let container = document.createElement('div')
-		let button = document.querySelector('#criar-tarefa')
-		let input = document.querySelector('#texto-tarefa')
-		let listOrdened = document.querySelector('#lista-tarefas')
-		container.appendChild(input)
-		container.appendChild(button)
-		container.appendChild(listOrdened)
-		document.body.appendChild(container)
-		container.className = 'container'
+  function createContainer() {
+	  const container = document.createElement('div');
+	  const button = document.querySelector('#criar-tarefa');
+	  const input = document.querySelector('#texto-tarefa');
+	  const listOrdened = document.querySelector('#lista-tarefas');
+	  container.appendChild(input);
+	  container.appendChild(button);
+	  container.appendChild(listOrdened);
+	  document.body.appendChild(container);
+	  container.className = 'container';
 	}
-	createContainer()
+  createContainer();
 
-	function addNewTodo() {
-		let button = document.querySelector('#criar-tarefa')
-		let input = document.querySelector('#texto-tarefa')
-		let list = document.querySelector('#lista-tarefas')
+  function addNewTodo() {
+	  const button = document.querySelector('#criar-tarefa');
+	  const input = document.querySelector('#texto-tarefa');
+	  const list = document.querySelector('#lista-tarefas');
 
-		button.addEventListener('click', function () {
-			if (input.value.length > 0) {
-				let newList = document.createElement('li')
-				newList.innerText = input.value
+	  button.addEventListener('click', function () {
+		  if (input.value.length > 0) {
+			  let newList = document.createElement('li');
+			  newList.innerText = input.value;
 
-				newList.className = 'list-items'
-				list.appendChild(newList)
-				input.value = ''
-			}
-		})
+			  newList.className = 'list-items';
+			  list.appendChild(newList);
+			  input.value = '';
+		  }
+	  });
 
-		input.addEventListener('keyup', function (event) {
-			if (event.keyCode === 13 && input.value.length > 0) {
-				let newList = document.createElement('li')
-				newList.innerText = input.value
+	  input.addEventListener('keyup', function (event) {
+		  if (event.keyCode === 13 && input.value.length > 0) {
+			  let newList = document.createElement('li');
+			  newList.innerText = input.value;
 
-				newList.className = 'list-items'
-				list.appendChild(newList)
-				input.value = ''
-			}
-		})
-	}
-	addNewTodo()
+			  newList.className = 'list-items';
+			  list.appendChild(newList);
+			  input.value = '';
+		  }
+	  });
+  };
+  addNewTodo();
 
-	function doubleClickList() {
-		let taskList = document.querySelector('#lista-tarefas')
-		taskList.addEventListener('dblclick', function (event) {
+  function doubleClickList() {
+	  let taskList = document.querySelector('#lista-tarefas');
+	  taskList.addEventListener('dblclick', function (event) {
 
-			event.target.classList.toggle('completed')
-		})
-	}
-	doubleClickList()
+		  event.target.classList.toggle('completed');
+	  });
+  };
+  doubleClickList();
 
 	function clickList() {
 		let task = document.querySelector('#lista-tarefas')
