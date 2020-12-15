3f7e0d7d2a9ff26761ab8977663183ea5214793b
J.Henrique <jhenriqueribeirodesa@gmail.com>
2020-12-15 10:12:53 -0300

    5° Requisito

---
+++ script.js
@@ -0,0 +1,31 @@
+/**
+ * @param {HTMLInputElement} inputElement
+ * @param {HTMLOListElement} taskListElement
+ */
+function handleAddTask(inputElement, taskListElement) {
+  return function (event) {
+    const inputText = inputElement.value.trim()
+    const listItem = document.createElement('li')
+    listItem.innerText = inputText
+
+    if (!inputText) return
+
+    taskListElement.appendChild(listItem)
+    inputElement.value = ''
+    inputElement.focus()
+  }
+}
+
+window.onload = () => {
+  const buttonAddTask = document.getElementById('criar-tarefa')
+  const inputTask = document.getElementById('texto-tarefa')
+  const taskList = document.getElementById('lista-tarefas')
+  inputTask.focus()
+
+  buttonAddTask.addEventListener('click', handleAddTask(inputTask, taskList))
+  inputTask.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+      handleAddTask(inputTask, taskList)(event)
+    }
+  })
+};
