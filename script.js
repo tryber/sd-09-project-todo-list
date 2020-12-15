71cac7a470a814cfefd96bbf5801bb1ec3cd20c5
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-15 17:21:53 -0400

    requisito 10: criando button que apaga todas as taks

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttonAddTasks = document.querySelector('#criar-tarefa');
 const listTask = document.querySelector('#lista-tarefas');
+const buttonDelete = document.querySelector('#apaga-tudo');
 
 function createTask() {
   const inputTask = document.querySelector('#texto-tarefa');
@@ -20,14 +21,23 @@ function bgTaskColorChange(event) {
   }
   event.target.classList.add('selected');
 }
+
 function dbClickCompleted(event) {
   event.target.classList.toggle('completed');
+}
+
+function deleteTask() {
+  const tasksCreated = document.querySelectorAll('.task');
+  for (let index = 0; index < tasksCreated.length; index += 1) {
+    listTask.removeChild(tasksCreated[index]);
+  }
 }
 
 function eventsListenersAll() {
   buttonAddTasks.addEventListener('click', createTask);
   listTask.addEventListener('click', bgTaskColorChange);
   listTask.addEventListener('dblclick', dbClickCompleted);
+  buttonDelete.addEventListener('click', deleteTask);
 }
 
 eventsListenersAll();
