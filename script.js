27256c7bed985892bfef67dfbfea11bda1305b0b
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-15 11:18:47 -0300

    step 1, step 2, step 3 and step 4 changed to script

---
+++ script.js
@@ -0,0 +1,18 @@
+function structure() {
+  let append = document.getElementById('infoSection');
+  let createP = document.createElement('p');
+  createP.id = 'funcionamento';
+  createP.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
+  append.appendChild(createP);
+  let createOL = document.createElement('ol');
+  createOL.id = 'lista-tarefas';
+  append.appendChild(createOL);
+  let createInput = document.createElement('input');
+  createInput.id = 'texto-tarefa'
+  createInput.setAttribute('type', 'text');
+  append.appendChild(createInput);    
+}
+
+window.onload = function() {
+  structure();
+}
