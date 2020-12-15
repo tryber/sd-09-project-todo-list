17a0de4df90f6f1df8453cf6b5c5bd2cf1ddb1b7
Poeta <pedrotrybe@gmail.com>
2020-12-15 20:03:08 -0300

    Adicionando botao de texto

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
 function createTitle() {
-  const headerAcess = document.querySelector('header');
+  const headerAccess = document.querySelector('header');
   const definitionTitle = document.createElement('h1');
-  headerAcess.appendChild(definitionTitle);
+  headerAccess.appendChild(definitionTitle);
   definitionTitle.innerText = 'Minha Lista de Tarefas';
-  definitionTitle.style.color = 'violet';
-  definitionTitle.style.textAlign = 'center';
 }
 
 function createParagraph() {
-  const mainAcess = document.querySelector('main');
+  const mainAccess = document.querySelector('main');
   const definitionParagraph = document.createElement('p');
-  mainAcess.appendChild(definitionParagraph);
+  mainAccess.appendChild(definitionParagraph);
   definitionParagraph.innerText =
     'Clique duas vezes em um item para marcá-lo como completo';
   definitionParagraph.id = 'funcionamento';
 }
 
+function createInputTextArea() {
+  const mainAccess = document.querySelector('main');
+  const definitionInnerTextArea = document.createElement('input');
+  mainAccess.appendChild(definitionInnerTextArea);
+  definitionInnerTextArea.type = 'text';
+  definitionInnerTextArea.id = 'texto-tarefa';
+  definitionInnerTextArea.placeholder = 'Digite seu texto aqui!';
+}
+
+function createOrderedList() {
+  const mainAccess = document.querySelector('main');
+  const definitionOrderedList = document.createElement('ol');
+  mainAccess.appendChild(definitionOrderedList);
+  definitionOrderedList.id = 'lista-tarefas';
+}
+
+function createButtonAddItem() {
+  const mainAccess = document.querySelector('main');
+  const definitionButtonAddItem = document.createElement('button');
+  mainAccess.appendChild(definitionButtonAddItem);
+  definitionButtonAddItem.id = 'criar-tarefa';
+  definitionButtonAddItem.innerText = 'Criar tarefa';
+}
+
+function clickAddItem() {
+  const buttonAccess = document.querySelector('#criar-tarefa');
+  const olAccess = document.querySelector('#lista-tarefas');
+
+  buttonAccess.addEventListener('click' , function () {
+    const elementList = document.createElement('li');
+    const input = document.querySelector('#texto-tarefa');
+    const inputValue = document.querySelector('#texto-tarefa').value;
+    olAccess.appendChild(elementList);
+    elementList.innerText = inputValue;
+    input.value = '';
+  });
+}
+
 window.onload = function () {
   createTitle();
   createParagraph();
+  createInputTextArea();
+  createOrderedList();
+  createButtonAddItem();
+  clickAddItem();
 };
