7905e1d0a3bd062cdedab8cde86dc7caa84e102d
Felipe Shcolnik <shcolnik@gmail.com>
2020-12-15 20:26:47 -0300

    intem 5

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// window.onload = function() {
+window.onload = function() {
+  createItemList()
+}
 
-// }
+function createItemList() {
+  const buttom = document.getElementById('criar-tarefa')
+  const list = document.getElementById('lista-tarefas')
+  const generateItem = buttom.addEventListener('click', function() {
+    const createItem = document.createElement('li')
+    list.appendChild(createItem)
+    const imputText = document.getElementById('texto-tarefa').value;
+    const textBox = document.getElementById('texto-tarefa');
+    createItem.innerText = imputText;
+    textBox.value = '';
+  })
+}
 
-// function createItemList() {
-//   const textButton = document.getElementById('texto-tarefa').value;
-//   const text = text.addEventListener('type', function() {
-
-//   })
-// }
-
-// function addItemList() {
-
-// }s
