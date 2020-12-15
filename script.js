ae60a008ae1794b1b380f9eb72fceccbfd54e018
cleber-teixeira <cleberlt@gmail.com>
2020-12-14 21:43:43 -0300

    07|08- Adiciona seleção a um elemento. :white_check_mark:

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
+function funcaoAdicionar() {
+  const botao = document.querySelector('#criar-tarefa');
+  botao.addEventListener('click', function () {
+    criarTarefa();
+  })
+}
+
 function criarTarefa() {
-  const inputTextoTarefa = document.getElementById('texto-tarefa');
+  const entradaTextoTarefa = document.getElementById('texto-tarefa');
   const listaTarefas = document.getElementById('lista-tarefas');
   const novaTarefa = document.createElement('li');
   novaTarefa.className = 'tarefa';
-  novaTarefa.innerText = inputTextoTarefa.value;
-  console.log(listaTarefas);
+  novaTarefa.innerText = entradaTextoTarefa.value;
   listaTarefas.appendChild(novaTarefa);
-  inputTextoTarefa.value = '';
+  entradaTextoTarefa.value = '';
 }
 
+function seleciona() {
+  const selecionar = document.querySelector('#lista-tarefas');
+  selecionar.addEventListener('click', function (event) {
+    let selecao = event.target;
+    limpaSelecao();
+    selecao.className = 'selecionado';
+  });
+}
+
+function limpaSelecao () {
+  let selecao = document.querySelectorAll('li');
+  for (let indice = 0; indice < selecao.length; indice += 1) {
+    if (selecao[indice].className = 'selecionado') {
+      selecao[indice].className = 'tarefa';
+    }
+  }
+}
+
+window.onload = function () {
+  funcaoAdicionar();
+  seleciona();
+}
