d75ad3b7c71ba44348b634c5f3d8c68913122d35
Pedro Henrique <pedroenrique.400@gmail.com>
2020-12-14 22:15:29 -0300

    Faz o requisito 8

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ button.id = 'criar-tarefa';
 button.innerHTML = 'Criar tarefa';
 body.appendChild(button);
 textBar = document.querySelector('input');
+let elementsOfList = [];
 window.onload = function() {
     button.addEventListener('click', function() {
         let itens = document.createElement('li');
         itens.innerHTML = textBar.value;
+        elementsOfList.push(itens);
         orderedList.appendChild(itens);
         textBar.value = '';
     });
     orderedList.addEventListener('click', function(event) {
-        event.target.style.backgroundColor = 'rgb(128,128,128)';
-
+        for (let index = 0; index < elementsOfList.length; index +=1) {
+            if (elementsOfList[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+                elementsOfList[index].style.backgroundColor = ''
+            }
+        }
+         event.target.style.backgroundColor = 'rgb(128,128,128)';
     });
 }
