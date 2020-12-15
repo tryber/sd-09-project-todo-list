294913a28481e3361d04fd39458cb16ad0890cba
IvanRafael <ivanrafael.dev@gmail.com>
2020-12-15 18:45:27 -0300

    Requisito 1 e 2

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-window.onload = function () {
-    createTitle();
+function createTitle() {
+  // armazenar o elemento pai de onde será criado o elemento novo
+  const header = document.querySelector('header');
+  // agora criamos o elemento que queremos
+  const heading = document.createElement('h1');
+  // o pai header ele ta recebendo o heading como filho
+  // o nosso h1 vai entrar dentro do header
+  header.appendChild(heading);
+  // coloca texto no h1
+  heading.innerText = 'Minha Lista de Tarefas';
+  heading.style.color = 'red';
+  heading.style.textAlign = 'center';
+  heading.style.border = '1px solid purple';
 }
 
-function createTitle() {
-    const header = document.getElementsByTagName('header');
-    const h1 = document.createElement('h1');
-    h1.innerText = 'Minha Lista de Tarefas';
-    h1.style.color = 'red';
-    h1.style.textAlign = 'center';
-    h1.style.fontSize = '50px';
-    header[0].appendChild(h1);
+function createParagraph() {
+  const main = document.querySelector('main');
+  const paragraph = document.createElement('p');
+  paragraph.innerText =
+    'Clique duas vezes em um item para marcá-lo como completo';
+  paragraph.id = 'funcionamento';
+  main.appendChild(paragraph);
 }
+
+function createInput() {
+  const main = document.querySelector('main');
+  const input = document.createElement('input');
+  input.id = 'texto-tarefa';
+  input.type = 'text';
+  input.placeholder = 'Digite o item que deseja adicionar a lista';
+  main.appendChild(input);
+}
+
+window.onload = function () {
+  createTitle();
+  createParagraph();
+  createInput();
+};
