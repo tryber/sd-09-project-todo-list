6e4caa6b56fee0f06496c603793d7454e533f5ee
Hudson Carlos <hc88806@gmail.com>
2020-12-16 11:09:59 -0400

    retirando espaço do arquivo script

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ window.onload = function () {
   const list = document.querySelector('#lista-tarefas');
   const buttonAddLesson = document.querySelector('#criar-tarefa');
   buttonAddLesson.addEventListener('click', function () {
-      if (imput.value !== '' && imput.value[0] !== ' ') {
-          const newLesson = document.createElement('li');
-          newLesson.innerText = imput.value;
-          list.appendChild(newLesson);
-          imput.value = '';
-        }
+    if (imput.value !== '' && imput.value[0] !== ' ') {
+        const newLesson = document.createElement('li');
+        newLesson.innerText = imput.value;
+        list.appendChild(newLesson);
+        imput.value = '';
+      }
   });
   list.addEventListener('click', function () {
-      const line = document.querySelectorAll('li')
-      for (const x = 0; x < line.length; x += 1){
-          line[x].style.backgroundColor = 'white';
-        }
-    });
+    const line = document.querySelectorAll('li')
+    for (const x = 0; x < line.length; x += 1){
+        line[x].style.backgroundColor = 'white';
+      }
+  });
   list.addEventListener('click', function (event) {
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)'
-    });
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+  });
 };
