161a49148163f4664e65e2bca049236475898e18
Hudson Carlos <hc88806@gmail.com>
2020-12-16 11:49:46 -0400

    corrigindo requisito 7 e 8

--- script.js
+++ script.js
@@ -4,19 +4,17 @@ window.onload = function () {
   const buttonAddLesson = document.querySelector('#criar-tarefa');
   buttonAddLesson.addEventListener('click', function () {
     if (imput.value !== '' && imput.value[0] !== ' ') {
-        const newLesson = document.createElement('li');
-        newLesson.innerText = imput.value;
-        list.appendChild(newLesson);
-        imput.value = '';
-      }
-  });
-  list.addEventListener('click', function () {
-    const line = document.querySelectorAll('li')
-    for (const x = 0; x < line.length; x += 1){
-        line[x].style.backgroundColor = 'white';
-      }
+      const newLesson = document.createElement('li');
+      newLesson.innerText = imput.value;
+      list.appendChild(newLesson);
+      imput.value = '';
+    }
   });
   list.addEventListener('click', function (event) {
+    const line = document.querySelectorAll('li') 
+    for (let x = 0; x < line.length; x += 1) {
+      line[x].removeAttribute('style');
+    }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)'
   });
 };
