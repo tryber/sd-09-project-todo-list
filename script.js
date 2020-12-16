99eca357353806da9f49afb68c7a9340071314c7
Felipe Shcolnik <shcolnik@gmail.com>
2020-12-16 15:22:30 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-window.onload = function() {
-  main();
-}
-function main(){
+function main (){
   createItemList();
 }
 
-function createItemList() {
+function createItemList () {
   const buttom = document.getElementById('criar-tarefa');
   const list = document.getElementById('lista-tarefas');
-  const generateItemByClick = buttom.addEventListener('click', function() {
+  const generateItemByClick = buttom.addEventListener('click', function () {
     const createItem = document.createElement('li');
     createItem.classList.add('list');
     const imputText = document.getElementById('texto-tarefa').value;
@@ -17,14 +14,18 @@ function createItemList() {
     createItem.innerText = imputText;
     textBox.value = '';
     greybackground();
-  })
+  });
 }
 
-function greybackground() {
+function greybackground () {
   const list2 = document.querySelectorAll('.list');
-  for (let index = 0; index < list2.length; index +=1) {
-   list2[index].addEventListener('click', function() {
-     list2[index].style.backgroundColor = 'rgb(128,128,128)';
+  for (let index = 0; index < list2.length; index += 1) {
+    list2[index].addEventListener('click', function() {
+    list2[index].style.backgroundColor = 'rgb(128,128,128)';
     });
   }
 }
+
+window.onload = function () {
+  main();
+};
