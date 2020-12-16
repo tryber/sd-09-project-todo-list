d20b083164c858a10bb5e7492734257df3759155
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-16 11:10:30 -0300

    add function to clean up inputed elements after assigment

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ function structure() {
   createP.id = 'funcionamento';
   createP.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
   append.appendChild(createP);
-
   let createButton = document.createElement('button');
   createButton.id = 'criar-tarefa';
   append.appendChild(createButton);
-
   let createInput = document.createElement('input');
   createInput.id = 'texto-tarefa'
   createInput.setAttribute('type', 'text');
+  createInput.name = "inputAssigment"
   append.appendChild(createInput);
-
   let createOL = document.createElement('ol');
   createOL.id = 'lista-tarefas';
   append.appendChild(createOL);
@@ -27,20 +25,28 @@ function addAssignment() {
     let addLI = document.createElement('li');    
     let getID = document.getElementById('lista-tarefas');
     getID.appendChild(addLI);
-    addLI.innerHTML = getInput;
-  }  
+    addLI.innerHTML = getInput;   
+  }
 }
-
-
-
+// href: https://pt.stackoverflow.com/questions/299730/limpando-campos-do-formul%C3%A1rio
+// for each inputed element on 'inputAssigment' change value for ' ';
+// function to clean up inputed elements after assigment
+function cleanAssigmentInput() {
+  let inputed = document.getElementsByName("inputAssigment");
+  inputed.forEach(element => {    
+    element.value = '';
+  })
+}
 // created a function to encapsulate all listeners/function callers
 function listeners() {
 let getInput = document.getElementById('criar-tarefa');
-getInput.addEventListener('click', addAssignment)
+getInput.addEventListener('click', addAssignment);
+let cleanInput = document.getElementById('criar-tarefa');
+cleanInput.addEventListener('click', cleanAssigmentInput);
 }
 
 window.onload = function() {
-  structure();
+  structure();  
   listeners();
 }
 // pixel art => modificar botão para input de valores; acrescentar função no VQV onde let base = ' ' copiar função de criação de base, adicionar if input tem numero adicionar  valor a base, if input < 5 || > 50 alert, if input é texto e não numero alert
