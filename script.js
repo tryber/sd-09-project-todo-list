1096826897ab60df05752bb4dece2fabfb30c233
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2020-12-16 14:11:56 -0300

    alterando funcoes

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
-const getList = document.getElementById('lista-tarefas');
-const getTitle = document.getElementById('title');
-let indexList = 0;
-const resolverErroSelected = document.createElement('div');
-resolverErroSelected.className = 'selected';
-getTitle.appendChild(resolverErroSelected);
+window.onload = function() {
+  let getAddButton = document.getElementById('criar-tarefa');
+  getAddButton.addEventListener('click', addTask);
+  const getRemoveBtn = document.getElementById('apaga-tudo');
+  getRemoveBtn.addEventListener('click', removeAll);
+  const getRemoveSelectedBtn = document.getElementById('remover-finalizados');
+  getRemoveSelectedBtn.addEventListener('click', removeSelected);
+};
 
-function coloringBackgroundListItens(event) {
-  const actualColoredItem = event.target;
-  const selectedItem = document.querySelector('.selected');
-  selectedItem.style.backgroundColor = 'white';
-  selectedItem.classList.remove('selected');
-  actualColoredItem.classList.add('selected');
-  actualColoredItem.style.backgroundColor = 'rgb(128 , 128 , 128)';
+function addTask() {
+  let getInputValue = document.getElementById('texto-tarefa');
+  let getList = document.getElementById('lista-tarefas');
+  let createLists = document.createElement('li');
+  getList.appendChild(createLists);
+  createLists.addEventListener('dblclick', selectTask);
+  createLists.addEventListener('click', addBackgroundList);
+  createLists.innerText = getInputValue.value;
+  getInputValue.value = '';
 }
 
-function lineThroughList(event) {
-  const actualLineThroughItem = event.target;
-  for (let index = 0; index < actualLineThroughItem.classList.length; index += 1) {
-    if (actualLineThroughItem.classList[index] !== 'completed') {
-      actualLineThroughItem.classList.add('completed');
-      break;
-    } else {
-      actualLineThroughItem.classList.remove('completed');
-      break;
-    }
+function selectTask(event) {
+  let getTargetList = event.target;
+  let toggleTeste = getTargetList.classList.toggle('lined');
+}
+
+function addBackgroundList(event) {
+  const listOfTasks = document.querySelector('.completed');
+  if (listOfTasks !== null) {
+    listOfTasks.classList.remove('completed');
+  }
+  event.target.classList.toggle('completed');
+}
+
+function removeAll() {
+  const getLists = document.getElementById('lista-tarefas');
+  const teste = getLists.children.length - 1;
+  for (let index = teste; index => teste; index -= 1) {
+    getLists.removeChild(getLists.children[index]);
   }
 }
 
-function listReceiveEvent() {
-  const listChildrens = getList.children;
-  listChildrens[indexList].addEventListener('dblclick', lineThroughList);
-  listChildrens[indexList].addEventListener('click', coloringBackgroundListItens);
-  indexList += 1;
+function removeSelected() {
+  const getSelecteds = document.getElementsByClassName('lined');
+  const getLists = document.getElementById('lista-tarefas');
+  const teste = getSelecteds.length - 1;
+  for (let index = teste; index => teste; index -= 1) {
+    getLists.removeChild(getSelecteds[index]);
+  }
 }
-
-function botaoCriarTarefa() {
-  const getInputValue = document.getElementById('texto-tarefa').value;
-  const newList = document.createElement('li');
-  getList.appendChild(newList);
-  newList.innerText = getInputValue;
-  document.getElementById('texto-tarefa').value = '';
-  listReceiveEvent();
-}
-
-const botao = document.querySelector('#criar-tarefa');
-botao.addEventListener('click', botaoCriarTarefa);
