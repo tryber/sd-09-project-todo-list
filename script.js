a705d17b876bf44649a77a13b6bc9a3337fa382e
Felipe Shcolnik <shcolnik@gmail.com>
2020-12-16 14:56:46 -0300

    background cinza

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 window.onload = function() {
-  createItemList()
+  main();
+}
+function main(){
+  createItemList();
 }
 
 function createItemList() {
-  const buttom = document.getElementById('criar-tarefa')
-  const list = document.getElementById('lista-tarefas')
-  const generateItem = buttom.addEventListener('click', function() {
-    const createItem = document.createElement('li')
-    list.appendChild(createItem)
+  const buttom = document.getElementById('criar-tarefa');
+  const list = document.getElementById('lista-tarefas');
+  const generateItemByClick = buttom.addEventListener('click', function() {
+    const createItem = document.createElement('li');
+    createItem.classList.add('list');
     const imputText = document.getElementById('texto-tarefa').value;
+    list.appendChild(createItem);
     const textBox = document.getElementById('texto-tarefa');
     createItem.innerText = imputText;
     textBox.value = '';
+    greybackground();
   })
 }
 
+function greybackground() {
+  const list2 = document.querySelectorAll('.list');
+  for (let index = 0; index < list2.length; index +=1) {
+   list2[index].addEventListener('click', function() {
+     list2[index].style.backgroundColor = 'rgb(128,128,128)';
+    });
+  }
+}
