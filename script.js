6ca4aff4a8cb25a5ead0f689926565ad52e28867
Hudson Carlos <hc88806@gmail.com>
2020-12-16 13:40:55 -0400

    adicionando requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const imput = document.querySelector('#texto-tarefa');
   const list = document.querySelector('#lista-tarefas');
+  const clear = document.querySelector('#apaga-tudo');
   const buttonAddLesson = document.querySelector('#criar-tarefa');
   buttonAddLesson.addEventListener('click', function () {
     if (imput.value !== '' && imput.value[0] !== ' ') {
@@ -18,10 +19,15 @@ window.onload = function () {
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   });
   list.addEventListener('dblclick', function (event) {
-      if (event.target.className === 'completed') {
-        event.target.classList.remove('completed');
-      } else {
-        event.target.classList.add('completed');
-      }
+    if (event.target.className === 'completed') {
+    event.target.classList.remove('completed');
+    } else {
+    event.target.classList.add('completed');
+    }
+  });
+  clear.addEventListener('click', function () {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
   });
 };
