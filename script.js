7f05ad56d7d4e779186155c6a49eb777b02c2e32
Maximilian Kaden <mkadenfilgo@gmail.com>
2020-12-17 18:02:21 -0300

    CodeClimate

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-function addEventsListener() {
-  //  Escutador no botão
-  const addTaskButton = document.querySelector('#criar-tarefa');
-  addTaskButton.addEventListener('click',addTaskOnList);
-  //  Escutador na lista
-  const listDiv = document.querySelector('#lista-tarefas');
-  listDiv.addEventListener('click',changeBackgroundColor);
-  listDiv.addEventListener("dblclick",setCompleted)
-  //  Escutador do botão apaga tudo
-  const deleteAllButton = document.querySelector('#apaga-tudo');
-  deleteAllButton.addEventListener('click',deleteAllLists)
-  //  Escutador do botão remover selecionados
-  const buttonRemoveCompleted = document.querySelector('#remover-finalizados');
-  buttonRemoveCompleted.addEventListener('click',removeCompletedElements)
-}
-
 function addTaskOnList() {
-  console.log(`Executando função addTaskOnList`)
   const taskText = document.querySelector('#texto-tarefa');
   const taskList = document.querySelector('#lista-tarefas');
-  let task = document.createElement('li');
+  const task = document.createElement('li');
   task.textContent = taskText.value;
   taskList.appendChild(task);
   taskText.value = '';
@@ -26,33 +9,47 @@ function addTaskOnList() {
 
 function changeBackgroundColor(event) {
   const grayList = document.querySelector('.gray');
-  if (grayList != undefined) {
-    grayList.classList.remove('gray')
+  if (grayList !== undefined) {
+    grayList.classList.remove('gray');
   }
-  let lista = event.target
-  lista.classList.add('gray')
-  console.log(lista);
+  const lista = event.target;
+  lista.classList.add('gray');
 }
 
 function setCompleted(event) {
-  console.log(`Executando função set completed`)
-  let element = event.target
-  element.classList.toggle('completed')
+  const element = event.target;
+  element.classList.toggle('completed');
 }
 
 function deleteAllLists() {
   const taskList = document.querySelector('#lista-tarefas');
-  for (;taskList.firstChild != undefined;){
-    taskList.removeChild(taskList.firstChild)
+  for (;taskList.firstChild !== undefined;) {
+    taskList.removeChild(taskList.firstChild);
   }
 }
 
 function removeCompletedElements() {
-  let completedElements = document.querySelectorAll('.completed')
-  for (;completedElements[0] != undefined;) {
-    console.log('Executou o laço')
-    completedElements[0].remove()
-    completedElements = document.querySelectorAll('.completed')
+  let completedElements = document.querySelectorAll('.completed');
+  for (;completedElements[0] !== undefined;) {
+    completedElements[0].remove();
+    completedElements = document.querySelectorAll('.completed');
   }
 }
+
+function addEventsListener() {
+//  Escutador no botão
+  const addTaskButton = document.querySelector('#criar-tarefa');
+  addTaskButton.addEventListener('click', addTaskOnList);
+  //  Escutador na lista
+  const listDiv = document.querySelector('#lista-tarefas');
+  listDiv.addEventListener('click', changeBackgroundColor);
+  listDiv.addEventListener('dblclick', setCompleted);
+  //  Escutador do botão apaga tudo
+  const deleteAllButton = document.querySelector('#apaga-tudo');
+  deleteAllButton.addEventListener('click', deleteAllLists);
+  //  Escutador do botão remover selecionados
+  const buttonRemoveCompleted = document.querySelector('#remover-finalizados');
+  buttonRemoveCompleted.addEventListener('click', removeCompletedElements);
+}
+
 window.onload = addEventsListener();
