c5771beac097b727f1a3788120bf32e1b40e9aec
Hudson Carlos <hc88806@gmail.com>
2020-12-18 16:30:07 -0400

    adicionado requisito 13

--- script.js
+++ script.js
@@ -15,17 +15,16 @@ window.onload = function () {
   });
   list.addEventListener('click', function (event) {
     const line = document.querySelectorAll('li');
+    if (line[0].previousSibling !== null) {
+      list.removeChild(line[0].previousSibling);
+    }
     for (let x = 0; x < line.length; x += 1) {
       line[x].removeAttribute('style');
     }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   });
   list.addEventListener('dblclick', function (event) {
-    if (event.target.className === 'completed') {
-      event.target.classList.remove('completed');
-    } else {
-      event.target.classList.add('completed');
-    }
+    event.target.classList.toggle('completed');
   });
   clear.addEventListener('click', function () {
     while (list.firstChild) {
@@ -47,6 +46,36 @@ window.onload = function () {
       if(line[x].style.backgroundColor === 'rgb(128, 128, 128)') {
         list.removeChild(line[x]);
       }
-    }    
+    }
+  });
+  const up = document.querySelector('#mover-cima');
+  up.addEventListener('click', function () {
+    line = document.querySelectorAll('li');
+    for (let x = 0; x < line.length; x += 1) {
+      if(line[x].style.backgroundColor === 'rgb(128, 128, 128)' && line[x].previousSibling !== null) {
+        const save = line[x -1].innerText;
+        line[x - 1].innerText = line[x].innerText;
+        for (let x = 0; x < line.length; x += 1) {
+          line[x].removeAttribute('style');
+        }
+        line[x - 1].style.backgroundColor = 'rgb(128, 128, 128)';
+        line[x].innerText = save;
+      }
+    }
+  });
+  const low = document.querySelector('#mover-baixo');
+  low.addEventListener('click', function () {
+    const line = document.querySelectorAll('li');
+    for (let x = line.length-1; x >= 0; x -= 1) {
+      if(line[x].style.backgroundColor === 'rgb(128, 128, 128)' && line[x].nextElementSibling !== null) {
+        const save2 = line[x].nextElementSibling.innerText;
+        line[x].nextElementSibling.innerText = line[x].innerText;
+        for (let x = 0; x < line.length; x += 1) {
+          line[x].removeAttribute('style');
+        }
+        line[x].nextElementSibling.style.backgroundColor = 'rgb(128, 128, 128)';
+        line[x].innerText = save2;
+      }
+    }
   });
 };
