75d282b54b5b47697f0274401b553b746302311f
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-20 18:57:04 -0300

    Adicionados os requisitos 4, 5 e 6 ao projeto.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-window.onload {
-  //calma cocada
-}
+// window.onload = function () {
+  //aproveitar o onload para recuperar o que estiver salvo no local storage.
+// }
+
+const myTaskList = document.getElementById('lista-tarefas');
+const myTaskInput = document.getElementById('texto-tarefa');
+const buttonAddTask = document.getElementById('criar-tarefa');
+
+buttonAddTask.addEventListener('click', function () {
+  const myElement = document.createElement('li');
+  let myTask = myTaskInput.value;
+  myElement.innerHTML = myTask;
+  myTaskList.appendChild(myElement);
+  myTaskInput.value = '';
+})
