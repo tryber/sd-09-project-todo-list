b1046799a409115bc15f964bfcac9e4b362cf830
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-12-21 19:52:33 -0300

    Exercicio 5

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function addTask() {
-  const inputTask = document.getElementById('texto-tarefa');
-  if (inputTask.value !== '') {
-    const ol = document.getElementById('lista-tarefas');
-    const li = document.createElement('li');
-    li.innerHTML = inputTask.value;
-    ol.appendChild(li);
-    inputTask.value = '';
-  }
+  const button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', function () {
+    const inputTask = document.getElementById('texto-tarefa');
+    if (inputTask.value !== '') {
+      const ol = document.getElementById('lista-tarefas');
+      const li = document.createElement('li');
+      li.innerHTML = inputTask.value;
+      ol.appendChild(li);
+      inputTask.value = '';
+    }
+  });
 }
+
+window.onload = function () {
+  addTask();
+};
