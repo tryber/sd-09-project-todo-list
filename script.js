524353d2b8956e91b10761ce8c8bd36e96f438fa
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-21 15:38:16 -0300

    Adicionado o botao remover todos os itens completos

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const myTaskList = document.getElementById('lista-tarefas');
 const myTaskInput = document.getElementById('texto-tarefa');
 const buttonAddTask = document.getElementById('criar-tarefa');
 const buttonRemoveAll = document.getElementById('apaga-tudo');
+const buttonRemoveCompleted = document.getElementById('remover-finalizados');
 
 buttonAddTask.addEventListener('click', function () {
   const myElement = document.createElement('li');
@@ -34,5 +35,11 @@ buttonRemoveAll.addEventListener('click', function () {
   const myTasks = myTaskList.children;
   while (myTasks.length > 0) {
     myTaskList.removeChild(myTasks[0]);
+  }
+});
+buttonRemoveCompleted.addEventListener('click', function () {
+  let completedClass = document.getElementsByClassName('completed');
+  while (completedClass.length > 0) {
+    completedClass[0].remove()
   }
 });
