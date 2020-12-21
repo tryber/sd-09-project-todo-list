a44ff78521ed2e7d14c15ae257449f8a2b146570
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-12-21 19:34:04 -0300

    Exercicio 5

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addTask() {
   const inputTask = document.getElementById('texto-tarefa');
   if (inputTask.value !== '') {
     const ol = document.getElementById('lista-tarefas');
-    let li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerHTML = inputTask.value;
     ol.appendChild(li);
     inputTask.value = '';
