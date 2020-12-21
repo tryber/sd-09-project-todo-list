21b6e77a102c209ef446f16c470cee80b1cb6254
Leticia Lima <letylima96@gmail.com>
2020-12-20 23:51:21 -0300

    realiza exercicio 7

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const taskList = document.querySelector('#lista-tarefas');
+
 function inputTaskOnTheList() {
-  const taskList = document.querySelector('#lista-tarefas');
   const taskText = document.querySelector('#texto-tarefa');
   const listElement = document.createElement('li');
-  
+  listElement.id = 'item-lista';
+
   if (taskText.value !== '') {
-  listElement.innerText = taskText.value;
-  taskList.appendChild(listElement);
-  taskText.value = '';
+    listElement.innerText = taskText.value;
+    taskList.appendChild(listElement);
+    taskText.value = '';
   }
 }
 
@@ -14,5 +16,14 @@ function taskButton() {
   const button = document.querySelector('#criar-tarefa');
   button.addEventListener('click', inputTaskOnTheList);
 }
-
 taskButton();
+
+function changeBgColor(event) {
+  const listItem = document.querySelectorAll('#item-lista');
+
+  for (let index = 0; index < listItem.length; index += 1) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+
+taskList.addEventListener('click', changeBgColor);
