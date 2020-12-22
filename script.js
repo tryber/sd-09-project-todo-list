25fe02dc9981c320b441e6f2e07342f21ef21fb7
Deusivan Gomes <gomesdeusivan@gmail.com>
2020-12-22 14:02:59 -0300

    7

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var addButton = document.getElementById('criar-tarefa')
-var textBox = document.getElementById('texto-tarefa')
+var addButton = document.getElementById('criar-tarefa');
+var textBox = document.getElementById('texto-tarefa');
+var father = document.getElementsByTagName('ol')[0];
 
 function add() {
     let son = document.createElement('li');
-    let father = document.getElementsByTagName('ol');
+    
     son.innerHTML = textBox.value
-    father[0].appendChild(son)
+    father.appendChild(son)
 
     textBox.value = ''
 }
 
 addButton.addEventListener('click', add)
+
+var list = father.getElementsByTagName('li')
+
+function changeBackground(originaryEvent) {
+    for(i = 0; i < list.length; i += 1) {
+        if (originaryEvent.target === list[i]) {
+            list[i].style.backgroundColor  = 'rgb(128, 128, 128)'
+        } else {
+            list[i].style.backgroundColor = ''                                                   
+        }
+    }
+}
+
+father.addEventListener('click', changeBackground)
+
