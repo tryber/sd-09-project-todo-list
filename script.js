55af895f19f2301d0748f955bf645fa62b21460e
Jhonatan Arguello <jhonatan.arguello@gmail.com>
2020-12-22 15:29:32 -0300

    Implantaçao de task concluida utilizando o double click, alem de seu cancelamento utilizando a mesma açao de mouse.

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // Adicionando itens na lista
 function addTask() {
-  let createTask = document.querySelector("#criar-tarefa")
+  const createTask = document.querySelector('#criar-tarefa');
 
   createTask.addEventListener('click', function () {
-    let task = document.querySelector("#texto-tarefa")
-    
-    let insertTask = document.createElement("li")
-    let fatherList = document.querySelector("#lista-tarefas")
+    let task = document.querySelector("#texto-tarefa");
 
-    insertTask.className = 'item-list'
-    insertTask.innerText = task.value
+    let insertTask = document.createElement('li');
+    let fatherList = document.querySelector('#lista-tarefas');
 
-    fatherList.appendChild(insertTask)
+    insertTask.className = ('item-list');
+    insertTask.innerText = task.value;
 
-    task.value = ""
+    fatherList.appendChild(insertTask);
+
+    task.value = "";
   })
 }
 addTask()
 
 // Removendo todos os itens da lista
 function removeAll() {
-  let removeAllItens = document.querySelector("#apaga-tudo")
+  let removeAllItens = document.querySelector("#apaga-tudo");
 
   removeAllItens.addEventListener('click', function () {
-    let itemList = document.querySelectorAll(".item-list")
-    let fatherList = document.querySelector("#lista-tarefas")
+    let itemList = document.querySelectorAll(".item-list");
+    let fatherList = document.querySelector("#lista-tarefas");
 
     for (let index = 0; index < itemList.length; index += 1) {
       fatherList.removeChild(itemList[index])
     }
-  })
+  });
 }
 removeAll()
 
@@ -38,15 +38,33 @@ function SelectItem() {
 
   task.addEventListener('click', function(event) {
     let identifySelectedItem = document.querySelector(".selected")
-    
+
     if( identifySelectedItem !== null) {
       identifySelectedItem.classList.remove("selected")
     }
-    
+
     let clickedItem = event.target
     clickedItem.classList.add("selected")
-    console.log(event.target)
   })
 }
 
 SelectItem ()
+
+
+function finishTask() {
+  const task = document.querySelector('#lista-tarefas');
+
+  task.addEventListener('dblclick', function (event) {
+    let finishedTask = event.target
+
+    if (finishedTask.className === 'item-list completed' || finishedTask.className === 'item-list selected completed' || finishedTask.className === 'item-list completed selected') {
+      finishedTask.classList.remove('selected')
+      finishedTask.classList.remove('completed')
+    } else {
+      finishedTask.classList.add('completed')
+  };
+
+    console.log(event.target)
+  });
+}
+finishTask()
