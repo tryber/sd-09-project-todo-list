161dbe213bfbf7b6bf5d901c3ee9f76b6ba012ab
Marina Alane <marina.alane@outlook.com>
2020-12-23 11:41:50 -0300

    requisito 5

---
+++ script.js
@@ -0,0 +1,28 @@
+ const criarTarefa = document.createElement('button');
+  criarTarefa.id = 'criar-tarefa';
+  criarTarefa.innerHTML = 'adicionar';
+  document.body.appendChild(criarTarefa);
+
+
+
+function AddTask (){
+  btnAdd = document.getElementById('criar-tarefa');
+
+  btnAdd.addEventListener('click', function(){
+    newTask = document.querySelector('#texto-tarefa').value;
+    taskLine = document.querySelector('#lista-tarefas');
+    newLine = document.createElement('li');
+    newLine.innerText = newTask;
+    taskLine.appendChild(newLine);
+
+  document.querySelector('#texto-tarefa').value = ' ';
+
+  })
+}
+window.onload = function () {
+  AddTask ();
+
+}
+
+
+
