ce2f2d7c64d2f38ae21fae2aeca51ee26cee3161
Marina Alane <marina.alane@outlook.com>
2020-12-23 17:54:19 -0300

    ajusta code climate

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
- window.onload = function () {
-   const criarTarefa = document.createElement('button');
+const criarTarefa = document.createElement('button');
    criarTarefa.id = 'criar-tarefa';
    criarTarefa.innerHTML = 'adicionar';
    document.body.appendChild(criarTarefa);
 
    function taskAdd() {
-      const btnAdd = document.getElementById('criar-tarefa');
-      btnAdd.addEventListener('click', function () {
-      const newTask = document.querySelector('#texto-tarefa').value;
-      const taskLine = document.querySelector('#lista-tarefas');
-      const newLine = document.createElement('li');
-      newLine.innerHTML = newTask;
-      taskLine.appendChild(newLine);
+     const btnAdd = document.getElementById('criar-tarefa');
+     btnAdd.addEventListener('click', function () {
+     const newTask = document.querySelector('#texto-tarefa').value;
+     const taskLine = document.querySelector('#lista-tarefas');
+     const newLine = document.createElement('li');
+     newLine.innerHTML = newTask;
+     taskLine.appendChild(newLine);
 
-       document.getElementById('texto-tarefa').value = null;
-    });
+        document.getElementById('texto-tarefa').value = null;
+      });
    }
 
    function colorItem() {
      const list = document.querySelector('#lista-tarefas');
      list.addEventListener('click', function (event) {
        if (event.target.style.backgroundColor === '') {
-          event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-       }else {
-          event.target.style.backgroundColor = '';
+         event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+       } else {
+         event.target.style.backgroundColor = '';
        }
      });
    }
 
-  
+   window.onload = function () {
    taskAdd();
    colorItem();
-}
+   };
