b5b35c56f5628afa4c3d23acd7d83d28e3a5d122
Marina Alane <marina.alane@outlook.com>
2020-12-23 12:13:34 -0300

    ajusta code climate

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
  window.onload = function () {
-
-  const criarTarefa = document.createElement('button');
- criarTarefa.id = 'criar-tarefa';
- criarTarefa.innerHTML = 'adicionar';
- document.body.appendChild(criarTarefa);
-  function AddTask () {
-  btnAdd = document.getElementById('criar-tarefa');
-
-  btnAdd.addEventListener('click', function (){
-    newTask = document.querySelector('#texto-tarefa').value;
-    taskLine = document.querySelector('#lista-tarefas');
-    newLine = document.createElement('li');
-    newLine.innerText = newTask;
+   const criarTarefa = document.createElement('button');
+   criarTarefa.id = 'criar-tarefa';
+   criarTarefa.innerHTML = 'adicionar';
+   document.body.appendChild(criarTarefa);
+   
+   function taskAdd() {
+    const btnAdd = document.getElementById('criar-tarefa');
+    btnAdd.addEventListener('click', function () {
+    const newTask = document.querySelector('#texto-tarefa').value;
+    const taskLine = document.querySelector('#lista-tarefas');
+    const newLine = document.createElement('li');
+    newLine.innerHTML = newTask;
     taskLine.appendChild(newLine);
 
-    document.querySelector('#texto-tarefa').value = ' ';
+    document.getElementById('texto-tarefa').value = ' ';
   });
-}
-  AddTask ();
-
+  }
+  taskAdd();
 }
 
 
