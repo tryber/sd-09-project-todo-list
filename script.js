bff28b9c800015cfb3cf74aa0de3e1e911be7d14
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-23 13:54:24 -0300

    adiciona requisito que move os items da lista

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const olList = document.querySelector('#lista-tarefas');
 const removeButton = document.querySelector('#apaga-tudo');
 const removeDoneButton = document.querySelector('#remover-finalizados');
 const saveButton = document.querySelector('#salvar-tarefas');
+const moveUpButton = document.querySelector('#move-up');
+const moveDownButton = document.querySelector('#move-down');
 
 function recriaLista() {
-  if (localStorage.length !== 0){
+  if (localStorage.length !== 0) {
     for(let index = 0; index < localStorage.length; index += 1) {
       let li = document.createElement('li');
       let conteudo = localStorage.getItem(index);
@@ -36,9 +38,9 @@ function changeLiBackgroundColor() {
   olList.addEventListener('click', function (event) {
     const list = document.querySelectorAll('li');
     for (const item of list) {
-      item.style.background = 'white';
+      item.classList.remove('selected')
     }
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    event.target.classList.add('selected');
   });
 }
 changeLiBackgroundColor();
@@ -70,14 +72,44 @@ function removeDoneListItens() {
 removeDoneListItens();
 
 function saveList() {
-  saveButton.addEventListener('click', function (){
+  saveButton.addEventListener('click', function () {
     let listas = document.querySelectorAll('li');
     for(let index = 0; index < listas.length; index +=1) {
       let arrayValorClasses = [];
-      arrayValorClasses.push(listas[index].innerHTML)
+      arrayValorClasses.push(listas[index].innerHTML);
       arrayValorClasses.push(listas[index].className);
-      localStorage.setItem(index, arrayValorClasses)
+      localStorage.setItem(index, arrayValorClasses);
     }
   });
 }
-saveList()
+saveList();
+
+function moveUpItem() {
+  moveUpButton.addEventListener('click', function () {
+    const selectedItem = document.querySelector('.selected');
+    const selectedClass = selectedItem.classList.value;
+    const selectedText  = selectedItem.innerHTML
+    const previusClass = selectedItem.previousElementSibling.classList.value;
+    const previusText = selectedItem.previousElementSibling.innerHTML;
+    selectedItem.innerHTML = previusText;
+    selectedItem.previousElementSibling.innerHTML = selectedText;
+    selectedItem.previousElementSibling.className = selectedClass;
+    selectedItem.className = previusClass;
+  });
+}
+moveUpItem();
+
+function moveDownItem() {
+  moveDownButton.addEventListener('click', function () {
+    const selectedItem = document.querySelector('.selected');
+    const selectedClass = selectedItem.classList.value;
+    const selectedText  = selectedItem.innerHTML
+    const previusClass = selectedItem.nextElementSibling.classList.value;
+    const previusText = selectedItem.nextElementSibling.innerHTML;
+    selectedItem.innerHTML = previusText;
+    selectedItem.nextElementSibling.innerHTML = selectedText;
+    selectedItem.nextElementSibling.className = selectedClass;
+    selectedItem.className = previusClass;
+  });
+}
+moveDownItem();
