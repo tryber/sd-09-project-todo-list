5bc390704d3ecc4a0bae84c786003e6154446d3d
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-23 00:21:24 -0300

    Adicionadas as funcionalidades de salvar a lista e deletar o item selecionado

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// window.onload = function () {
-  // aproveitar o onload para recuperar o que estiver salvo no local storage.
-// }
-
 const myTaskList = document.getElementById('lista-tarefas');
 const myTaskInput = document.getElementById('texto-tarefa');
 const buttonAddTask = document.getElementById('criar-tarefa');
 const buttonRemoveAll = document.getElementById('apaga-tudo');
 const buttonRemoveCompleted = document.getElementById('remover-finalizados');
+const buttonSaveAll = document.getElementById('salvar-tarefas');
+const buttonDeleteSelected = document.getElementById('remover-selecionado');
 
+window.onload = function () {
+  for (let item = 0; item < localStorage.length; item += 1) {
+    let task = localStorage[item];
+    myTaskList.innerHTML += task;
+  };
+}
 buttonAddTask.addEventListener('click', function () {
   const myElement = document.createElement('li');
   const myTask = myTaskInput.value;
@@ -35,11 +39,25 @@ buttonRemoveAll.addEventListener('click', function () {
   const myTasks = myTaskList.children;
   while (myTasks.length > 0) {
     myTaskList.removeChild(myTasks[0]);
-  }
+  };
 });
 buttonRemoveCompleted.addEventListener('click', function () {
-  let completedClass = document.getElementsByClassName('completed');
+  const completedClass = document.getElementsByClassName('completed');
   while (completedClass.length > 0) {
-    completedClass[0].remove()
+    completedClass[0].remove();
+  };
+});
+buttonSaveAll.addEventListener('click', function () {
+  localStorage.clear();
+  if (myTaskList.children.length === 0) {
+    alert('Nothing to save...')
   }
+  for (let index = 0; index < myTaskList.children.length; index += 1) {
+    localStorage.setItem(index, `<li class=${myTaskList.children[index].className}>${myTaskList.children[index].innerHTML}</li>`);
+  }
+  alert('Everything is save :)')
+})
+buttonDeleteSelected.addEventListener('click', function () {
+  const selectedClass = document.getElementsByClassName('selected');
+  selectedClass[0].remove();
 });
