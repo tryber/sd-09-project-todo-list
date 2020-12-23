0cf478d6771fae255635ee76d3c7c450c49c1473
Igor Machado <rf59imft@gmail.com>
2020-12-23 09:17:56 -0300

    desafio 05

---
+++ script.js
@@ -0,0 +1,21 @@
+/**
+ * DESAFIO 05
+ * 
+ * CRIAR UM ELEMENTO DA TAG <button id="criar-tarefa">
+criar uma variavel para acessar o id="criar-tarefa"da tag button
+criar uma variavel para acessar o id="lista-tarefas" da tag ol
+criar uma variavel para inserir uma tag <li> para adicionar o texto dentro da <ol>
+fazer um clear up na caixa de texto
+ */
+
+function addInputText() {
+  let getInput = document.querySelector("#texto-tarefa");
+  let getOl = document.querySelector("#lista-tarefas");
+  let createTagLi = document.createElement("li");
+
+  if (getInput !== "") {
+    createTagLi.innerHTML = getInput.value;
+    getOl.appendChild(createTagLi);
+    getInput.value = "";
+  }
+}
