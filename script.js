ccf943656df3e44cd362b16745e659c999da6a4c
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-25 16:03:59 -0300

    primeiros requisitos feitos

---
+++ script.js
@@ -0,0 +1,3 @@
+const textoTarefa = document.querySelector('#texto-tarefa');
+const criarTarefa = document.querySelector('#criar-tarefa');
+const listaTarefas = document.querySelector('#lista-tarefas');
