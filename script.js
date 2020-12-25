e9f009f92aea7d58503d9c50789f9e873a88fca6
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-25 17:07:40 -0300

    requisito 5 feito

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const textoTarefa = document.querySelector('#texto-tarefa');
-const criarTarefa = document.querySelector('#criar-tarefa');
-const listaTarefas = document.querySelector('#lista-tarefas');
+function addNewTask() {
+  const inputField = document.querySelector('#texto-tarefa');
+  const inputButton = document.querySelector('#criar-tarefa');
+  const taskList = document.querySelector('#lista-tarefas');
+  inputButton.addEventListener('click', function (){
+    if (inputField.value.length > 0) {
+      const listItem = document.createElement('li');
+      listItem.innerText = inputField.value;
+
+      taskList.appendChild(listItem);
+      inputField.value = '';
+    } else {
+      alert('Error: Digite ao menos 1 caractere.');
+    }
+  });
+}
+addNewTask();
