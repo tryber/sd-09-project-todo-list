36f387f632a4d19ac03e84d682b2c7d9528d818e
Micaela Alves <micaela.alves93@gmail.com>
2020-12-27 02:19:34 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function createTask () {
+function createTask() {
   const button = document.querySelector('.button');
   const inputText = document.querySelector('.inputText');
   const father = document.querySelector('.taskList');
   button.addEventListener('click', function () {
     const task = document.createElement('li');
     task.innerText = inputText.value;
+    task.className = 'task'
     father.appendChild(task);
     inputText.value = '';
   });
 }
 createTask();
+function changeColorTask() {
+  let task = document.getElementsByClassName('task');
+  console.log(task);
+  const color = 'rgb(128, 128, 128)';
+  for (let index = 0; index < task.length; index += 1) {
+    task[index].addEventListener('click', function (event) {
+      console.log('oi')
+      console.log(event.target);
+      event.target.style.backgroudColor = color;
+    });
+  }
+}
+changeColorTask();
