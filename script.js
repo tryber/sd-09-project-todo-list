f07ae13e2661bd2d5c3e325078047194ed71bdcf
Bernardo Smith <bernardostech@gmail.com>
2020-12-26 23:05:47 -0500

    tentativas botao remover

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-
-
-function criaTarefa(){
-  let tarefa  = document.querySelector('#texto-tarefa');
+function criaTarefa() {
+  let tarefa = document.querySelector('#texto-tarefa');
   let lista = document.querySelector('#lista-tarefas');
   let itemLista = document.createElement('li');
   itemLista.innerHTML = tarefa.value;
   itemLista.className = 'item';
   lista.appendChild(itemLista)
   tarefa.value = " ";
- }
+}
 
-function apagarTudo(){
-   let apagaTudo = document.querySelectorAll('.item');
-   for(i=0; i<apagaTudo.length;i+=1){
-       apagaTudo[i].remove();
-   }
-} 
+function apagarTudo() {
+  let apagaTudo = document.querySelectorAll('.item');
+  for (i = 0; i < apagaTudo.length; i += 1) {
+    apagaTudo[i].remove();
+  }
+}
 
-let adicTarefa= document.querySelector('#criar-tarefa');
+let adicTarefa = document.querySelector('#criar-tarefa');
 adicTarefa.addEventListener('click', criaTarefa);
 
 let apagaTudo = document.getElementById('apaga-tudo');
@@ -25,28 +23,27 @@ apagaTudo.addEventListener('click', apagarTudo);
 
 
 
-let corFundoTarefa= document.querySelector('p');
-corFundoTarefa.addEventListener('click', color);
-
-
-function color(event){
-event.target.className='completed'
-}
-
-
-let lista= document.querySelector('#lista-tarefas');
+let lista = document.querySelector('#lista-tarefas');
 
 lista.addEventListener('click', changeColor);
 lista.addEventListener('dblclick', risk);
 
 
-  function risk(event){
-  event.target.classList.toggle('completed'); 
+function risk(event) {
+  event.target.classList.toggle('completed');
+}
+
+function changeColor(event) {
+  event.target.classList.toggle('selecao');
+}
+
+let finalizados = document.querySelector('#remover-finalizados');
+let complet = document.querySelectorAll('.completed');
+finalizados.addEventListener('click', apagarFinalizados);
+
+function apagarFinalizados(){
+  for (i = 0; i < complet.length; i += 1) {
+      complet[i].remove();
   }
-
-  function changeColor(event){
-    event.target.classList.toggle('selecao');
-  }
-
-
+}
 
