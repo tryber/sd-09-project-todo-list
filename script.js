9907301aeddbcd52b9900a3318385fea65ec0dd6
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2020-12-28 16:06:58 -0300

    seleciona um item por vez com o click

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let input = document.querySelector('#texto-tarefa');
-let button = document.querySelector('#criar-tarefa');
-let taskList = document.querySelector('#lista-tarefas')
-
 function clickButton () {
+    let input = document.querySelector('#texto-tarefa');
+    let button = document.querySelector('#criar-tarefa');
+    let taskList = document.querySelector('#lista-tarefas')
     button.addEventListener('click', function (event) {
         let inputText = input.value;
         if (inputText === '') {
@@ -11,18 +10,25 @@ function clickButton () {
         }
         let newTask = document.createElement('li');
         taskList.appendChild(newTask);
+        newTask.className = 'task';
         newTask.innerText = inputText;
         input.value = '';
     })
 }
-clickButton();
 
-function selectedItem() {
-    let itemTask = document.querySelectorAll('li');
-    for (let index = 0; index < itemTask.length; index += 1) {
-        itemTask[index].addEventListener('click', function (event) {
-            event.target.classList.toggle('selected')
-        }) 
+function taskSelected() {
+    let divTasks = document.querySelectorAll('#lista-tarefas')
+    let taskSelected = divTasks[0];
+    for (let index = 0; index < divTasks.length; index += 1) {
+        divTasks[index].addEventListener('click', function (event) {
+            taskSelected.className = 'task';
+            event.target.className = 'task selected';
+            return taskSelected = event.target;
+        })
     }
 }
-selectedItem();
+
+window.onload = function() {
+    clickButton();
+    taskSelected()
+}
