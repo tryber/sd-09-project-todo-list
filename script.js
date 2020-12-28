af81df2f16cf2b563ee74fdefa688f67351eb17c
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2020-12-28 11:26:05 -0300

    funçao de click ao botao

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let lista_ordenada = document.querySelector('#lista-tarefas');
+let inputText = document.querySelector('#texto-tarefa').value;
+let buttonClick = document.querySelector('#criar-tarefa');
+
+function cLickBtn () {
+    buttonClick.addEventListener('click', function () {
+        if (inputText === '') {
+            alert ('A tarefa não pode ser vazia!');
+            return;
+        }
+        let newTask = document.createElement ('li');
+        lista_ordenada.appendChild(newTask);
+        newRow.innerText = inputText;
+        inputText = '';
+    })
+    
+}
