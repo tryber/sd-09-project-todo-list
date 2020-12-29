c2b83551abfae450aa1c86b2494be4935867e168
Fernando Resende <fernandocastellob@gmail.com>
2020-12-29 15:42:35 -0300

    requisito 10 concluido

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ const buttonPress = document.getElementById('criar-tarefa');
 const inputField = document.getElementById('texto-tarefa');
 const toDoList = document.getElementById('lista-tarefas');
 const buttonDelete = document.getElementById('apaga-tudo');
+const list = document.getElementsByTagName('li');
 
-buttonPress.addEventListener('click', function() {
-  var appendToList = document.createElement('li');
+buttonPress.addEventListener('click', function () {
+  let appendToList = document.createElement('li');
   appendToList.classList.add('lista-tarefas');
   appendToList.innerText = inputField.value;
   toDoList.appendChild(appendToList);
   inputField.value = '';
 });
 
-buttonPress.addEventListener('dblclick', function() {
-  appendToList.style.textDecoration = 'line-through';
+buttonPress.addEventListener('dblclick', function () {
+  
 });
-
-buttonDelete.addEventListener('click', function() {
-  toDoList.removeChild('li');
+  
+buttonDelete.addEventListener('click', function () {
+  for (index = list.length - 1; index >= 0; index -= 1) {
+    list[index].parentNode.removeChild(list[index]);
+  }
 });
