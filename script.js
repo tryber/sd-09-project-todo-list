2555dd5d0036f33c66c1f87397e03090ab93dc61
Kevin Fraga <kevinciolac@gmail.com>
2020-12-28 23:39:16 -0300

    :zero::five:

---
+++ script.js
@@ -0,0 +1,12 @@
+const novaTarefa = document.getElementById("criar-tarefa");
+const nomeNovaTarefa = document.getElementById("texto-tarefa");
+const listaTarefas = document.getElementById("lista-tarefas");
+
+function criarTarefa() {
+  let tarefa = document.createElement("li");
+  tarefa.innerText = nomeNovaTarefa.value;
+  listaTarefas.appendChild(tarefa);
+  nomeNovaTarefa.value = "";
+}
+
+novaTarefa.addEventListener("click", criarTarefa);
