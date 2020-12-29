67c693a2628332be3622446abeca7d4ded817d53
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2020-12-29 17:53:19 -0300

    Requisito 5

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('teste')
+
+function createLiElement() {
+  const orderedList = document.getElementById('lista-tarefas')
+  let textBox = document.getElementById('texto-tarefa');
+  let liElement = document.createElement('li');
+  liElement.innerText = textBox.value;
+  orderedList.appendChild(liElement);
+  textBox.innerText = '';
+}
+
+const button = document.getElementById('criar-tarefa');
+button.addEventListener('click', createLiElement);
