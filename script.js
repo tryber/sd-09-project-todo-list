4da5b6c8aee394c90c8b77d56817fcdcf0b2ca0f
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2020-12-29 18:19:44 -0300

    Requisito 7 completo

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-console.log('teste')
-
 function createLiElement() {
-  const orderedList = document.getElementById('lista-tarefas')
-  let textBox = document.getElementById('texto-tarefa');
-  let liElement = document.createElement('li');
+  const orderedList = document.getElementById('lista-tarefas');
+  const textBox = document.getElementById('texto-tarefa');
+  const liElement = document.createElement('li');
   liElement.innerText = textBox.value;
+  liElement.addEventListener('click', changeBackgroundColor);
   orderedList.appendChild(liElement);
   textBox.value = '';
 }
 
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', createLiElement);
+
+function changeBackgroundColor(event) {
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
