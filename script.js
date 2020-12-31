a344361a7ace789d7e08ad89d6ae86f3fc8fd53a
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-12-31 16:57:35 -0300

    doc: code-climate

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const inputTextoTarefa = document.querySelector("#texto-tarefa");
-const buttonCriarTarefas = document.querySelector("#criar-tarefa");
-const listaTarefas = document.querySelector("#lista-tarefas");
+const inputTextoTarefa = document.querySelector('#texto-tarefa');
+const buttonCriarTarefas = document.querySelector('#criar-tarefa');
+const listaTarefas = document.querySelector('#lista-tarefas');
 
 //5 - Deve haver um botão com id="criar-tarefa" e ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.
 function receberTexto() {
-  const listaLi = document.createElement("li");
+  const listaLi = document.createElement('li');
   listaLi.innerText = inputTextoTarefa.value;
   listaTarefas.appendChild(listaLi);
   inputTextoTarefa.value = '';
 }
 
 buttonCriarTarefas.addEventListener('click', receberTexto);
-
