dd5ea2e34093eea2aefcb0430e2e168ca97c8534
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-12-31 14:54:39 -0300

    Adicionando botao para limpar a lista de tarefas

--- script.js
+++ script.js
@@ -18,16 +18,19 @@ function itemSelected(){
     const olListaTarefas = document.querySelector('ol#lista-tarefas');
     olListaTarefas.addEventListener('click', function(event){
         const li = event.target;
-        li.classList.add('selectedList')        
+        li.classList.add('selectedList')                 
     })   
 }
 itemSelected();
 
-// const selectedListaTarefas = document.querySelectorAll('ol#lista-tarefas li');
-// if(selectedListaTarefas.length > 0){
-//     for(let indexList = 0; indexList < selectedListaTarefas.length; indexList += 1){
-//         selectedListaTarefas[indexList].addEventListener('click', function(){
-//             selectedListaTarefas[indexList].classList.add('selectedList');                
-//         })
-//     }
-// }    
+const btnLimparLista = document.querySelector('#apaga-tudo');
+function removeList(){
+btnLimparLista.addEventListener('click', function(){
+    const cleanList = document.querySelectorAll('ol li');
+    const ol = document.querySelector('ol');
+    for(let indexClean = 0; indexClean < cleanList.length; indexClean +=1){
+        ol.removeChild(cleanList[indexClean]);
+    }
+})
+}
+removeList();
