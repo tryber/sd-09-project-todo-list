095be8882d1a7d428e207ae10e0435f0d1113bbd
SantosDiv <diogenesfelipe.s@outlook.com>
2020-12-31 17:02:07 -0300

    Resoluçao dos desafios de mover para baixo e para cima e deletar os item completos

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
+function moveUp(list) {
+  const btnUp = document.querySelector('#mover-cima');
+  btnUp.addEventListener('click', function () {
+    for (let index = 0; index < list.childElementCount; index += 1) {
+      if (list.childNodes[index].classList.contains('selected')) {
+        let selectedItem = list.childNodes[index].innerText;
+        let prevItem = list.childNodes[index - 1].innerText;
+        list.childNodes[index - 1].innerText = selectedItem;
+        list.childNodes[index].innerText = prevItem;
+        list.childNodes[index - 1].classList.add('selected');
+        list.childNodes[index].classList.remove('selected');
+      }
+    }
+  });
+}
+
+function moveDown(list) {
+  const btnDown = document.querySelector('#mover-baixo');
+  btnDown.addEventListener('click', function () {
+    for (let index = 0; index < list.childElementCount; index += 1) {
+      if (list.childNodes[index].classList.contains('selected')) {
+        let selectedItem = list.childNodes[index].innerText;
+        let nextItem = list.childNodes[index + 1].innerText;
+        list.childNodes[index + 1].innerText = selectedItem;
+        list.childNodes[index].innerText = nextItem;
+        list.childNodes[index + 1].classList.add('selected');
+        list.childNodes[index].classList.remove('selected');
+        return;
+      }
+    }
+  });
+}
+
+// Delete Tasks Completed
+function removeCompleted() {
+  const btnDeleteComplete = document.querySelector('#remover-finalizados');
+  const list = document.querySelector('#lista-tarefas');
+  btnDeleteComplete.addEventListener('click', function () {
+    for (let index = 0; index < list.childElementCount; index += 1) {
+      if (list.childNodes[index].classList.contains('completed')) {
+        list.removeChild(list.childNodes[index]);
+      }
+    }
+  });
+}
+
 // Mark
-function markCompletedTask(event) {
+function taskCompleted(event) {
   event.target.classList.toggle('completed');
+  removeCompleted();
 }
 
 // Change Background
@@ -21,26 +68,27 @@ function createTaskElement(taskName) {
   taskElement.classList.add('block-text-selected');
   taskElement.innerText = taskName;
   taskElement.addEventListener('click', selectedElement);
-  taskElement.addEventListener('dblclick', markCompletedTask);
+  taskElement.addEventListener('dblclick', taskCompleted);
   return taskElement;
 }
 
 // Add task in list
-function addTaskInlist(button, inputText) {
-  const listContainer = document.querySelector('#lista-tarefas');
+function addTaskInlist(button, inputText, taskList) {
   button.addEventListener('click', function () {
     const valueInput = inputText.value;
+    if (!valueInput) {
+      alert('Por favor, digite uma tarefa');
+      return;
+    }
     const taskElement = createTaskElement(valueInput);
-    listContainer.appendChild(taskElement);
+    taskList.appendChild(taskElement);
     inputText.value = '';
   });
 }
 
-// Delete All Tasks 
-function deleteAllTasks() {
+// Delete All Tasks
+function deleteAllTasks(taskList) {
   const btnDeleteAll = document.querySelector('#apaga-tudo');
-  const taskList = document.querySelector('#lista-tarefas');
-  const task = document.querySelectorAll('.task');
   btnDeleteAll.addEventListener('click', function () {
     while (taskList.firstChild) {
       taskList.removeChild(taskList.firstChild);
@@ -51,6 +99,9 @@ function deleteAllTasks() {
 window.onload = function () {
   const textTaskInput = document.querySelector('#texto-tarefa');
   const buttonAddTask = document.querySelector('#criar-tarefa');
-  addTaskInlist(buttonAddTask, textTaskInput);
-  deleteAllTasks();
+  const taskList = document.querySelector('#lista-tarefas');
+  addTaskInlist(buttonAddTask, textTaskInput, taskList);
+  deleteAllTasks(taskList);
+  moveDown(taskList);
+  moveUp(taskList);
 };
