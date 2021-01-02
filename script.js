1acb73a3c17cc598e3246188d3a1357f485729f8
Mathaus <mathausannibal@gmail.com>
2021-01-02 13:34:16 -0300

    adiciona itens 7,8,9,10

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ function adicionar() {
    list.appendChild(item);
    document.getElementById("texto-tarefa").value =""
 }
+function limpar() {
+   document.querySelectorAll("li").forEach(element => {
+      element.remove();
+   })
+}
+document.addEventListener('click', function (event) {
+    
+
+   
+      if ( event.target.nodeName === 'LI' ) {
+         
+         
+                     
+            document.querySelectorAll('.selected').forEach(element => {
+               element.classList.remove('selected')
+            })
+            event.target.classList.toggle('selected')
+         
+   }
+}, false);
+
+document.addEventListener('dblclick', function(event){
+   console.log(event.target.nodeName)
+   if ( event.target.nodeName === 'LI' ) {
+      
+      if(event.target.classList.contains('completed')) {
+         event.target.classList.remove('completed')
+      }
+      else {
+         event.target.classList.remove('selected')
+         event.target.classList.toggle('completed')
+   }}
+})
