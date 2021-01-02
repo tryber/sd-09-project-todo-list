8ad4adaf4f9f1938b7d291c6957f60f02f36591a
PeryclesF <perycles_reis@hotmail.com>
2021-01-02 13:51:31 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,43 +1,72 @@
 const main = document.querySelector('body');
 
-function title(titlesString) {
-  const newHeader = document.createElement('header');
-  newHeader.innerHTML = titlesString;
-  main.appendChild(newHeader);
-}
-title('Minha Lista de Tarefas');
-
-function workout(textString) {
-  const paragraph = document.createElement('p');
+function criaParagrafo(textString) {
+  const paragraph = document.querySelector('p');
   paragraph.id = 'funcionamento';
   paragraph.innerHTML = textString;
-  main.appendChild(paragraph);
 }
-workout('Clique duas vezes em um item para marcá-lo como completo');
+criaParagrafo('Clique duas vezes em um item para marcá-lo como completo.');
 
-function textBox(idString) {
-  const newInput = document.createElement('input');
+function caixaTexto(idString) {
+  const newInput = document.querySelector('input');
   newInput.id = idString;
-  main.appendChild(newInput);
 }
-textBox('texto-tarefa');
+caixaTexto('texto-tarefa');
 
 function criarLista(listString) {
-  const newList = document.createElement('ol');
+  const newList = document.querySelector('ol');
   newList.id = listString;
-  main.appendChild(newList);
 }
 criarLista('lista-tarefas');
 
-function criarTarefa(tarefa) {
-  const newTask = document.createElement('button');
+function criarBotao(tarefa) {
+  const newTask = document.querySelector('button');
   newTask.id = tarefa;
   newTask.innerHTML = 'Adicionar';
-  main.appendChild(newTask);
 }
-criarTarefa('criar-tarefa');
+criarBotao('criar-tarefa');
 
-let botao = document.querySelector('button');
+function limpaTexto() {
+  const textInput = document.querySelector('#texto-tarefa');
+  if(textInput.value !== '') {
+    textInput.value = '';
+  }
+}
+// function trocaFundoItemLista() {
+//   const itensLista = document.querySelector('#lista-tarefas').childNodes;
+//   for (let index = 0; index < itensLista.length; index++) {
+//     const elementoItem = itensLista[index];
+//     elementoItem.addEventListener('click', function(event){
+//       pegaItem();
+//       console.log(elementoItem);
+//         event.target.classList.toggle('color');
+//     })
+//   }
+// }
+
+// function pegaItem() {
+//   const itensLista = document.querySelector('#lista-tarefas').childNodes;
+//   for (let index = 0; index < itensLista.length; index++) {
+//     const elementoItem = itensLista[index];
+//     if (elementoItem.classList.contains('color')) {
+//       elementoItem.classList.remove('color');
+//     }
+//   }
+// }
+
+const listaDeTarefas = document.querySelector('#lista-tarefas');
+function selecionarTarefa(event) {
+  const selecionarItem = document.querySelector('.color');
+  const tarefaSelecionada = event.target;
+  if (selecionarItem !== null) {
+    selecionarItem.classList.remove('color');
+  } else {
+    tarefaSelecionada.classList.add('color');
+  }
+}
+listaDeTarefas.addEventListener('click', selecionarTarefa);
+
+const botao = document.querySelector('button');
 function itemLista() {
   const item = document.querySelector('#lista-tarefas');
   const textInput = document.querySelector('#texto-tarefa');
@@ -45,12 +74,6 @@ function itemLista() {
   listaTarefa.innerHTML = textInput.value;
   item.appendChild(listaTarefa);
   limpaTexto();
+  // trocaFundoItemLista();
 }
 botao.addEventListener('click', itemLista);
-
-function limpaTexto() {
-  const textInput = document.querySelector('#texto-tarefa');
-  if(textInput.value !== ''){
-    textInput.value = '';
-  }
-}
