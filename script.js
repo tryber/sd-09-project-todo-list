3a19f020e2bce2f1862b9a1c6de9427f8c20d8bc
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-03 18:35:55 -0300

    Mandatory requirements 5 to 7

---
+++ script.js
@@ -0,0 +1,64 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when double-clicking on a list item
+var list = document.querySelector('ol');
+list.addEventListener('dblclick', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("texto-tarefa").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("lista-tarefas").appendChild(li);
+  }
+  document.getElementById("texto-tarefa").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// Creates the function of clicking on the "criar-tarefa" button by pressing "Enter"
+document.getElementById("texto-tarefa")
+    .addEventListener("keyup", function(e) {
+    e.preventDefault();
+    if (e.key === "Enter") {
+        document.getElementById("criar-tarefa").click();
+    }
+});
