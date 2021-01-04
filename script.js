4e7a0ba413990ad1279f9558984157560fb01208
palenske <viniciuspalenske@gmail.com>
2021-01-04 03:22:14 -0300

    função para selecionar tarefa, mudando a cor do background, permitindo apenas uma seleção

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 function pushTaskToList() {
-  let createTaskBtn = document.getElementById("criar-tarefa");
+  const createTaskBtn = document.getElementById("criar-tarefa");
   createTaskBtn.addEventListener("click", function () {
     let inputText = document.querySelector("#texto-tarefa").value;
     let taskLine = document.createElement("li");
     let taskList = document.querySelector("#lista-tarefas");
     document.getElementById("texto-tarefa").value = "";
     taskLine.innerText = inputText;
+    taskLine.className = "linha";
     taskList.appendChild(taskLine);
   });
 }
 pushTaskToList();
+
+function selectTask() {
+  const taskList = document.querySelector("#lista-tarefas");
+  taskList.addEventListener("click", function (event) {
+    let allItens = document.querySelectorAll(".linha");
+    if (event.target.className.indexOf("selecionado") === -1) {
+      for (let index = 0; index < allItens.length; index += 1) {
+        allItens[index].classList.remove("selecionado");
+      }
+      event.target.classList.add("selecionado");
+    } else {
+      for (let index = 0; index < allItens.length; index += 1) {
+        allItens[index].classList.remove("selecionado");
+      }
+    }
+  });
+}
+
+selectTask();
