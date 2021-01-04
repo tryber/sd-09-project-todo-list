6093a56ab574dbb36857c1dc41777911725233ea
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-01-04 14:53:48 -0300

    corrigindo requisito 5

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//const textInput = document.querySelector('#texto-tarefa');
-//const makeTask = document.querySelector('#criar-tarefa');
-
-  //function insereText () {
-  //  textInput.innerText = textInput.value;
-//}
-  //function criandoTarefa () {
-    //makeTask.addEventListener('click', function(event)) {
-      //if (textInput.length === 0) {
-        
-      //}
-    //}
-//}
-
 function addText () {
   const text = document.getElementById('texto-tarefa');
   const add = document.getElementById('lista-tarefas');
@@ -20,5 +6,3 @@ function addText () {
     criarElemento.innerText = text.value;
     add.appendChild(criarElemento);
 }
-
-//add.addEventListener(click,addText);
