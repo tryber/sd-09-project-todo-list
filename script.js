712aaedc79e4ff32de279ba698d48d2dea76a2bb
Pedro Castro <pa.coelho.castro@gmail.com>
2021-01-04 12:33:27 -0300

    Task #5 to #6

---
+++ script.js
@@ -0,0 +1,28 @@
+let task
+
+let addButton = document.querySelector('#criar-tarefa')
+let taskList = document.querySelector('#lista-tarefas')
+let itemOfList = document.getElementsByTagName('ol li');
+
+addButton.addEventListener('click', (e) => {
+  task = document.querySelector('#texto-tarefa').value;
+    
+  let listItem = document.createElement('li');
+  listItem.innerText = task;
+  document.querySelector('#texto-tarefa').value = '';
+  document.querySelector('#texto-tarefa').focus();
+  taskList.appendChild(listItem);
+})
+
+let removeAllBG = () => {
+itemOfList.forEach(item => {
+    item.classList.remove = 'cinza'
+})
+}
+
+itemOfList.forEach(item => {
+    item.addEventListener('click', () => {
+        removeAllBG()
+      item.classList.add = 'cinza'
+    })
+})
