1e6ec1b8f9cfe795fdffffccf5bc97ad7a391372
Rodrigo Farias <deltadigo@gmail.com>
2021-01-05 09:33:43 -0300

    Requisito 7 - Adicionar cor de fundo a tarefa selecionada

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const criarTarefa = document.querySelector("#criar-tarefa");
-const textoTarefa = document.querySelector("#texto-tarefa");
-const listaTarefas = document.querySelector("#lista-tarefas");
+const criarTarefa = document.querySelector('#criar-tarefa');
+const textoTarefa = document.querySelector('#texto-tarefa');
+const listaTarefas = document.querySelector('#lista-tarefas');
 
-criarTarefa.addEventListener("click", function(event) {
-  const tarefa = document.createElement("li");
+function adicionarTarefa () {
+  const tarefa = document.createElement('li');
   tarefa.innerText = textoTarefa.value;
-  textoTarefa.value = "";
+  textoTarefa.value = '';
   listaTarefas.appendChild(tarefa);
-})
+}
+
+function backgroundItemSelecionado(event) {
+  if (event.target == listaTarefas) {
+    return;
+  }
+  if (!event.target.style.backgroundColor == "rgb(128,128,128") {
+    event.target.style.backgroundColor = rgb(128,128,128);
+  }
+}
+
+criarTarefa.addEventListener ('click', adicionarTarefa);
+
+listaTarefas.addEventListener("click", backgroundItemSelecionado);
+
+
+
