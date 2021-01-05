bcd88ec02f4e3cf5ffbcd31a93b42c78b3913256
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-05 19:16:46 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//===== FUNCTIONS DOS BOTÕES =====
 function addTask() {
   const newItem = document.createElement('li');
   if (document.getElementById('texto-tarefa').value !== '') {
@@ -12,13 +13,20 @@ function clearList() {
   document.querySelector('#lista-tarefas').innerHTML = '';
 }
 
+// Baseado na solução encontrada em: https://stackoverflow.com/a/14066534
 function clearCompleted() {
-  let completedList = document.getElementsByClassName('completed');
+  const completedList = document.getElementsByClassName('completed');
   while (completedList.length > 0) {
     completedList[0].parentNode.removeChild(completedList[0]);
   }
 }
 
+//===== BOTÕES =====
+document.getElementById('criar-tarefa').addEventListener('click', addTask);
+document.getElementById('apaga-tudo').addEventListener('click', clearList);
+document.getElementById('remover-finalizados').addEventListener('click', clearCompleted);
+
+//===== FUNCTIONS DA LISTA =====
 const taskList = document.querySelector('#lista-tarefas');
 
 taskList.addEventListener('click', function (select) {
