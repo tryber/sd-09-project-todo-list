52b275b90f523ea79bb6f239046d6fe1af299bf7
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-05 18:10:59 -0300

    requisitos 9-11

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function addTask() {
   const newItem = document.createElement('li');
-  const newItemText = document.createTextNode(document.getElementById('texto-tarefa').value);
-  newItem.appendChild(newItemText);
-  document.getElementById('lista-tarefas').appendChild(newItem);
-  document.getElementById('texto-tarefa').value = '';
+  if (document.getElementById('texto-tarefa').value !== '') {
+    const newItemText = document.createTextNode(document.getElementById('texto-tarefa').value);
+    newItem.appendChild(newItemText);
+    document.getElementById('lista-tarefas').appendChild(newItem);
+    document.getElementById('texto-tarefa').value = '';
+  }
+}
+
+function clearList() {
+  document.querySelector('#lista-tarefas').innerHTML = '';
+}
+
+function clearCompleted() {
+  let completedList = document.getElementsByClassName('completed');
+  while (completedList.length > 0) {
+    completedList[0].parentNode.removeChild(completedList[0]);
+  }
 }
 
 const taskList = document.querySelector('#lista-tarefas');
@@ -15,5 +28,13 @@ taskList.addEventListener('click', function (select) {
     select.target.classList.add('selected');
   } else {
     select.target.classList.add('selected');
+  }
+});
+
+taskList.addEventListener('dblclick', function (done) {
+  if (done.target.classList.contains('completed')) {
+    done.target.classList.remove('completed');
+  } else {
+    done.target.classList.add('completed');
   }
 });
