a1bbc3e8f1c1c8d2c2caf6fbe1c970639eddcea6
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-05 19:00:50 -0300

    Atualizando funcao remove itens

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-/* let tarefas = [];
- let novaTarefa = 'arrumar a casa';
-  function adicionaTarefa(tarefa) {
-      tarefas.push(tarefa);
-  }
-  adicionaTarefa(novaTarefa2);
-console.log(novaTarefa2); */
-
 function saveData() {
-  let listaTarefas = document.getElementById('lista-tarefas');
-  let textoTarefa = document.getElementById('texto-tarefa').value;
-  let incluiTarefa = document.createElement('li');
+  const listaTarefas = document.getElementById('lista-tarefas');
+  const textoTarefa = document.getElementById('texto-tarefa').value;
+  const incluiTarefa = document.createElement('li');
   incluiTarefa.innerText = textoTarefa;
+  incluiTarefa.className = 'tarefa-from-list';
   listaTarefas.appendChild(incluiTarefa);
-  
-  // document.getElementById('texto').innerText = textoTarefa;
 }
-
 const criarTarefa = document.getElementById('criar-tarefa');
 criarTarefa.addEventListener('click', saveData);
 
-
+function clearList() {
+  document.getElementById('lista-tarefas').innerHTML = '';
+}
+const limpaList = document.getElementById('apaga-tudo');
+limpaList.addEventListener('click', clearList);
