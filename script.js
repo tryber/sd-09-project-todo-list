a8f4e19f6d669ffe218429bd87189fc5472f17b0
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-01-05 19:31:09 -0300

    Adicionando requisito 9

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 function addText () {
-  const text = document.getElementById('texto-tarefa');
-  const add = document.getElementById('lista-tarefas');
-  
-  let criarElemento = document.createElement('li');
-    criarElemento.innerText = text.value;
-    add.appendChild(criarElemento);
-    text.value = '';
+	const text = document.getElementById('texto-tarefa');
+	const add = document.getElementById('lista-tarefas');
+	
+	let criarElemento = document.createElement('li');
+	criarElemento.innerText = text.value;
+	add.appendChild(criarElemento);
+	text.value = '';
 }
 
 function changebg(event) {
-    const color = document.querySelector('.bg-color');
-    if (color !== null) {
-        color.classList.remove('bg-color');
-    } event.target.classList.add('bg-color');
+	const color = document.querySelector('.bg-color');
+		if (color !== null) {
+				color.classList.remove('bg-color');
+	} event.target.classList.add('bg-color');
 } 
-    const teste = document.querySelector('#lista-tarefas');
-    teste.addEventListener('click', changebg);
+	const change = document.querySelector('#lista-tarefas');
+	change.addEventListener('click', changebg);
  
-    const riscarItem = document.querySelector('#lista-tarefas');
-function riscar (event) {
-    riscarItem.classList.remove('completed');
-    event.target.classList.add('completed');
-}
-const mark = document.querySelector('.completed');
-riscarItem.addEventListener('dblclick', riscar);
+
+
+
+	function riscar (event) {
+		
+		if (event.target.classList.contains('completed')){
+			event.target.classList.remove('completed');
+		} else {
+			(event.target.classList.add('completed'))
+		}
+
+
+		//event.target.classList.remove('completed');
+}	
+	const lirisc= document.querySelector('#lista-tarefas');
+	lirisc.addEventListener('dblclick', riscar);
