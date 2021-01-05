1d8ea4e10a0b390e92c0518060f563d6f517f5e3
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-05 19:51:25 -0300

    Adiciona funcao limpar campo do input

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function saveData() {
   const listaTarefas = document.getElementById('lista-tarefas');
-  const textoTarefa = document.getElementById('texto-tarefa').value;
+  const textoTarefa = document.getElementById('texto-tarefa');
   const incluiTarefa = document.createElement('li');
-  incluiTarefa.innerText = textoTarefa;
+  incluiTarefa.innerText = textoTarefa.value;
   incluiTarefa.className = 'tarefa-from-list';
   listaTarefas.appendChild(incluiTarefa);
+  textoTarefa.value = '';
 }
 const criarTarefa = document.getElementById('criar-tarefa');
 criarTarefa.addEventListener('click', saveData);
