6a29ab0d9921ab0c55186320f3e4b02e82ff650c
GabrielLennox <meunomeelennox@gmail.com>
2021-01-05 19:17:57 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const novaTarefa = document.getElementById('texto-tarefa');
 const listaDeTarefas = document.getElementById('lista-tarefas');
 const createButton = document.getElementById('criar-tarefa');
+const tarefas = document.getElementsByTagName('li');
 
 function criarTarefa () {
     const novoElemento = document.createElement('li');
     novoElemento.innerText = novaTarefa.value;
-    console.log(novoElemento.innerText);
     listaDeTarefas.appendChild(novoElemento);
+    novoElemento.addEventListener('click', function(event) {
+        let teste = document.querySelector('.selected');
+        if (teste){
+            teste.classList.remove("selected");
+        }
+        event.target.classList.add("selected");
+    });
     novaTarefa.value = '';
 }
 
