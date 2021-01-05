8209590839d7cc498f545699c29be941d89dc570
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-05 14:50:30 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function addTask() {
   document.getElementById('lista-tarefas').appendChild(newItem);
   document.getElementById('texto-tarefa').value = '';
 }
+
+const taskList = document.querySelector('#lista-tarefas');
+
+taskList.addEventListener('click', function (select) {
+  const selectionCheck = document.querySelector('.selected');
+  if (selectionCheck) {
+    selectionCheck.classList.remove('selected');
+    select.target.classList.add('selected');
+  } else {
+    select.target.classList.add('selected');
+  }
+});
