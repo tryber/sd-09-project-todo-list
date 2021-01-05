982e82e51a10dd65557c510b4eee49000f6817ae
Sabrina A <sasa2605.alves@gmail.com>
2021-01-05 18:48:24 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
   let olParent = document.getElementById("lista-tarefas");
   let btnCreaTask = document.getElementById("criar-tarefa");
 
-  function createTasks() {
+    function createTasks() {
     let task = document.createElement("li");
     let inputBtn = document.getElementById("texto-tarefa").value;
     task.textContent = inputBtn;
@@ -24,14 +24,23 @@ window.onload = function () {
     task.addEventListener('dblclick', dBComplete)
 
     let erase = document.querySelector('#apaga-tudo');
-
+    let olChilds = document.querySelector('ol').childNodes;
+    
     function eraser(evento) {
-      let olChilds = document.querySelector('ol').childNodes;
       for (let index = 0; index < olChilds.length; index++) {
         olParent.removeChild(olChilds[index])
       }
     }
     erase.addEventListener('click', eraser)
+
+    let deleteCompleted = document.getElementById('remover-finalizados');
+    function deleteCom (evento) {
+      let lineThroughLi = document.querySelectorAll('.completed');
+      for(index = 0; index < lineThroughLi.length; index += 1) {
+        olParent.removeChild(lineThroughLi[index]);
+      }
+    }
+    deleteCompleted.addEventListener('click', deleteCom);
   }
   btnCreaTask.addEventListener("click", createTasks)
 }
