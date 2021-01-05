1798403fd0ba9e9d4efdad4661b1c0e9902418b3
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-05 09:33:11 -0300

    Criando funcao adicionar na lista

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('projeto to do list');
+/* let tarefas = [];
+ let novaTarefa = 'arrumar a casa';
+  function adicionaTarefa(tarefa) {
+      tarefas.push(tarefa);
+  }
+  adicionaTarefa(novaTarefa2);
+console.log(novaTarefa2); */
+
+function saveData() {
+  let listaTarefas = document.getElementById('lista-tarefas');
+  let textoTarefa = document.getElementById('texto-tarefa').value;
+  let incluiTarefa = document.createElement('li');
+  incluiTarefa.innerText = textoTarefa;
+  listaTarefas.appendChild(incluiTarefa);
+  
+  // document.getElementById('texto').innerText = textoTarefa;
+}
+
+const criarTarefa = document.getElementById('criar-tarefa');
+criarTarefa.addEventListener('click', saveData);
+
+
