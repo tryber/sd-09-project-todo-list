6848bfc0b2f5971974c5f424d9673ca3c8eed382
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-05 22:13:55 -0300

    Requisito 9, code climate

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function createTask() {
-  const input = document.getElementById("texto-tarefa");
-  if (input.value !== "") {
-    const ol = document.getElementById("lista-tarefas");
-    const li = document.createElement("li");
+  const input = document.getElementById('texto-tarefa');
+  if (input.value !== '') {
+    const ol = document.getElementById('lista-tarefas');
+    const li = document.createElement('li');
     li.innerHTML = input.value;
     ol.appendChild(li);
-    input.value = "";
+    input.value = '';
   }
 }
 
 function changeColor() {
-  const lista = document.getElementById("lista-tarefas");
-  lista.addEventListener("click", function (e) {
-    const selectedLista = document.getElementsByClassName("selected");
+  const lista = document.getElementById('lista-tarefas');
+  lista.addEventListener('click', function (e) {
+    const selectedLista = document.getElementsByClassName('selected');
     if (selectedLista[0]) {
-      selectedLista[0].classList.remove("selected");
+      selectedLista[0].classList.remove('selected');
     }
-    e.target.classList.add("selected");
+    e.target.classList.add('selected');
   });
 }
 
 function completed() {
-  const lista = document.getElementById("lista-tarefas");
-  lista.addEventListener("dblclick", function (event) {
-    event.target.classList.toggle("completed");
+  const lista = document.getElementById('lista-tarefas');
+  lista.addEventListener('dblclick', function (event) {
+    event.target.classList.toggle('completed');
   });
 }
 
