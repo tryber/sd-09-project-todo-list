c17481284c425bc5ae59601621a30b10207481cc
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-06 10:39:45 -0300

    Add tasks

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const orderedList = document.querySelector('#lista-tarefas');
 const list = document.createElement('li');
 list.className = 'todo-list';
 const buttonsSection = document.querySelector('#buttons');
+
+// Adiciona as tarefas
+function addTask() {
+  button.addEventListener('click', function () {
+    if (text.value === '') {
+      alert('[ERRO!] - Digite algum valor!');
+    } else {
+      const lists = document.createElement('li');
+      lists.className = 'todo-list';
+      orderedList.appendChild(lists);
+      lists.innerText = text.value;
+      text.value = '';
+    }
+  });
+}
