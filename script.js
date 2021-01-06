aa07d3fdaecd0ee8a9324673cefd9f8459ebe86d
GabrielLennox <meunomeelennox@gmail.com>
2021-01-06 14:45:50 -0300

    refatorando requisito 9

--- script.js
+++ script.js
@@ -16,11 +16,6 @@ function criarTarefa () {
         event.target.classList.add("selected");
     });
     novoElemento.addEventListener('dblclick', function(event) {
-        // if (event.target.classList.contains("completed")) {
-        //     event.target.classList.remove("completed");
-        // } else {
-        //     event.target.classList.add("completed");
-        // }
         event.target.classList.toggle("completed");
     })
     novaTarefa.value = '';
