0d96465e3cd8cb66cadedad1ab8265e49d2f4706
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-05 21:28:30 -0300

    Requisito 5, tentativa 4

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function createTask() {
-  let input = document.getElementById('texto-tarefa');
-  if (input.value !== "") {
-    let ol = document.getElementById('lista-tarefas');
-    let li = document.createElement("li");
+  const input = document.getElementById('texto-tarefa');
+  if (input.value !== '') {
+    const ol = document.getElementById('lista-tarefas');
+    const li = document.createElement('li');
     li.innerHTML = input.value;
     ol.appendChild(li);
-    input.value = "";
+    input.value = '';
   }
 }
 
