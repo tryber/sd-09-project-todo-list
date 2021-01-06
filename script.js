4c428b78ea00d6ee1b94256fe7a59f12bae26efd
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-06 10:32:52 -0300

    JS Started

---
+++ script.js
@@ -0,0 +1,5 @@
+const text = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
+const orderedList = document.querySelector('#lista-tarefas');
+const list = document.createElement('li');
+list.className = 'todo-list';
