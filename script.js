4458091a89fff96f89d23633be45ea889e08ffa0
Lucas Silva <martssilvapro@gmail.com>
2021-01-06 19:46:09 -0300

    funcao salvar lista feito

--- script.js
+++ script.js
@@ -35,42 +35,45 @@ function criarTarefa() {
   
     addTaskToList()
   
-    function changeBG(event) {
-  
-      let bgLi = document.querySelector('.bg-task');
-       
-      // verificar se existe a li
-  
-      if( bgLi ) {
-        bgLi.classList.remove('bg-task');
-  
-      } 
-  
-      // muda o bg
-  
-      event.target.classList.add('bg-task');
-  
-    } 
-  
-    function taskCompleted(event) {
-  
-      // propriedade contains verifica se algo contêm uma classe por exemplo
-  
-      if (event.target.classList.contains('completed') ){
-  
-        event.target.classList.remove('completed');
-  
-      } else {
-  
-        event.target.classList.add('completed');
-  
-      }
-  
-    }
+    
   })
 } 
 
 criarTarefa();
+
+function changeBG(event) {
+  
+  let bgLi = document.querySelector('.bg-task');
+   
+  // verificar se existe a li
+
+  if( bgLi ) {
+    bgLi.classList.remove('bg-task');
+
+  } 
+
+  // muda o bg
+
+  event.target.classList.add('bg-task');
+
+} 
+
+function taskCompleted(event) {
+  
+  // propriedade contains verifica se algo contêm uma classe por exemplo
+
+  if (event.target.classList.contains('completed') ){
+
+    event.target.classList.remove('completed');
+
+  } else {
+
+    event.target.classList.add('completed');
+
+  }
+
+}
+
 
 function removeSelectedTask() {
 
@@ -129,7 +132,7 @@ function cleanList() {
       taskList.remove('li');
     };
 
-    //list.parentNode.removeChild(list)
+    localStorage.clear();
 
   });
 
@@ -138,138 +141,54 @@ function cleanList() {
 
 cleanList();
 
+function saveList() {
 
-
-
-
-
-
-
-
-
-
-
-/*
-//Adiciona tarefa 
-
-function criarTarefa() {
-
-  const input = document.querySelector('#texto-tarefa');
-  const taskList = document.querySelector('#lista-tarefas');
-
-  function addTaskToList() {
-
-    // Criando o elemento
-    const newTask = document.createElement('li');
-
-    // Adicionando classe ao elemento
-    newTask.className = 'task';
-
-    // Adicionando o valor do input
-    newTask.innerText = input.value;
-
-    // Adicionar o elemento filho ao elemento pai
-    taskList.appendChild(newTask);
-
-    input.value = '';
-
-    // Muda a cor do background e da fonte ao receber o click
-    newTask.addEventListener('click', changeBG)
-
-    newTask.addEventListener('dblclick', taskCompleted)
-  
-  }
-
-
-  addTaskToList()
-
-  function changeBG(event) {
-
-    let bgLi = document.querySelector('.bg-task');
-     
-    // verificar se existe a li
-
-    if( bgLi ) {
-      bgLi.classList.remove('bg-task');
-
-    } 
-
-    // muda o bg
-
-    event.target.classList.add('bg-task');
-
-  } 
-
-  function taskCompleted(event) {
-
-    // propriedade contains verifica se algo contêm uma classe por exemplo
-
-    if (event.target.classList.contains('completed') ){
-
-      event.target.classList.remove('completed');
-
-    } else {
-
-      event.target.classList.add('completed');
-
-    }
-
-  }
-}
-
-function removeCompleted() {
-
-  const btn = document.querySelector('#remover-finalizados');
+  let btn = document.querySelector('#salvar-tarefas');
 
   btn.addEventListener('click', function() {
 
-    const list = document.querySelectorAll('.completed');
+    const list = document.querySelectorAll('.task')
+
+    let arr = [];
 
     for(index = 0; index < list.length; index += 1) {
-      let taskList = list[index];
 
-      taskList.remove('li');
-    };
+      let obj = {
+        text: list[index].textContent,
+        class: list[index].className
+      }
 
-  });
+      arr.push(obj) 
+    }
+    localStorage.setItem('List', JSON.stringify(arr))
 
-};
+  })
+}
 
-removeCompleted();
+saveList();
 
-function cleanList() {
+function showItemListOnLoad() {
 
-  // Meu botão
-  const btn = document.querySelector('#apaga-tudo');
-
-  // Adiciona um evento no botão 
-  btn.addEventListener('click', function () {
-
-    // Seleciona os elementos com a class task
-    const list = document.querySelectorAll('.task');
-
-    // Percorre a lista
-    for (index = 0; index < list.length; index += 1) {
-      let taskList = list[index];
-
-      // Remove o elemento
-      taskList.remove('li');
-    };
-
-    //list.parentNode.removeChild(list)
-
-  });
+  let list = document.querySelector("#lista-tarefas")
 
 
-};
+    let item = JSON.parse(localStorage.getItem('List'))
 
-cleanList();
+    for(index = 0; index < item.length; index += 1) {
 
+     let element = document.createElement('li');
 
-//newTask.style.backgroundColor = 'rgb(128, 128, 128)';
-      //newTask.style.color = 'rgb(255, 255, 255)';
+     element.textContent = item[index].text
+     element.className = item[index].class
 
+     list.appendChild(element)
+     element.addEventListener('click', changeBG)
+  
+     element.addEventListener('dblclick', taskCompleted)
+    }
 
-       //let task = input.value;
-//localStorage.setItem('input', JSON.stringify(task))
-*/
+    
+
+}
+
+showItemListOnLoad()
