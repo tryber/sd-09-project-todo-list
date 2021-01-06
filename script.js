c741a1758c65a3a58be11050ab562d5b57a1b973
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-06 13:49:50 -0300

    adicionando funcao colorir lista

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ function saveData() {
   const incluiTarefa = document.createElement('li');
   incluiTarefa.innerText = textoTarefa.value;
   incluiTarefa.className = 'tarefa-from-list';
+  incluiTarefa.addEventListener('click', mudaParaCinza);
   listaTarefas.appendChild(incluiTarefa);
   textoTarefa.value = '';
 }
+function mudaParaCinza() {
+  colorList = document.getElementsByClassName('tarefa-from-list');
+  for (let index = 0; index < colorList.length ; index +=1){
+    colorList[index].style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+
 const criarTarefa = document.getElementById('criar-tarefa');
 criarTarefa.addEventListener('click', saveData);
 
