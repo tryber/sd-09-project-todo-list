b9322b4de9e2d45d721a511a0dcd57bd042a71f4
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-05 22:03:16 -0300

    Requisito 7, tentativa 3

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 function createTask() {
-  const input = document.getElementById('texto-tarefa');
-  if (input.value !== '') {
-    const ol = document.getElementById('lista-tarefas');
-    const li = document.createElement('li');
+  const input = document.getElementById("texto-tarefa");
+  if (input.value !== "") {
+    const ol = document.getElementById("lista-tarefas");
+    const li = document.createElement("li");
     li.innerHTML = input.value;
     ol.appendChild(li);
-    input.value = '';
+    input.value = "";
   }
 }
 
 function changeColor() {
-  const li = document.getElementsByTagName('li');
-  for (let index = 0; index < li.length; index++) {
-    li[index].addEventListener('click', function () {
-      li[index].style.backgroundColor = 'rgb(' + 128 + ',' + 128 + ',' + 128 + ')';
-    });
-  }
+  const lista = document.getElementById('lista-tarefas');
+  lista.addEventListener('click', function (e) {
+    const selectedLista = document.getElementsByClassName('selected');
+    if (selectedLista[0]) {
+      selectedLista[0].classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+  });
 }
 
 window.onload = function () {
-  createTask()
-  changeColor()
+  createTask();
+  changeColor();
 };
