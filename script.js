0f8551dd057114679ff9536dbe882a0ce39dc4ff
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-06 22:45:41 -0300

    pushColour (Listener) da Lista de Tarefas

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const buttonCriarTarefa = document.querySelector("#criar-tarefa");
 
-function checkInput() {
+function checkInput(){
     let textoTarefa = document.querySelector("#texto-tarefa").value;
-    let adicionaTarefa = document.querySelector("#lista-tarefa");
+    let adicionaTarefa = document.querySelector("#lista-tarefas");
     let criaTarefa = document.querySelector("li");
 
     adicionaTarefa.appendChild(criaTarefa).innerText = textoTarefa;
@@ -10,4 +10,18 @@ function checkInput() {
 }
 
 buttonCriarTarefa.addEventListener("click", checkInput);
+
+const pushColour = document.querySelector("#lista-tarefas");
+
+function backgroundColour(colorize){
+    let deleteBackgroundColour = document.querySelectorAll("li");
+
+    for (let index=0; index < deleteBackgroundColour.length; index += 1){
+        deleteBackgroundColour[index].style.backgroundColor = "white";}
+
+    let listaDeItens = colorize.target;
+    listaDeItens.style.backgroundColor = "rgb(128, 128, 128)";
+}
+
+pushColour.addEventListener("click", backgroundColour);
 
