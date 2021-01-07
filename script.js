3f75aa7a210c203b8c2b6b17e2179990ddd28ba4
Victor Costa <victordiricoo@gmail.com>
2021-01-07 04:41:31 -0300

    requisito 7

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function btn() {
     document.getElementById("lista-tarefas").appendChild(li);
     document.getElementById("texto-tarefa").value = "";
 }
+
+let list = document.querySelector('ol');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.style.backgroundColor = 'rgb(128, 128, 128)'
+  }
+}, false);
