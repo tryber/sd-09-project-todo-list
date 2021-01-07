9dd4cbdc8e79f5515c3eb01eb28c0d09c0113053
PeryclesF <perycles_reis@hotmail.com>
2021-01-07 09:36:44 -0300

    refatorando codigo

--- script.js
+++ script.js
@@ -1,111 +1,75 @@
-const botao = document.querySelector('button');
-const botao2 = document.querySelectorAll('button')[1];
-const botao3 = document.querySelectorAll('button')[2];
+const listaDeTarefas = document.querySelector("#lista-tarefas");
+const textInput = document.querySelector("#texto-tarefa");
+const botao = document.querySelector("button");
+const botao2 = document.querySelectorAll("button")[1];
+const botao3 = document.querySelectorAll("button")[2];
 
-function criaParagrafo(textString) {
-  const paragraph = document.querySelector('p');
-  paragraph.id = 'funcionamento';
-  paragraph.innerHTML = textString;
+function criaParagrafo() {
+  const paragraph = document.querySelector("p");
+  paragraph.id = "funcionamento";
+  paragraph.innerHTML =
+    "Clique duas vezes em um item para marcá-lo como completo.";
 }
-criaParagrafo('Clique duas vezes em um item para marcá-lo como completo.');
-
-function caixaTexto(idString) {
-  const newInput = document.querySelector('input');
-  newInput.id = idString;
-}
-caixaTexto('texto-tarefa');
-
-function criarLista() {
-  const secaoOl = document.querySelector('.ol');
-  const newList = document.createElement('ol');
-  newList.id = 'lista-tarefas';
-  secaoOl.appendChild(newList);
-}
-criarLista();
-
-function criarBotao(tarefa) {
-  const newTask = document.querySelector('button');
-  newTask.id = tarefa;
-  newTask.innerHTML = 'Adicionar';
-}
-criarBotao('criar-tarefa');
+criaParagrafo();
 
 // limpa o o texto digitado na caixa de texto
 function limpaTexto() {
-  const textInput = document.querySelector('#texto-tarefa');
-  if (textInput.value !== '') {
-    textInput.value = '';
+  if (textInput.value) {
+    textInput.value = "";
   }
 }
 
 // limpa elemento com class color
 function pegaItem() {
-  const itensLista = document.querySelector('#lista-tarefas').childNodes;
+  const itensLista = listaDeTarefas.childNodes;
   for (let index = 0; index < itensLista.length; index += 1) {
     const elementoItem = itensLista[index];
-    if (elementoItem.classList.contains('color')) {
-      elementoItem.classList.remove('color');
+    if (elementoItem.classList.contains("color")) {
+      elementoItem.classList.remove("color");
     }
   }
 }
 
 // funcao para selecionar elemento da lista com acao de click
 function trocaFundoItemLista() {
-  const itensLista = document.querySelector('#lista-tarefas').childNodes;
+  const itensLista = listaDeTarefas.childNodes;
   for (let index = 0; index < itensLista.length; index += 1) {
     const elementoItem = itensLista[index];
-    elementoItem.addEventListener('click', function (event) {
+    elementoItem.addEventListener("click", function (event) {
       pegaItem();
-      event.target.classList.toggle('color');
+      event.target.classList.toggle("color");
     });
   }
 }
 
 function removeItemCompleto() {
-  const filhosDeOl = document.querySelector('#lista-tarefas');
-  const selectedClass = document.querySelector('.completed');
+  const selectedClass = document.querySelector(".completed");
   if (selectedClass) {
-    filhosDeOl.removeChild(selectedClass);
+    listaDeTarefas.removeChild(selectedClass);
   }
 }
-
-// cria botao remover item
-function criaBotaoRemove() {
-  botao3.id = 'remover-finalizados';
-  botao3.innerText = 'Remover Item';
-}
-criaBotaoRemove();
 
 // cria uma lista ordenada dentro de ol
 function itemLista() {
-  const item = document.querySelector('#lista-tarefas');
-  const textInput = document.querySelector('#texto-tarefa');
-  const listaTarefa = document.createElement('li');
+  const textInput = document.querySelector("#texto-tarefa");
+  const listaTarefa = document.createElement("li");
   listaTarefa.innerText = textInput.value;
-  item.appendChild(listaTarefa);
+  listaDeTarefas.appendChild(listaTarefa);
   limpaTexto();
   trocaFundoItemLista();
 }
-botao.addEventListener('click', itemLista);
-botao3.addEventListener('click', removeItemCompleto);
 
 // funcao para riscar um item da lista que ja foi completado
-const listaDeTarefas = document.querySelector('#lista-tarefas');
 function riscaItem(event) {
-  const selecionarItem = document.querySelector('.completed');
+  const selecionarItem = document.querySelector(".completed");
   const tarefaSelecionada = event.target;
   if (selecionarItem) {
-    tarefaSelecionada.classList.remove('completed');
+    tarefaSelecionada.classList.remove("completed");
   } else {
-    tarefaSelecionada.classList.add('completed');
+    tarefaSelecionada.classList.add("completed");
   }
 }
-listaDeTarefas.addEventListener('dblclick', riscaItem);
-
-window.onload = function criaBotaoApagar() {
-  botao2.id = 'apaga-tudo';
-  botao2.innerText = 'Apagar Lista';
-};
+listaDeTarefas.addEventListener("dblclick", riscaItem);
 
 // remove itens da lista
 function apagaLista() {
@@ -113,4 +77,9 @@ function apagaLista() {
     listaDeTarefas.removeChild(listaDeTarefas.firstChild);
   }
 }
-botao2.addEventListener('click', apagaLista);
+
+window.onload = function () {
+  botao.addEventListener("click", itemLista);
+  botao2.addEventListener("click", apagaLista);
+  botao3.addEventListener("click", removeItemCompleto);
+};
