a999d55d89abb77755f7c68854198df851d6d923
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-01-07 13:55:02 -0300

    Refatorando funcao para adicionar tarefas com enter e click

--- script.js
+++ script.js
@@ -1,43 +1,29 @@
-function addTasks() {
-  const olTaskList = document.querySelector('#lista-tarefas');
-  const buttonTasks = document.querySelector('#criar-tarefa');
-  buttonTasks.addEventListener('click', function(){
-    const lengthValue = document.querySelector('#texto-tarefa').value.length
-      if(lengthValue > 0){
-        const textTasks = document.querySelector('#texto-tarefa');
-        const li = document.createElement('li');
-        li.innerText = textTasks.value;
-        olTaskList.appendChild(li);
-        textTasks.value = '';
-      }
-  })
-}
-addTasks();
+const buttonTasks = document.querySelector('#criar-tarefa');
+const input = document.querySelector('#texto-tarefa');
 
-function addKeyTasks(){
+function addTasks(event) {  
   const olTaskList = document.querySelector('#lista-tarefas');
-  const input = document.querySelector('#texto-tarefa');
-  input.addEventListener('keyup', function(event){
-    if(event.keyCode === 13){
-      const lengthValue = document.querySelector('#texto-tarefa').value.length
-      if(lengthValue > 0){
-        const textTasks = document.querySelector('#texto-tarefa');
-        const li = document.createElement('li');
-        li.innerText = textTasks.value;
-        olTaskList.appendChild(li);
-        textTasks.value = '';
-      }
+  const lengthValue = document.querySelector('#texto-tarefa').value.length
+  const enter = lengthValue > 0 && event.keyCode === 13
+  const click = lengthValue > 0 && event.target.id === 'criar-tarefa'
+    if(enter || click) {      
+      const textTasks = document.querySelector('#texto-tarefa');
+      const li = document.createElement('li');
+      li.innerText = textTasks.value;
+      olTaskList.appendChild(li);
+      textTasks.value = '';
     }
-  })
 }
-addKeyTasks();
+buttonTasks.addEventListener('click', addTasks)
+input.addEventListener('keyup' , addTasks)
 
-function itemSelected(){
+
+function itemSelected() {
   const olTaskList = document.querySelector('ol#lista-tarefas');
-  olTaskList.addEventListener('click', function(event){    
+  olTaskList.addEventListener('click', function(event) {    
     const li = event.target;
     const list = document.querySelector('.selectedList');
-    if(list){
+    if(list) {
       list.classList.remove('selectedList');      
     }  
     li.classList.add('selectedList');
@@ -47,22 +33,22 @@ itemSelected();
 
 
 const btnClearList = document.querySelector('#apaga-tudo');
-function removeList(){
-btnClearList.addEventListener('click', function(){
-  const clearList = document.querySelectorAll('ol li');
+function removeList() {
+btnClearList.addEventListener('click', function() {
+  const olLi = document.querySelectorAll('ol li');
   const ol = document.querySelector('ol');
-  for(let indexClear = 0; indexClear < clearList.length; indexClear +=1){
-    ol.removeChild(clearList[indexClear]);
+  for(let indexClear = 0; indexClear < olLi.length; indexClear +=1) {
+    ol.removeChild(olLi[indexClear]);
   }
 })
 }
 removeList();
 
-function DoubleClick(){
+function DoubleClick() {
   const ol = document.querySelector('ol#lista-tarefas');
-  ol.addEventListener('dblclick', function(event){
+  ol.addEventListener('dblclick', function(event) {
     const dlb = event.target;
-    if(dlb.className.includes('completed')){
+    if(dlb.className.includes('completed')) {
       dlb.classList.remove('completed')
     } else {
       dlb.classList.add('completed')
@@ -71,14 +57,14 @@ function DoubleClick(){
 }
 DoubleClick();
 
-function removeFinished(){
+function removeFinished() {
   const btnRemoveFinished = document.querySelector('#remover-finalizados');
-  btnRemoveFinished.addEventListener('click', function(){
-    const cleanFinished = document.querySelectorAll('ol li');
+  btnRemoveFinished.addEventListener('click', function() {
+    const olLi = document.querySelectorAll('ol li');
     const ol = document.querySelector('ol');
-    for(let index = 0; index < cleanFinished.length; index += 1){
-      if(cleanFinished[index].className.includes('completed')){
-        ol.removeChild(cleanFinished[index]);
+    for(let index = 0; index < olLi.length; index += 1) {
+      if(olLi[index].className.includes('completed')) {
+        ol.removeChild(olLi[index]);
       }
     }
   })
