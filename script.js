a3d9ce29964bd0a78c5875dff697b70b3cd0b95f
pauloeliezerg <pauloeliezerg@gmail.com>
2021-01-07 06:35:28 -0300

    Refactored taskCreation function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const taskCreationButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
-taskCreationButton.addEventListener('click', taskCreation);
-
 function taskCreation() {
-  let inputText = document.getElementById('texto-tarefa').value;
-  let taskText = taskList.appendChild(document.createElement('li'));
+  const inputText = document.getElementById('texto-tarefa').value;
+  const taskText = taskList.appendChild(document.createElement('li'));
   taskText.innerHTML = inputText;
-  console.log(taskText);
+  document.getElementById('texto-tarefa').value = '';
 }
+
+taskCreationButton.addEventListener('click', taskCreation);
