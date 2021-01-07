d11f8b5da88b3404d111b833e78f43905b0b2059
pauloeliezerg <pauloeliezerg@gmail.com>
2021-01-07 08:07:41 -0300

    Added taskCompletion function

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ function taskCreation() {
   const inputText = document.getElementById('texto-tarefa').value;
   const taskText = taskList.appendChild(document.createElement('li'));
   taskText.innerHTML = inputText;
+  taskText.className = 'uncomplete';
   document.getElementById('texto-tarefa').value = '';
 }
 
 function changeTaskColor(originEvent) {
-  let taskCollection = document.querySelectorAll('li');
-  for (index = 0; index < taskCollection.length; index += 1) {
+  const taskCollection = document.querySelectorAll('li');
+  for (let index = 0; index < taskCollection.length; index += 1) {
     taskCollection[index].style.backgroundColor = 'white';
   }
   originEvent.target.style.backgroundColor = 'gray';
 }
 
+function taskCompletion(originEvent) {
+  if (originEvent.target.style.textDecoration === 'line-through') {
+    originEvent.target.style.textDecoration = 'none';
+    originEvent.target.className = 'uncomplete';
+  } else {
+    originEvent.target.style.textDecoration = 'line-through';
+    originEvent.target.className = 'completed';
+  }
+}
+
 taskCreationButton.addEventListener('click', taskCreation);
 taskList.addEventListener('click', changeTaskColor);
+taskList.addEventListener('dblclick', taskCompletion);
