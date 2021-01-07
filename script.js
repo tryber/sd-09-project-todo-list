0fceacae132c5620e5201507fe8bc03ec05016d3
Victor Costa <victordiricoo@gmail.com>
2021-01-07 11:03:31 -0300

    requisito 10

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ function btn() {
   let inputValue = document.getElementById("texto-tarefa").value;
   let t = document.createTextNode(inputValue);
   li.appendChild(t);
+    li.classList.add('corFundo')
     document.getElementById("lista-tarefas").appendChild(li);
     document.getElementById("texto-tarefa").value = "";
 }
 
+//Requisito 7 e 8
 let list = document.querySelector('ol');
 list.addEventListener('click', function(param) {
   if (param.target.tagName === 'LI') {
     param.target.style.backgroundColor = 'rgb(128, 128, 128)'
   }
-}, false);
+})
 
+// Requisito 10
 let btnApagar = document.getElementById('apaga-tudo');
 btnApagar.addEventListener('click', apagaTudo);
 
 function apagaTudo() {
   let lista = document.getElementById('lista-tarefas');
-  let tarefas = document.querySelector('li')
-  lista.removeChild(tarefas);
+  while(lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
 }
