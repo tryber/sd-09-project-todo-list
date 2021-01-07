872a1790d1d0eaf038965167717e809034a9b260
Pedro Castro <pa.coelho.castro@gmail.com>
2021-01-07 01:44:35 -0300

    Chasing requirements

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ let clearAllBtn = document.querySelector('#apaga-tudo');
 addButton.addEventListener('click', () => {
   task = document.querySelector('#texto-tarefa').value;
     
-  let listItem = document.createElement('li');
+  let listItem = document.createElement('li')
   listItem.innerText = task;
+  listItem.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'rgb(128, 128, 128)'
+  })
+  listItem.addEventListener('dblclick', (e) => {
+    e.target.style.textDecoration = 'line-through'
+  })
   document.querySelector('#texto-tarefa').value = '';
   document.querySelector('#texto-tarefa').focus();
   taskList.appendChild(listItem);
