4ac04baa7dc8539efb0bdf291168c2c235edb96f
Pedro Castro <pa.coelho.castro@gmail.com>
2021-01-07 01:55:32 -0300

    Task #10

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ addButton.addEventListener('click', () => {
     e.target.style.backgroundColor = 'rgb(128, 128, 128)'
   })
   listItem.addEventListener('dblclick', (e) => {
-    e.target.style.textDecoration = 'line-through'
+    if (e.target.classList.contains = 'completed') {
+      e.target.classList.remove = 'completed'
+    } else {
+      e.target.classList.add = 'completed'
+    }
+    
   })
   document.querySelector('#texto-tarefa').value = '';
   document.querySelector('#texto-tarefa').focus();
   taskList.appendChild(listItem);
 });
+
+clearAllBtn.addEventListener('click', () => {
+  taskList.innerHTML = ''
+})
