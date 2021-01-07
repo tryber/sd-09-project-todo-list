622dbd27fdf9813d13e34a6b19f60086833692da
Victor Costa <victordiricoo@gmail.com>
2021-01-07 04:29:28 -0300

    fazendo uns testes na lista

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Requisito 5
 function btn() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("texto-tarefa").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-        document.getElementById("lista-tarefas").appendChild(li);
-        document.getElementById("texto-tarefa").value = "";
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("texto-tarefa").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+    document.getElementById("lista-tarefas").appendChild(li);
+    document.getElementById("texto-tarefa").value = "";
 }
