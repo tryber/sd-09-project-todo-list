8ae360d06071d416b90117c3c715d0adc4c59f63
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-01-07 01:32:21 -0300

    alterando cor do item da lista

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+function changeBackgroundColor(event) {
+  event.target.style.backgroundColor = 'gray';
+}
+
 function addAssignment() {
   const inputTaskText = document.getElementById('texto-tarefa');
   const listItem = document.createElement('li');
   listItem.innerText = inputTaskText.value;
-  const olListaTarefa = document.getElementById('lista-tarefas')
+  const olListaTarefa = document.getElementById('lista-tarefas');
   olListaTarefa.appendChild(listItem);
   inputTaskText.value = '';
+  listItem.addEventListener('click', changeBackgroundColor);
 }
 
 const buttonInforms = document.getElementById('criar-tarefa');
 buttonInforms.addEventListener('click', addAssignment);
-
