f2501b40af858498585b329e2e201bd419ed08f5
MarceloPelegrino <pelegrinocorrea@gmail>
2021-01-18 19:24:49 -0300

    Requisitos 6 7 8 Dia 18-01-2021 19:24

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-window.onload = function () {
-  // Elementos recuperado
-  const creatingTasks = document.querySelector('#criar-tarefa');
-  
-  // Eventos criados
-  creatingTasks.addEventListener('click', addingTasks);
-}
-
 // Botão criar tarefas
 const textEntry = document.querySelector('#texto-tarefa');
 const list = document.querySelector('#lista-tarefas');
-function addingTasks () {
+function addingTasks() {
   const item = document.createElement('li');
+  item.className = 'item';
   item.innerText = textEntry.value;
   list.appendChild(item);
   textEntry.value = '';
-}
+  item.addEventListener('click', selectingItem);
+};
+
+// Retirando a seleção do item
+function removingSelection() {
+  const selection = document.querySelectorAll('.selected');
+  for (let index = 0; index < selection.length; index += 1) {
+    selection[index].classList.remove('selected');
+  }
+};
+
+// Selecionando item
+function selectingItem(event) {
+  removingSelection();
+  event.target.className = 'selected';
+};
+
+window.onload = function () {
+  // Elementos recuperados
+  const creatingTasks = document.querySelector('#criar-tarefa');
+
+  // Eventos criados
+  creatingTasks.addEventListener('click', addingTasks);
+};
