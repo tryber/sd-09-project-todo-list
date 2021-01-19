50233f8074ad04cd39e3ef829e6822b63352e7e8
MarceloPelegrino <pelegrinocorrea@gmail>
2021-01-19 10:45:55 -0300

    Requisito 9 Dia 19-01-2021 10:45

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Retirando a seleção do item
+function removingSelection() {
+  const selection = document.querySelectorAll('.selected');
+  for (let index = 0; index < selection.length; index += 1) {
+    selection[index].classList.remove('selected');
+  }
+}
+
+// Selecionando item
+function selectingItem(event) {
+  removingSelection();
+  event.target.className = 'selected';
+}
+
+// Item completo efeito letra tachada
+function itemComplete(event) {
+  if (event.className === 'completed') {
+    event.classList.remove('completed');
+  } else {
+    event.target.className = 'completed';
+  }
+}
+
 // Botão criar tarefas
 const textEntry = document.querySelector('#texto-tarefa');
 const list = document.querySelector('#lista-tarefas');
@@ -8,21 +31,8 @@ function addingTasks() {
   list.appendChild(item);
   textEntry.value = '';
   item.addEventListener('click', selectingItem);
-};
-
-// Retirando a seleção do item
-function removingSelection() {
-  const selection = document.querySelectorAll('.selected');
-  for (let index = 0; index < selection.length; index += 1) {
-    selection[index].classList.remove('selected');
-  }
-};
-
-// Selecionando item
-function selectingItem(event) {
-  removingSelection();
-  event.target.className = 'selected';
-};
+  item.addEventListener('dblclick', itemComplete);
+}
 
 window.onload = function () {
   // Elementos recuperados
