f66627ccbe46f92a6098e680e06b60afd7c307da
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-01-20 04:38:02 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function createTask () {
+const taskList = document.getElementById('lista-tarefas');
+
+function createTask() {
   const taskName = document.getElementById('texto-tarefa');
-  const elementTask = document.createElement('p');
+  const elementTask = document.createElement('li');
   elementTask.innerText = taskName.value;
-  document.getElementById('lista-tarefas').appendChild(elementTask);
+  taskList.appendChild(elementTask);
   taskName.value = '';
 }
 
+function selectedTask(newSelected) {
+  const selected = document.querySelector('.selected');
+  if (selected) {
+    selected.className = '';
+    newSelected.target.className = 'selected';
+  } else {
+    newSelected.target.className = 'selected';
+  }
+}
+
 document.getElementById('criar-tarefa').addEventListener('click', createTask);
+taskList.addEventListener('click', selectedTask);
