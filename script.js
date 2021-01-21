7c426da1290ad3c14e0d8fa3b83872461a049209
MarceloPelegrino <pelegrinocorrea@gmail>
2021-01-21 18:37:45 -0300

    Requisitos bonus 12 13 14 Dia 21-01-2021 18:37

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ function itemComplete(event) {
 function deletingList() {
   const retrievesItems = document.querySelectorAll('.item');
   if (retrievesItems.length !== 0) {
-    let list = document.querySelector('#lista-tarefas');
+    const list = document.querySelector('#lista-tarefas');
     list.innerHTML = '';
   }
 }
 
 // Botão apagar itens finalizados
 function deletingCompletedItem() {
-  let itemsMade = document.querySelectorAll('.completed');
+  const itemsMade = document.querySelectorAll('.completed');
   for (let index = 0; index < itemsMade.length; index += 1) {
     itemsMade[index].remove();
   }
@@ -51,14 +51,62 @@ function addingTasks() {
   item.addEventListener('dblclick', itemComplete);
 }
 
-window.onload = function () {
+// Salvando a lista
+function savingList() {
+  localStorage.setItem('list', list.innerHTML);
+}
+
+// Recuperando ao iniciar
+function retrievingList() {
+  list.innerHTML = localStorage.getItem('list');
+  const recoverItem = document.querySelectorAll('li');
+  for (let index = 0; index < recoverItem.length; index += 1) {
+    recoverItem[index].addEventListener('click', selectingItem);
+    recoverItem[index].addEventListener('dblclick', itemComplete);
+  }
+}
+
+// Movendo item selecionado para cima
+function movingUp() {
+  const selectedItem = document.querySelector('.selected');
+  if (selectedItem !== undefined && selectedItem.previousElementSibling !== null) {
+    selectedItem.parentNode.insertBefore(selectedItem, selectedItem.previousElementSibling);
+  }
+}
+
+// Movendo item selecionado para baixo
+function movingDown() {
+  const selectedItem = document.querySelector('.selected');
+  if (selectedItem !== undefined && selectedItem.nextSibling !== null) {
+    selectedItem.parentNode.insertBefore(selectedItem.nextSibling, selectedItem);
+  }
+}
+
+// Botão remover item selecionado
+function removingSelectedItem() {
+  const selectedItem = document.querySelector('.selected');
+  selectedItem.remove();
+}
+
+window.onload = function() {
   // Elementos recuperados
   const creatingTasks = document.querySelector('#criar-tarefa');
   const clearList = document.querySelector('#apaga-tudo');
   const deleteCompletedItem = document.querySelector('#remover-finalizados');
+  const saveList = document.querySelector('#salvar-tarefas');
+  const moveUp = document.querySelector('#mover-cima');
+  const moveDown = document.querySelector('#mover-baixo');
+  const removeItemButton = document.querySelector('#remover-selecionado');
 
   // Eventos criados
   creatingTasks.addEventListener('click', addingTasks);
   clearList.addEventListener('click', deletingList);
   deleteCompletedItem.addEventListener('click', deletingCompletedItem);
+  saveList.addEventListener('click', savingList);
+  moveUp.addEventListener('click', movingUp);
+  moveDown.addEventListener('click', movingDown);
+  removeItemButton.addEventListener('click', removingSelectedItem);
+
+  // Chamando funções
+  retrievingList ();
 };
