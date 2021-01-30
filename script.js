32fdf1eaf9ec38d71da0f6d7241abd331b768e79
Caio <caioqs.info@gmail.com>
2021-01-30 13:10:19 -0300

    Feito 5º requisito

---
+++ script.js
@@ -0,0 +1,13 @@
+const textoTarefa = document.querySelector('#texto-tarefa');
+const criarTarefa = document.querySelector('#criar-tarefa');
+const listaTarefa = document.querySelector('#lista-tarefas');
+
+const createTasks = () => {
+    let inputValue = textoTarefa.value;
+    const li = document.createElement('li');
+    li.innerText = inputValue;
+    listaTarefa.appendChild(li);
+    textoTarefa.value = '';
+}
+
+criarTarefa.addEventListener('click', createTasks);
