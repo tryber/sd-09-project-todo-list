81a26e345d0f63ca1a7464028edcb5f99a7ea656
thosijulio <thosijulio@gmail.com>
2021-01-31 04:53:39 -0300

    resolucao exercicio 11

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function adicionarTarefa () {
       textoInput.value = '';
     }
     else {
-        alert('Digite uma tarefa');
+      alert('Digite uma tarefa');
     }
   })
 }
@@ -19,30 +19,44 @@ function adicionarTarefa () {
 function tarefaSelecionada () {
   let listaTarefas = document.querySelector ('#lista-tarefas');
   listaTarefas.addEventListener ('click', function (event) {
-      let itensLista = document.getElementsByTagName ('li');
-      for (index = 0; index < itensLista.length; index += 1) {
-          itensLista[index].style.backgroundColor = '';
-      }
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    let itensLista = document.getElementsByTagName ('li');
+    for (index = 0; index < itensLista.length; index += 1) {
+        itensLista[index].style.backgroundColor = '';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   })
 }
 
 function tarefaConcluida () {
-    let listaTarefas = document.querySelector ('#lista-tarefas');
-    listaTarefas.addEventListener ('dblclick', function (event) {
-        event.target.classList.toggle ('completed');
-    })
+  let listaTarefas = document.querySelector ('#lista-tarefas');
+  listaTarefas.addEventListener ('dblclick', function (event) {
+      event.target.classList.toggle ('completed');
+  })
 }
 
 function apagarTarefa () {
-    let listaTarefas = document.querySelector ('#lista-tarefas');
-    let botaoApaga = document.querySelector ('#apaga-tudo');
-    botaoApaga.addEventListener ('click', function () {
-        listaTarefas.innerText = '';
-    })
+  let listaTarefas = document.querySelector ('#lista-tarefas');
+  let botaoApaga = document.querySelector ('#apaga-tudo');
+  botaoApaga.addEventListener ('click', function () {
+      listaTarefas.innerText = '';
+  })
+}
+
+function removeTarefaConcluida () {
+  let listaTarefas = document.querySelector ('#lista-tarefas');
+  let botaoLimpaConcluidos = document.querySelector ('#remover-finalizados');
+  let itensLista = document.getElementsByTagName ('li');
+  botaoLimpaConcluidos.addEventListener ('click', function () {
+    for (index = 0; index < itensLista.length; index += 1) {
+        if (itensLista[index].classList.contains ('completed')) {
+            listaTarefas.removeChild(itensLista[index]);
+        }
+    }
+  })
 }
 
 adicionarTarefa ();
 tarefaSelecionada ();
 tarefaConcluida ();
 apagarTarefa ();
+removeTarefaConcluida ();
