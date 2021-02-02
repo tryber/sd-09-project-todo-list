17e3203b72b0129a003b4c6a1fb16d11818d160a
Marcos Mantovani <mvmes23@gmail.com>
2021-02-01 21:58:59 -0300

    nao deve ter mais de 1 li selecionada ao mesmo tempo tentativa3

--- script.js
+++ script.js
@@ -1,84 +1,46 @@
-let inputBox = document.getElementById('texto-tarefa');
-let button = document.querySelector('#criar-tarefa');
-let listItens = document.querySelectorAll('li');
-let capturedText = '';
+let createElementLi = document.createElement('li');
+let listaOl = document.querySelector('#lista-tarefas');
+const createTaskButton = document.querySelector('#criar-tarefa');
+let text = document.querySelector('#texto-tarefa');
 let orderCount = 0;
 
-function captureInputAndCreate() {
-    button.addEventListener('click', function() {
-        let captureText = document.getElementById('texto-tarefa').value;
-        let ul = document.querySelector('#lista-tarefas');
-        if (capturedText === captureText && capturedText != '') {
-            alert('Você já tem esta tarefa na lista.');
-        } else {
-            if (captureText != '' && captureText != 'escreva uma tarefa...' && capturedText != captureText) {
-                capturedText = captureText;
-                let create = document.createElement('li');
-                let createBox = document.createElement('div');
-                createBox.style.width = '20px';
-                createBox.style.height = '20px';
-                createBox.style.display = 'inline-block';
-                create.innerText = capturedText;
-                orderCount += 1;
-                create.style.order = orderCount;                
-                create.style.alignItems = 'center';
-                create.classList.add('task');
-                create.addEventListener('click', function(event) {
-                    let selected = document.querySelector('.selected');
-                    if (selected && event.target.classList.contains('task')) {
-                        selected.classList.remove('selected');
-                        selected.style.backgroundColor = '';
-                    }
-                    if (event.target.classList.contains('task')) {
-                        event.target.classList.add('selected');
-                        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-                    }
-                })
-                ul.appendChild(create);
-                create.appendChild(createBox);
-                document.getElementById('texto-tarefa').value = '';
-            } else {
-                alert('Digite alguma tarfa primeiro');
-            }
+
+
+const createTask = () =>{
+    createTaskButton.addEventListener('click', function(){ 
+        let createElementLi = document.createElement('li');      
+        createElementLi.innerText = text.value;
+        liStyle(createElementLi);
+        listaOl.appendChild(createElementLi); 
+        text.value= '';       
+    })
+}
+
+const selectTask = () => { 
+    let listaOl = document.querySelector('#lista-tarefas');   
+    listaOl.addEventListener('click', function(event) {
+    let selectedList = document.querySelector('.selected');
+        if(selectedList) {
+            selectedList.classList.remove('selected');
         }
+        event.target.classList.add('selected');
     })
 }
 
-function eraseInputBox() {
-    let erase = document.getElementById('texto-tarefa');
-    erase.addEventListener('click', function() {
-        erase.value = '';
-    })
+const clearList = () => {
+
+}
+
+const liStyle = (createElementLi) => {
+    orderCount += 1;
+    createElementLi.style.order = orderCount;
+       
+    
 }
 
 
-function completedTask() {
-    let list = document.querySelector('#lista-tarefas');
-    list.addEventListener('dblclick', function(event) {
-        let completed = document.querySelector('.completed');
-        if (completed && event.target.classList.contains('completed')) {
-            event.target.classList.remove('completed');
-            event.target.firstElementChild.classList.remove('checkedBox')
-        } 
-        else  {
-            event.target.classList.add('completed');
-            event.target.firstElementChild.classList.add('checkedBox')
-
-        }
-    })
-}
-
-function eraseButtom () {
-    let eraseButtom = document.getElementById('apaga-tudo');    
-    let list = document.querySelector('#lista-tarefas');    
-    eraseButtom.addEventListener('click', function(event) {        
-        list.innerHTML = '';           
-    })
-}
-
-window.onload = function() {
-    captureInputAndCreate();
-    eraseInputBox();    
-    completedTask();
-    eraseButtom ();
+window.onload = () => {
+    createTask();
+    selectTask();
+    
 }
