0b7334325cfd6dd262022aa719cad0dff528da50
Caio <caioqs.info@gmail.com>
2021-02-07 11:14:26 -0300

    feito project todolist

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let text = document.querySelector('#texto-tarefa');
 let createElementLi = document.createElement('li');
 let listaOl = document.querySelector('#lista-tarefas');
 const createTaskButton = document.querySelector('#criar-tarefa');
+const eraseAllButtom = document.querySelector('#apaga-tudo');
+const eraseCompletedButtom = document.querySelector('#remover-finalizados');
+const buttomUp = document.querySelector('#mover-cima');
+const buttomDown = document.querySelector('#mover-baixo');
+const buttomRemoveSelected = document.querySelector('#remover-selecionado');
 
 
 const createTask = () =>{
@@ -31,7 +36,73 @@ const addSelectTask = () => {
     })
 }
 
+const addCompletedTask = () => {    
+    listaOl.addEventListener('dblclick', function(event){    
+        if (event.target.classList.contains('completed')){
+            event.target.classList.remove('completed')            
+        } else {
+            event.target.classList.add('completed');
+        }        
+    })
+}
+
+const clearList = () => {
+    eraseAllButtom.addEventListener('click', function(){
+        listaOl.innerHTML = '';
+    })
+
+}
+
+const clearCompletedTasks = () => {
+    eraseCompletedButtom.addEventListener('click', function(){
+        let completeds = document.querySelectorAll('li.completed');
+        completeds.forEach((key) => {
+            listaOl.removeChild(key);
+        })
+    })
+}
+
+const moveTaskUp = () => {
+    buttomUp.addEventListener('click', function(){
+        let taskList = document.querySelectorAll('li');
+        for (let index = 0; index < taskList.length; index +=1) {
+            let pos = taskList[index];
+            if(pos.classList.contains('selected') && pos.previousElementSibling !== null){
+                listaOl.insertBefore(pos, taskList[index - 1]);
+            }
+        }
+    })
+}
+
+const moveTaskDown = () => {
+    buttomDown.addEventListener('click', function(){
+        let taskList = document.querySelectorAll('li');
+        for (let index = 0; index < taskList.length; index +=1) {
+            let pos = taskList[index];
+            if(pos.classList.contains('selected') && pos.nextElementSibling !== null){
+                listaOl.insertBefore(taskList[index + 1], pos);
+            }
+        }
+    })
+}
+
+const clearSelectedTask = () => {
+    buttomRemoveSelected.addEventListener('click', function() {
+        let selected = document.querySelectorAll('li.selected');
+        selected.forEach((key) => {
+            listaOl.removeChild(key);
+        }) 
+    })
+}
+
+
 window.onload = () => {
     createTask();
     addSelectTask();
+    addCompletedTask();
+    clearList();  
+    clearCompletedTasks();  
+    moveTaskUp();
+    moveTaskDown();
+    clearSelectedTask();
 }
