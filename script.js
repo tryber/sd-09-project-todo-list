0f9a5d333ae5e712978957bfd54b79d09a35e989
Caio <caioqs.info@gmail.com>
2021-02-07 09:29:39 -0300

    feito requisito 2

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const textoTarefa = document.querySelector('#texto-tarefa');
 const criarTarefa = document.querySelector('#criar-tarefa');
 const listaTarefa = document.querySelector('#lista-tarefas');
 
+
 const createTasks = () => {
     let inputValue = textoTarefa.value;
     const li = document.createElement('li');
     li.innerText = inputValue;
     listaTarefa.appendChild(li);
     textoTarefa.value = '';
-    li.addEventListener('click', (event) => {
-        event.target.style.backgroundColor = 'rgb(128,128,128)';
+    listaTarefa.addEventListener('click', (event) => {
+        
     });
 }
 
 criarTarefa.addEventListener('click', createTasks);
+
