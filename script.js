39915ac1913dc566e7de489458d47df1ef3a20eb
Arlesson Moura <arlessonmss@gmail.com>
2021-02-08 23:33:56 -0300

    Task Selection gears

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
-//Lista vazia
-function empty() {
-  const orderedList = document.getElementById('lista-tarefas');
-  const hello = document.createElement('h5');
-  hello.id = 'hello';
-  hello.innerText =
-  `Não seja vagabundo,
+//Iniciando a pagina
+function coachStart() {
+  const hello = document.querySelector('#hello');
+  hello.innerText = `Não seja vagabundo,
   e coloque algo aqui,
   desgraça!`;
-  if (orderedList.childElementCount === 0) {
-    orderedList.appendChild(hello);
-  }
-  else;
+  return hello;
 }
 
-window.onload = empty;
+function coach(){
+  const hello = document.querySelector('#hello');
+  hello.innerText = `Criou coragem e escreveu!
+  agora, é deixar de corpo mole!`;
+  return hello;
+}
+
+function coachSpace() {
+  const orderedList = document.getElementById('lista-tarefas');
+  if(orderedList.childElementCount === 1) {
+    coachStart();
+  } else {
+    coach();
+  }
+}
+
+onload = coachSpace();
 
 // Criar a lista de tarefas
+
 function creatSchedule() {
-  if (document.getElementById('texto-tarefa').value !== '') {
-    const hello = document.getElementById('hello');
-  hello.innerHTML = '';
-  const write = document.getElementById('texto-tarefa').value;
-  const orderedList = document.getElementById('lista-tarefas');
-  const listItem = document.createElement('li');
-  listItem.innerText = write;
-  orderedList.appendChild(listItem);
-  document.getElementById('texto-tarefa').value = '';
+  if (document.getElementById('texto-tarefa').value === '' || document.getElementById('texto-tarefa').value === null) {
+    alert('Ai não dá, filhão!')
+  } else {
+    coach();
+    const write = document.getElementById('texto-tarefa').value;
+    const orderedList = document.getElementById('lista-tarefas');
+    const listItem = document.createElement('li');
+    listItem.innerText = write;
+    orderedList.appendChild(listItem);
+    document.getElementById('texto-tarefa').value = '';
   }
-  else if (document.getElementById('texto-tarefa').value === '') {
-    alert('Ai não dá filhão')
-  }
+  select();
 }
 
 const btnTask = document.getElementById('criar-tarefa');
 btnTask.addEventListener('click', creatSchedule);
+
+
+//Seleção de tarefa
+function deselect() {
+  document.querySelectorAll('.selected').forEach((element) => {
+  element.classList.remove('selected')});
+}
+
+function greyMark(focusTask) {
+  deselect();
+  const taskselected = focusTask.target;
+  taskselected.classList.add('selected');
+}
+
+function select(){
+  const scheduleList = document.querySelectorAll('li');
+  scheduleList.forEach((element) => {element.addEventListener('click', greyMark)});
+}
+
+//
+
+
