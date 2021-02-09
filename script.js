0269fc7d6e15ecaacf1dfcc268d95060ac5368b3
Arlesson Moura <arlessonmss@gmail.com>
2021-02-09 16:44:58 -0300

    It was a bad joke 3

--- script.js
+++ script.js
@@ -33,41 +33,12 @@ function select() {
   });
 }
 
-// //  Iniciando a pagina
-
-// function coachStart() {
-//   const hello = document.querySelector('#hello');
-//   hello.innerText = `Não seja vagabundo,
-//   e coloque algo aqui,
-//   desgraça!`;
-//   return hello;
-// }
-
-// function coach() {
-//   const hello = document.querySelector('#hello');
-//   hello.innerText = `Criou coragem e escreveu!
-//   agora, é deixar de corpo mole!`;
-//   return hello;
-// }
-
-// function coachSpace() {
-//   const orderedList = document.getElementById('lista-tarefas');
-//   if (orderedList.childElementCount === 1) {
-//     coachStart();
-//   } else {
-//     coach();
-//   }
-// }
-
-// onload = coachSpace();
-
 // Criar a lista de tarefas
 
 function creatSchedule() {
   if (document.getElementById('texto-tarefa').value === '' || document.getElementById('texto-tarefa').value === null) {
     alert('Ai não dá, filhão!');
   } else {
-    coach();
     const write = document.getElementById('texto-tarefa').value;
     const orderedList = document.getElementById('lista-tarefas');
     const listItem = document.createElement('li');
