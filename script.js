4190a5636b5e4abfd7f7d6b92c366bf5fc971b66
lcgoandete <lcgoandete@hotmail.com>
2021-03-15 10:43:35 -0300

    salve o conteúdo da lista. Se você fechar e reabrir a página, a lista deve continuar no estado em que estava-01

--- script.js
+++ script.js
@@ -100,7 +100,8 @@ function moveToDown() {
     for (let index = 0; index < taskList.childElementCount; index += 1) {
       if (taskList.children[index].style.backgroundColor === 'rgb(128, 128, 128)') {
         if (taskList.children[index].nextSibling) {
-          taskList.insertBefore(taskList.children[index], taskList.children[index].nextElementSibling);
+          taskList.insertBefore(taskList.children[index],
+             taskList.children[index].nextElementSibling);
         }
       }
     }
